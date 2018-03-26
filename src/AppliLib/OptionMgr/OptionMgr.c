1f02373e8d134cca9c01672110c487da9b43e790
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-03-26 17:44:57 +0100

    Preliminary architecture

---
+++ src/AppliLib/OptionMgr/OptionMgr.c
@@ -0,0 +1,9 @@
+/**********************************************//** 
+  @file OptionMgr.c
+  
+  @class OptionMgr
+  
+  @brief The OptionMgr class manages the application configuration.
+  
+  The class OptionMgr is TBD
+**************************************************/
