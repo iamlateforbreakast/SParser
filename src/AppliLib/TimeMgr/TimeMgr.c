87d6db3c42297a569760bb83cf9d58b21140a21c
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-03-26 17:43:56 +0100

    Preliminary architecture

--- src/AppliLib/TimeMgr/TimeMgr.c
+++ src/AppliLib/TimeMgr/TimeMgr.c
@@ -1,1 +1,9 @@
-
+/**********************************************//** 
+  @file TimeMgr.c
+  
+  @class TimeMgr
+  
+  @brief This file implements the class TimeMgr
+  
+  The class TimeMgr is TBD
+**************************************************/
--- src/AppliLib/TimeMgr/TimeMgr.h
+++ src/AppliLib/TimeMgr/TimeMgr.h
@@ -1,1 +1,9 @@
-
+/**********************************************//** 
+  @file TimeMgr.h
+  
+  @class TimeMgr
+  
+  @brief This file contains the prototye for the class TimeMgr
+  
+  The class TimeMgr is TBD
+**************************************************/
