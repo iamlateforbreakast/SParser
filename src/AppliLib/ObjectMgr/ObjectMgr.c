127bce182dde9ddd942553147ab11d72bf96b01a
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-04-30 16:37:37 +0100

    Fill class framework

--- src/AppliLib/ObjectMgr/ObjectMgr.c
+++ src/AppliLib/ObjectMgr/ObjectMgr.c
@@ -1,1 +1,32 @@
+/**********************************************//** 
+  @file ObjectMgr.c
+  
+  @brief TBD
+  @details TBD
+**************************************************/
 
+#include "ObjectMgr.h"
+#include "Object.h"
+
+struct ObjectMgr
+{
+  Object object;
+};
+
+PRIVATE ObjectMgr * ObjectMgr_new()
+{
+  ObjectMgr * this = 0;
+  
+  return this;
+}
+
+PUBLIC void ObjectMgr_delete(ObjectMgr * this)
+{
+}
+
+PUBLIC ObjectMgr * ObjectMgr_copy(ObjectMgr * this)
+{
+  ObjectMgr * copy = 0;
+  
+  return copy;
+}
--- src/AppliLib/ObjectMgr/ObjectMgr.h
+++ src/AppliLib/ObjectMgr/ObjectMgr.h
@@ -1,1 +1,13 @@
+/* ObjectMgr.h */
 
+#ifndef _OBJECTMGR_H_
+#define _OBJECTMGR_H_
+
+#include "Types.h"
+
+typedef struct ObjectMgr ObjectMgr;
+
+PUBLIC void ObjectMgr_delete(ObjectMgr * this);
+PUBLIC ObjectMgr * ObjectMgr_copy(ObjectMgr * this);
+
+#endif /* _OBJECTMGR_H_ */
