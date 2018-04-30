4faf3682fa027836c98fa34f671db2728981db53
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-04-30 16:36:27 +0100

    Fill class framework

--- src/AppliLib/ErrorMgr/ErrorMgr.c
+++ src/AppliLib/ErrorMgr/ErrorMgr.c
@@ -1,1 +1,30 @@
+/**********************************************//** 
+  @file ErrorMgr.c
+  
+  The class ErrorMgr is TBD
+**************************************************/
 
+#include "ErrorMgr.h"
+#include "Object.h"
+
+struct ErrorMgr{
+  Object object;
+};
+
+PRIVATE ErrorMgr * ErrorMgr_new()
+{
+  ErrorMgr * this = 0;
+  
+  return this;
+}
+
+PUBLIC void ErrorMgr_delete(ErrorMgr * this)
+{
+}
+
+PUBLIC ErrorMgr * ErrorMgr_copy(ErrorMgr * this)
+{
+  ErrorMgr * copy;
+  
+  return copy;
+}
