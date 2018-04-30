40238368af96c437413b397fc56c49b39bcac629
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-04-30 16:38:37 +0100

    Fill class framework

--- src/AppliLib/OptionMgr/OptionMgr.h
+++ src/AppliLib/OptionMgr/OptionMgr.h
@@ -1,4 +1,14 @@
-/**********************************************//** 
-  @file OptionMgr.h
-  
-**************************************************/
+/* OptionMgr.h */
+
+#ifndef _OPTIONMGR_H_
+#define _OPTIONMGR_H_
+
+#include "Types.h"
+
+typedef struct OptionMgr OptionMgr;
+
+PUBLIC void OptionMgr_delete(OptionMgr * this);
+PUBLIC OptionMgr * OptionMgr_copy(OptionMgr * this);
+PUBLIC PUBLIC OptionMgr* OptionMgr_getOptionMgr();
+
+#endif /* _OPTIONMGR_H_ */
