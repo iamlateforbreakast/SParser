976a10e5bf06c98f35ca70b5f827d184227a7ab5
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-04-30 16:43:02 +0100

    Minor cosmetic updates

--- src/ParseLib/SParse/SParse.c
+++ src/ParseLib/SParse/SParse.c
@@ -5,21 +5,36 @@
   
   @brief This file contains the implementation for the class SParse
   
-  The class SParse parses all files with extension
+  The class SParse parses all files with extension .X
   and stores the result of the parsing in the SQLite DB name.
 **************************************************/
 
+#include "Object.h"
 #include "FileReader.h"
-
+#include "SdbMgr.h"
+//#include "ErrorMgr.h"
+//#include "Grammar1.h"
 #include "SParse.h"
+
+typedef struct SParseDefault
+{
+  char* extension;
+  unsigned int (*function_parse)(/* FileReader, SdbMgr */);
+} SParseDefault;
 
 struct SParse
 {
+  Object object;
   char * extension;
   char * sdbName;
 };
 
-SParse *SParse_new(/* Sdb name */)
+static const SParseDefault SParse_default[] = {{"*.c",0/*&Grammar1_process*/},
+                                               {"*.c",0/*&Grammar2_process*/}};
+
+static const char * SParse_ignoreFiles[] = {"test.c"};
+
+PUBLIC SParse *SParse_new(/* Sdb name */)
 {
   SParse * result = 0;
 
@@ -28,7 +43,11 @@ SParse *SParse_new(/* Sdb name */)
   return result;
 }
 
-unsigned int SParse_parse()
+PUBLIC void SParse_delete(SParse * this)
+{
+}
+
+PUBLIC unsigned int SParse_parse(/* extension */)
 {
   unsigned int result = 0;
   
@@ -42,22 +61,29 @@ unsigned int SParse_parse()
   
   return result;  
 }
- 
-void SParse_delete(SParse * this)
+
+PRIVATE unsigned int SParse_parseFile(SParse * this /* , Filename * file */ )
 {
+  unsigned int error = 0;
+  /* 1) If fileName is to be ignored */
+  
+  /* 2) Create a FileReader object */
+  /*   FileReader * fileReader = FileReader_new(fileName); */
+  
+  /* 3) Parse file with Grammar and stores in DB */
+  /*   Grammar1_process(FileReader, sdbMgr); */
+  
+  /* 4) If error in StreamParser_parse then exit */
+
+  return error;
 }
 
-unsigned int SParse_parseFile(SParse * this /* , Filename * file */ )
+PRIVATE unsigned int SParse_isFileIgnored(SParse * this /* FileName * file */)
 {
   unsigned int result = 0;
   
-  /*   1) Create a FileReader object */
-  /* FileReader * fileReader = FileReader_new(file); */
+  /* Check if a file needs to be ignored */
   
-  /*   2) Create a StreamParser object */
-  /*  StreamParser * streamParser = StreamParser_new(fileReader); */
-  
-  /*   3) If error in StreamParser_parse then exit */
-
   return result;
 }
+
