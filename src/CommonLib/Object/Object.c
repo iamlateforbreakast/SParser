5920fdd51d936001e490eb734d5a4859d61b93d5
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-04-30 16:57:49 +0100

    Add class framework

--- src/CommonLib/Object/Object.c
+++ src/CommonLib/Object/Object.c
@@ -1,1 +1,38 @@
+/**********************************************//** 
+  @file Object.c
+  
+  @class Object
+  
+  @brief This file contains the implementation for the class Object
+  
+  The class Object is TBD
+**************************************************/
 
+#include "Object.h"
+
+PUBLIC Object * Object_new(unsigned int size, void (*f_delete)(Object*), void (*f_copy)(Object*))
+{
+  Object * this = 0;
+  
+  /* this->delete = f_delete; */
+  /* this->copy = f_copy; */
+  /* this->refCount = 0; */
+  /* this->size = size; */
+  
+  return this;
+}
+
+PUBLIC void Object_delete(Object * this)
+{
+}
+
+PUBLIC Object * Object_copy(Object * this)
+{
+  Object * copy;
+  
+  return copy;
+}
+
+PUBLIC Object* Object_getRef(Object* this)
+{
+}
--- src/CommonLib/Object/Object.h
+++ src/CommonLib/Object/Object.h
@@ -1,1 +1,24 @@
+/* Object.h */
 
+#ifndef _OBJECT_H_
+#define _OBJECT_H_
+
+#include "Types.h"
+
+typedef struct Object Object;
+
+struct Object
+{
+  unsigned int id;
+  void (*delete)(Object * this);
+  void (*copy)(Object * this);
+  unsigned int refCount;
+  unsigned int size;
+};
+
+PUBLIC Object * Object_new(unsigned int size, void (*f_delete)(Object*), void (*f_copy)(Object*));
+PUBLIC void Object_delete(Object * this);
+PUBLIC Object * Object_copy(Object * this);
+PUBLIC Object* Object_getRef(Object* this);
+
+#endif /* _OBJECT_H_ */
