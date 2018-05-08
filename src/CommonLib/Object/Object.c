dc9b00e03798ee3b66a661a453a626ac15515fa8
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-05-08 17:38:14 +0100

    Update API

--- src/CommonLib/Object/Object.c
+++ src/CommonLib/Object/Object.c
@@ -9,11 +9,20 @@
 **************************************************/
 
 #include "Object.h"
+#include "ObjectMgr.h"
+
+/* PRIVATE ObjectMgr * Object_objMgrPtr = 0; */
 
 PUBLIC Object * Object_new(unsigned int size, void (*f_delete)(Object*), void (*f_copy)(Object*))
 {
   Object * this = 0;
   
+  /* if (object_objectPtr==NULL)
+  {
+    // TODO: Not re-entrant
+    Object_objectPtr = ObjectMgr_getRef();
+  } */
+  /* this = ObjectMgr_allocate(objectMgr, size); */
   /* this->delete = f_delete; */
   /* this->copy = f_copy; */
   /* this->refCount = 0; */
@@ -24,15 +33,20 @@ PUBLIC Object * Object_new(unsigned int size, void (*f_delete)(Object*), void (*
 
 PUBLIC void Object_delete(Object * this)
 {
+  /* ObjectMgr_deallocate(objectPtr, this); */
 }
 
 PUBLIC Object * Object_copy(Object * this)
 {
   Object * copy;
   
+  /* copy = this->f_copy(this); */
+  
   return copy;
 }
 
 PUBLIC Object* Object_getRef(Object* this)
-{
+{ 
+  /* this->refCount++; */
+  return this;
 }
--- src/CommonLib/Object/Object.h
+++ src/CommonLib/Object/Object.h
@@ -11,12 +11,12 @@ struct Object
 {
   unsigned int id;
   void (*delete)(Object * this);
-  void (*copy)(Object * this);
+  Object * (*copy)(Object * this);
   unsigned int refCount;
   unsigned int size;
 };
 
-PUBLIC Object * Object_new(unsigned int size, void (*f_delete)(Object*), void (*f_copy)(Object*));
+PUBLIC Object * Object_new(unsigned int size, void (*f_delete)(Object*), Object * (*f_copy)(Object*));
 PUBLIC void Object_delete(Object * this);
 PUBLIC Object * Object_copy(Object * this);
 PUBLIC Object* Object_getRef(Object* this);
