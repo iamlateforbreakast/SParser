28573edfa415c8dcd9b0a0a11dbdc86f3e8e4d56
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-05-08 17:37:34 +0100

    Move object from AppliLIb

--- src/CommonLib/ObjectMgr/ObjectMgr.h
+++ src/CommonLib/ObjectMgr/ObjectMgr.h
@@ -1,1 +1,17 @@
+/* ObjectMgr.h */
 
+#ifndef _OBJECTMGR_H_
+#define _OBJECTMGR_H_
+
+#include "Object.h"
+#include "Types.h"
+
+typedef struct ObjectMgr ObjectMgr;
+
+PUBLIC void ObjectMgr_delete(ObjectMgr * this);
+PUBLIC ObjectMgr * ObjectMgr_copy(ObjectMgr * this);
+PUBLIC ObjectMgr * ObjectMgr_getRef();
+PUBLIC Object * ObjectMgr_allocate(ObjectMgr * this, unsigned int size);
+PUBLIC void ObjectMgr_deallocate(ObjectMgr * this, Object * object);
+
+#endif /* _OBJECTMGR_H_ */
