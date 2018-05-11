274846b9d2e8e3175842fcd7394281cb87109901
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-05-11 17:05:54 +0100

    Implement class

--- src/AppliLib/FileMgr/FileMgr.c
+++ src/AppliLib/FileMgr/FileMgr.c
@@ -1,35 +1,102 @@
 /**********************************************//** 
   @file FileMgr.c
   
-  @class FileMgr
-  
-  @brief This file contains the prototye for the class FileMgr
+  @brief The FileMgr class manages a list of files contained
+  in a group of locations.
   
   The class FileMgr is TBD
 **************************************************/
 
 #include "FileMgr.h"
+#include "String2.h"
 #include "Object.h"
+#include "List.h"
 
+/**********************************************//**
+  @class FileMgr
+**************************************************/
 struct FileMgr
 {
   Object object;
+  List * files;
 };
+
+PRIVATE FileMgr * fileMgr = 0;
 
 PRIVATE FileMgr * FileMgr_new()
 {
-  FileMgr * this;
+  FileMgr * this = 0;
   
   return this;
 }
 
+/**********************************************//** 
+  @brief Delete an instance of the class FileMgr.
+  @public
+  @memberof FileMgr
+**************************************************/
 PUBLIC void FileMgr_delete(FileMgr * this)
 {
 }
 
+/**********************************************//** 
+  @brief Copy an instance of the class FileMgr.
+  @public
+  @memberof FileMgr
+  @return New instance
+**************************************************/
 PUBLIC FileMgr * FileMgr_copy(FileMgr * this)
 {
   FileMgr * copy;
   
   return copy;
+}
+
+/**********************************************//** 
+  @brief Get a reference to the singleton instance of FileMgr.
+  @public
+  @memberof FileMgr
+  @return Reference to the singleton.
+**************************************************/
+PUBLIC FileMgr* FileMgr_getRef()
+{
+  if (fileMgr==0)
+  {
+    fileMgr = FileMgr_new();
+  }
+  else
+  {
+    fileMgr->object.refCount++;
+  }
+  
+  return fileMgr;
+}
+
+/**********************************************//** 
+  @brief Add all files in the given directory to the list of managed files.
+  @public
+  @memberof FileMgr
+  @return Status.
+**************************************************/
+PUBLIC unsigned int FileMgr_addDirectory(FileMgr * this, const char * directoryName)
+{
+  unsigned int result = 0;
+  
+  return result;
+}
+
+PUBLIC unsigned int FileMgr_addFile(FileMgr * this, const char * fileName)
+{
+  unsigned int result = 0;
+  
+  return result;
+}
+
+PUBLIC String* FileMgr_load(FileMgr* this, const char * fileName)
+{
+  String * fileContent = 0;
+  
+  /* Find file in list */
+  
+  return fileContent;
 }
--- src/AppliLib/FileMgr/FileMgr.h
+++ src/AppliLib/FileMgr/FileMgr.h
@@ -4,10 +4,20 @@
 #define _FILEMGR_H_
 
 #include "Types.h"
+#include "String2.h"
 
 typedef struct FileMgr FileMgr;
 
 PUBLIC void FileMgr_delete(FileMgr * this);
 PUBLIC FileMgr * FileMgr_copy(FileMgr * this);
-
+PUBLIC String* FileMgr_load(FileMgr* this, const char * fileName);
+PUBLIC void FileMgr_close(FileMgr* this, String* fileName);
+PUBLIC FileMgr* FileMgr_getRef();
+PUBLIC unsigned int FileMgr_addDirectory(FileMgr * this, const char * directoryName);
+PUBLIC unsigned int FileMgr_addFile(FileMgr * this, const char * fileName);
 #endif /* _FILEMGR_H_ */
+
+/* PUBLIC String* FileMgr_getRootPath(FileMgr* this);
+PUBLIC List* FileMgr_filterFiles(FileMgr* this, String* filter);
+PUBLIC String* FileMgr_searchAndLoad(FileMgr* this, String* fileName);
+PUBLIC List* FileMgr_getFiles(FileMgr* this); */
