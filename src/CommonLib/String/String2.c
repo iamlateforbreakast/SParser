ca6e89002951c4073bb29f690e1ad2078f33e32c
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-05-15 17:59:54 +0100

    Implement body of class

--- src/CommonLib/String/String2.c
+++ src/CommonLib/String/String2.c
@@ -1,1 +1,98 @@
+/**********************************************//** 
+  @file String2.c
+  
+  @brief The String class provide a dynamic array of char terminated
+  by 0.
+  
+  The class String is TBD
+**************************************************/
 
+#include "String2.h"
+#include "Object.h"
+#include <stdlib.h>
+
+/**********************************************//** 
+  @class String
+**************************************************/
+struct String
+{
+  Object object;
+  char * buffer;
+  unsigned int length;
+};
+
+/**********************************************//** 
+  @brief Create a new String object.
+  @public
+  @memberof String
+  @return Created String object.
+**************************************************/
+PUBLIC String * String_new()
+{
+  String * this = 0;
+  
+  this = (String*)Object_new(sizeof(String),(Destructor)&String_delete, (Copy_operator)&String_copy);
+  this->buffer = 0;
+  this->length = 0;
+  
+  return this;
+}
+
+/**********************************************//** 
+  @brief TBD
+  @public
+  @memberof String
+**************************************************/
+PUBLIC void String_delete(String * this)
+{
+  if (this->buffer!=0) free(this->buffer);
+  Object_delete(&this->object);
+}
+
+/**********************************************//** 
+  @brief TBD
+  @public
+  @memberof String
+**************************************************/
+PUBLIC String * String_copy(String * this)
+{
+  String * copy = 0;
+  
+  return copy;
+}
+
+/**********************************************//** 
+  @brief TBD
+  @public
+  @memberof String
+**************************************************/
+PUBLIC unsigned int String_isEqual(String * this, String * compared)
+{
+}
+
+/**********************************************//** 
+  @brief TBD
+  @public
+  @memberof String
+**************************************************/
+PUBLIC String* String_subString(String* this, unsigned int pos, unsigned int length)
+{
+}
+
+/**********************************************//** 
+  @brief TBD
+  @public
+  @memberof String
+**************************************************/
+PUBLIC int String_toInt(String* this)
+{
+}
+
+/**********************************************//** 
+  @brief TBD
+  @public
+  @memberof String
+**************************************************/
+PUBLIC String* String_searchAndReplace(String* this, String* search, String* replace)
+{
+}
--- src/CommonLib/String/String2.h
+++ src/CommonLib/String/String2.h
@@ -1,1 +1,14 @@
+/* String2.h */
 
+#ifndef _STRING2_H_
+#define _STRING2_H_
+
+#include "Types.h"
+
+typedef struct String String;
+
+PUBLIC String * String_new();
+PUBLIC void String_delete(String * this);
+PUBLIC String * String_copy(String * this);
+
+#endif /* _STRING2_H_ */
