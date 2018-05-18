5ddd247da4b962cc2d80b15f15121edabf51b77e
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-05-18 17:09:36 +0100

    Implementation

--- src/AppliLib/FileMgr/FileMgr.c
+++ src/AppliLib/FileMgr/FileMgr.c
@@ -11,8 +11,11 @@
 #include "String2.h"
 #include "Object.h"
 #include "List.h"
+#include "FileDesc.h"
 
 #include <unistd.h>
+#include <dirent.h>
+//#include <stat.h>
 
 #define FILEMGR_MAX_PATH (1024)
 
@@ -38,6 +41,7 @@ PRIVATE FileMgr * fileMgr = 0;
 **************************************************/
 PRIVATE void FileMgr_changeDirectory(FileMgr * this);
 PRIVATE void FileMgr_mergePath(FileMgr* this, String* path1, String* path2);
+PRIVATE void FileMgr_listFiles(FileMgr * this, String * directory);
 
 /**********************************************//** 
   @brief Create an instance of the class FileMgr.
@@ -119,16 +123,26 @@ PUBLIC FileMgr* FileMgr_getRef()
 PUBLIC unsigned int FileMgr_addDirectory(FileMgr * this, const char * directoryName)
 {
   unsigned int result = 0;
-  String * rootDirectory = String_new(this->rootLocation);
+  String * fullPathDirectory = String_new(this->rootLocation);
   String * addedDirectory = String_new(directoryName);
   
   /* Merge directory name with current path to have the full path of the directory*/
-  FileMgr_mergePath(this, rootDirectory, addedDirectory);
+  FileMgr_mergePath(this, fullPathDirectory, addedDirectory);
   
   /* add directory to this->directories */
-  /* List_insertHead(this->directories,addedDirectory); */
+  List_insertHead(this->directories,fullPathDirectory);
+  
   /* For each directory */
   /* List_forEach(this->directories, FileMgr_listFiles, this); */
+  fullPathDirectory = List_getNext(this->directories);
+  
+  while (fullPathDirectory!=0)
+  {
+    FileMgr_listFiles(this, fullPathDirectory);
+    fullPathDirectory = List_getNext(this->directories);
+  }
+  
+  String_delete(addedDirectory);
   
   return result;
 }
@@ -157,6 +171,27 @@ PUBLIC String* FileMgr_load(FileMgr* this, const char * fileName)
 
 PRIVATE void FileMgr_listFiles(FileMgr * this, String * directory)
 {
+  struct dirent *directoryEntry = NULL;
+  DIR * dir = 0;
+  FileDesc * fileDesc= 0;
+  String * fullName = 0;
+  
+  dir = opendir(String_getBuffer(directory));
+  
+  if (dir!=0)
+  {
+    while ((directoryEntry = readdir(dir)) != NULL) 
+    {
+      if (directoryEntry->d_type != DT_DIR)
+      {
+        fileDesc = FileDesc_new();
+        fullName = String_copy(directory);
+        FileMgr_mergePath(this, fullName, String_new(directoryEntry->d_name));
+        FileDesc_setFullName(fileDesc, fullName);
+        List_insertHead(this->files, (void*)fileDesc);
+      }
+    }
+  }
 }
 
 PRIVATE void FileMgr_changeDirectory(FileMgr * this)
@@ -175,57 +210,51 @@ PRIVATE void FileMgr_changeDirectory(FileMgr * this)
 PRIVATE void FileMgr_mergePath(FileMgr* this, String* path1, String* path2)
 {
   char * p2_idx = 0;
-  unsigned int p1_idx = String_length(path1);
+  char * p1_idx = 0;
   char buffer[FILEMGR_MAX_PATH];
-  //char* token = String_getBuffer(path2);
   
   // Initialise result buffer
   memcpy(buffer, String_getBuffer(path1), String_getLength(path1));
   
-  buffer[p1_idx] = '/';
-  p1_idx++;
-  
-  for (p2_idx=0; p2_idx<String_getLength(path2); p2_idx++)
+  p1_idx = buffer + String_getLength(path1);
+  if (*p1_idx != '/')
   {
-    if ((path2->buffer[p2_idx] == '/') || (p2_idx > (path2->length - 1)))
-    {
+    *p1_idx = '/';
+    p1_idx++;
+  }
+  
+  for (p2_idx=String_getBuffer(path2); p2_idx<(String_getBuffer(path2)+String_getLength(path2)); p2_idx++)
+  {
       //Take ../ into account
-      if (memcmp(token, "..", 2) == 0)
+      if (memcmp(p2_idx, "..", 2) == 0)
       {
-        p1_idx = p1_idx - 4;
-        while ((path1->buffer[p1_idx] != '/') && (p1_idx > 0))
+        p1_idx = p1_idx - 2;
+        while ((*p1_idx != '/') && (p1_idx > buffer))
         {
-          p1_idx = p1_idx - 1;
+          p1_idx--;
         }
-        p1_idx = p1_idx + 1;
-        token = path2->buffer + p2_idx + 1;
+        p1_idx++;
+        p2_idx = p2_idx + 2;
       }
       //Ignore ./ in path2
-      else if (memcmp(token, ".", 1) == 0)
+      else if (memcmp(p2_idx, ".", 1) == 0)
       {
-        p1_idx = p1_idx - 1;
-        token = path2->buffer + p2_idx + 1;
+        //p1_idx = p1_idx - 1;
+        p2_idx = p2_idx + 2;
       }
       else
       {
         //p2_idx = p2_idx + 1;
-        token = path2->buffer + p2_idx + 1;
-        buffer[p1_idx] = '/';
+        *p1_idx = *p2_idx;
+        //*p1_idx = '/';
         p1_idx++;
       }
-    }
-    else
-    {
-      // Copy char from p2 to p1
-      buffer[p1_idx] = path2->buffer[p2_idx];
-      p1_idx++;
-    }
   }
   
+  *p1_idx = 0;
+  
   //Place merged path into path1
-  Memory_free(path1->buffer, path1->length);
-  path1->length = p1_idx;
-  path1->buffer = Memory_alloc(path1->length);
-  memcpy(path1->buffer, buffer, path1->length);
-  //String_print(path1, "FileMgr: active path ");
+  String_delete(path1);
+  path1 = String_new(buffer);
+  printf("Merged path: %s\n", buffer);
 }
