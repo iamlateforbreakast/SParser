20e161536267bdec3a48ef11ce24531165857832
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-05-23 18:21:08 +0100

    Implementation

--- src/CommonLib/Debug/Debug.c
+++ src/CommonLib/Debug/Debug.c
@@ -1,1 +1,19 @@
+/**********************************************//**
+  @file Debug.c
 
+  @brief This file contains debugging functions.
+
+  The debugging function are TBD
+**************************************************/
+
+#include "Debug.h"
+
+#include <stdio.h>
+
+void dbg_printf(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stdout, fmt, args);
+    va_end(args);
+}
--- src/CommonLib/Debug/Debug.h
+++ src/CommonLib/Debug/Debug.h
@@ -1,1 +1,7 @@
+/* Debug.h */
 
+#include <stdarg.h>
+
+#define TRACE(x) do { if (DEBUG) dbg_printf x; } while (0)
+
+void dbg_printf(const char *fmt, ...);
