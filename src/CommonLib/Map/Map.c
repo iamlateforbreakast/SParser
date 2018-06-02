890e220ca576fe28a998b3807701a863f8b4f679
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-06-02 08:45:58 +0100

    Implementation

--- src/CommonLib/Map/Map.c
+++ src/CommonLib/Map/Map.c
@@ -2,7 +2,7 @@
   @file Map.c
  
   @brief A Map class.
-  @details This class provides a container indexed
+  This class provides a container indexed
   by a string.
 **************************************************/
 
@@ -12,13 +12,18 @@
 
 #define HTABLE_SIZE (50)
 
-PRIVATE unsigned int Map_hash(Map* this, String* s, unsigned int i);
+/**********************************************//**
+  @private
+**************************************************/
+PRIVATE unsigned int Map_hash(Map * this, char * s, unsigned int i);
 
+/**********************************************//**
+  @private
+**************************************************/
 typedef struct MapEntry
 {
-  String* s;
-  void* p;
-  List* l;
+  String * s;
+  void * item;
 } MapEntry;
 
 /**********************************************//**
@@ -26,54 +31,127 @@ typedef struct MapEntry
 **************************************************/
 struct Map
 {
-  MapEntry htable[HTABLE_SIZE];
+  List * htable[HTABLE_SIZE];
 };
 
 /**********************************************//** 
-  @brief TBD
-  @details TBD
+  @brief Create a new instance of the class Map.
   @public
   @memberof Map
 **************************************************/
 PUBLIC Map* Map_new()
 {
   Map * this = 0;
+  unsigned int i = 0;
+  
+  this = (Map*)Object_new(sizeof(Map),(Destructor)&Map_delete, (Copy_operator)&Map_copy);
+  
+  for (i=0;i<HTABLE_SIZE;i++)
+  {
+    this->htable[i] = 0;
+  }
   
   return this;
 }
 
 /**********************************************//** 
   @brief TBD
-  @details TBD
   @public
   @memberof Map
 **************************************************/
-PUBLIC void Map_delete(Map* this)
+PUBLIC void Map_delete(Map * this)
 {
+  unsigned int i = 0;
+  
+  if (this != 0)
+  {
+    for (i=0;i<HTABLE_SIZE;i++)
+    {
+      List_delete(this->htable[i]);
+    }
+  
+    Object_delete(&this->object);
+  }
 }
 
 /**********************************************//** 
-  @brief TBD
-  @details TBD
+  @brief Copy an instance of the class Map.
   @public
   @memberof Map
 **************************************************/
-PUBLIC unsigned int Map_insert(Map* this,String* s, void* p)
+PUBLIC Map * Map_copy(Map * this)
 {
-  unsigned int result = 0;
+  Map * result = 0;
   
   return result;
 }
 
 /**********************************************//** 
   @brief TBD
-  @details TBD
+  @public
+  @memberof Map
+**************************************************/
+PUBLIC unsigned int Map_insert(Map * this,String * s, void * p)
+{
+  unsigned int result = 0;
+  unsigned int key = 0;
+  unsigned int i = 0;
+  MapEntry * entry =0;
+  
+  /* TODO: Manage duplication */
+  for (i=1; (i<=s->length) && (result==0); i++)
+  {
+    key = Map_hash(this,String_getBuffer(s), i);
+    if (this->htable[key] == 0)
+    {
+      this->htable[key] = List_new();
+      entry = (MapEntry*)Memory_alloc(sizeof(MapEntry));
+      entry->s = s;
+      entry->item = p;
+      List_insertHead(this->htable[key], entry);
+      result = 1;
+    }
+    else if (i==s->length) 
+    {
+      entry = (MapEntry*)Memory_alloc(sizeof(MapEntry));
+      entry->s = s;
+      entry->item = p;
+      List_insertHead(this->htable[key], entry);
+    }
+  }
+  return result;
+}
+
+/**********************************************//** 
+  @brief TBD
   @public
   @memberof Map
 **************************************************/
 PUBLIC unsigned int Map_find(Map* this, String* s, void** p)
 {
   unsigned int result = 0;
+  unsigned int key = 0;
+  unsigned int i = 0;
+  unsigned int isFound = 0;
+  ListNode n = 0;
+  
+  for (i=1; (i<=s->length) && (!isFound); i++)
+  {
+    key = Map_hash(this, String_getBuffer(s), i);
+    if (this->htable[key] != 0)
+    {
+      while ((n = List_getNext(this->htable[key]))!= 0)
+      {
+        if (String_isEqual((ListNode*)n->s, s))
+        {
+          *p = (ListNode*)n->item;
+          isFound = 1;
+          result = 1;
+          break;
+        }
+      }
+    }
+  }
   
   return result;
 }
@@ -84,9 +162,23 @@ PUBLIC unsigned int Map_find(Map* this, String* s, void** p)
   @private
   @memberof Map
 **************************************************/
-PRIVATE unsigned int Map_hash(Map* this, String* s, unsigned int i)
+PRIVATE unsigned int Map_hash(Map * this, char * s, unsigned int length)
 {
   unsigned int result = 0;
+  unsigned int i = 0;
   
+  for (i=0; i<length; i++)
+  {
+    result = result + s[i];
+    result += (result << 10);
+    result ^= (result>>6);
+  }
+  result = result % HTABLE_SIZE;
+  /*
+          hash += key[i], hash += ( hash << 10 ), hash ^= ( hash >> 6 );
+    }
+    hash += ( hash << 3 ), hash ^= ( hash >> 11 ), hash += ( hash << 15 );
+    */
+
   return result;
 }
--- src/CommonLib/Map/Map.h
+++ src/CommonLib/Map/Map.h
@@ -3,14 +3,14 @@
 #ifndef _MAP_H_
 #define _MAP_H_
 
-#include "List.h"
-#include "String2.h"
 #include "Types.h"
 
 typedef struct Map Map;
 
-PUBLIC Map* Map_new();
-PUBLIC void Map_delete(Map* this);
-PUBLIC unsigned int Map_insert(Map* this,String* s, void* p);
-PUBLIC unsigned int Map_find(Map* this, String* s, void** p);
-#endif
+PUBLIC Map * Map_new();
+PUBLIC void Map_delete(Map * this);
+PUBLIC Map * Map_copy(Map * this);
+PUBLIC unsigned int Map_insert(Map * this,String* s, void * p);
+PUBLIC unsigned int Map_find(Map * this, String* s, void ** p);
+
+#endif /* _MAP_H_ */
