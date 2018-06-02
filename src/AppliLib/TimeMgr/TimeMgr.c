1c832f6ea37b63aa6835599c7762b03e12d5ac22
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-06-02 08:47:43 +0100

    Create Timer class

--- src/AppliLib/TimeMgr/TimeMgr.c
+++ src/AppliLib/TimeMgr/TimeMgr.c
@@ -1,9 +1,90 @@
 /**********************************************//** 
   @file TimeMgr.c
   
-  @class TimeMgr
-  
   @brief This file implements the class TimeMgr
   
   The class TimeMgr is TBD
 **************************************************/
+#include "TimeMgr.h"
+#include "Timer.h"
+#include "String2.h"
+
+/**********************************************//**
+  @class TimeMgr
+**************************************************/
+struct TimeMgr
+{
+  List * timers;
+}
+
+/**********************************************//**
+  @private
+**************************************************/
+PRIVATE TimeMgr * TimeMgr_new()
+{
+  TimeMgr * this = 0;
+  
+  return this;
+}
+
+/**********************************************//** 
+  @brief TBD
+  @public
+  @memberof TimeMgr
+**************************************************/
+PUBLIC void TimeMgr_delete(TimeMgr * this)
+{
+}
+
+/**********************************************//**
+  @brief TBD
+  @public
+  @memberof TimeMgr
+**************************************************/
+PUBLIC TimeMgr * TimeMgr_copy(TimeMgr * this)
+{
+  TimeMgr * copy = 0;
+  
+  return copy;
+}
+
+/**********************************************//**
+  @brief TBD
+  @public
+  @memberof TimeMgr
+**************************************************/
+PUBLIC TimeMgr * TimeMgr_getRef()
+{
+  TimeMgr * result = 0;
+  
+  return result;
+}
+
+/**********************************************//** 
+  @brief TBD
+  @public
+  @memberof TimeMgr
+**************************************************/
+PUBLIC void TimeMgr_createTimer(TimeMgr * this, String * s)
+{
+}
+
+/**********************************************//** 
+  @brief TBD
+  @public
+  @memberof TimeMgr
+**************************************************/
+PUBLIC void TimeMgr_timeStamp(TimeMgr * this, String * s)
+{
+  /* Find timer s */
+  
+}
+
+/**********************************************//** 
+  @brief TBD
+  @public
+  @memberof TimeMgr
+**************************************************/
+PUBLIC void TimeMgr_report(TimeMgr * this)
+{
+}
