e3cb9b136eca0ff7c31647368bced056d1dd608d
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-06-03 18:25:39 +0100

    Add tests

---
+++ src/CommonLib/Map/tests/main.c
@@ -0,0 +1,48 @@
+#include "Map.h"
+#include "String.h"
+
+Map * testMap = 0;
+
+int step1()
+{
+  testMap = Map_new();
+  
+  return 0;
+}
+
+int step2()
+{
+  String * s = String_new("Hello world");
+  String * item = String_new("The value");
+  
+  Map_insert(testMap, s, item);
+  
+  return 0;
+}
+
+int step3()
+{
+  String * s = String_new("Hello world");
+  String * item = 0;
+  
+  Map_find(testMap, s, &item);
+  
+  return 0;
+}
+
+int step4()
+{
+  Map_delete(testMap);
+  
+  return 0;
+}
+
+int main()
+{
+  step1();
+  step2();
+  step3();
+  step4();
+  
+  return 0;
+}
