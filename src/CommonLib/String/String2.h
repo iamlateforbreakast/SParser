ed178f55acbb7514985d96be5263df7316eca4bc
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-06-19 08:29:14 +0100

    Add isEqual

--- src/CommonLib/String/String2.h
+++ src/CommonLib/String/String2.h
@@ -15,4 +15,5 @@ PUBLIC char * String_getBuffer(String * this);
 PUBLIC void String_setBuffer(String * this, char * buffer);
 PUBLIC unsigned int String_isContained(String * this, String * s2);
 PUBLIC unsigned int String_prepend(String * this, const char * prefix);
+PUBLIC unsigned int String_isEqual(String * this, String * compared);
 #endif /* _STRING2_H_ */
