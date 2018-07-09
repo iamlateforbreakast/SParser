0b5ab6320442bd68d600782a917de09580fb9598
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-07-09 09:06:05 +0100

    Finish to implement

--- src/AppliLib/SdbMgr/SdbRequest.c
+++ src/AppliLib/SdbMgr/SdbRequest.c
@@ -1,6 +1,7 @@
 #include "SdbRequest.h"
 #include "Object.h"
 #include "SdbMgr.h"
+#include "Memory.h"
 
 #include <stdarg.h>
 
@@ -14,12 +15,17 @@ struct SdbRequest
 
 PRIVATE SdbMgr * sdbMgr = 0;
 
-PUBLIC SdbRequest * SdbRequest_new()
+PUBLIC SdbRequest * SdbRequest_new(const char * fmt)
 {
   SdbRequest * this = 0;
-  
+  if (!sdbMgr)
+  {
+    sdbMgr = SdbMgr_getRef();
+  }
   this = (SdbRequest*)Object_new(sizeof(SdbRequest),(Destructor)&SdbRequest_delete, (Copy_operator)&SdbRequest_copy);
   this->object.size = sizeof(SdbRequest);
+  
+  this->fmt = fmt;
   
   return this;
 }
@@ -39,13 +45,15 @@ PUBLIC SdbRequest * SdbRequest_copy(SdbRequest * this)
 PUBLIC void SdbRequest_execute(SdbRequest * this, ...)
 {
   unsigned int size = 0;
-  
-  /* get SdbMgr */
   va_list args;
+  
   va_start(args, this->fmt);
   size = vsnprintf(0, 0, this->fmt, args);
-  this->buffer = malloc(size+ 1);
+  this->buffer = Memory_realloc(size + 1);
   vsnprintf(this->buffer, size, this->fmt, args);
   va_end(args);
-  SdbMgr_execute(sdbMgr, this->buffer);
+  
+  SdbMgr_execute(sdbMgr, 
+                 this->buffer, 
+                 this->result);
 }
