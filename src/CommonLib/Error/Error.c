6351c347110d3c0b4106dcfd4950b0a6f8b7ce21
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-07-10 17:39:01 +0100

    Initial revision

--- src/CommonLib/Error/Error.c
+++ src/CommonLib/Error/Error.c
@@ -1,1 +1,37 @@
+#include "Error.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+PUBLIC void Error_new(ErrorSeverity severity, char * msg, ...)
+{
+  va_list args;
+
+  switch (severity)
+  {
+    case ERROR_DBG:
+	va_start(args, msg);
+	printf("[DBUG] ");
+	printf(args);
+	va_end(args);
+	break;
+    case ERROR_INFO:
+	va_start(args, msg);
+	printf("[INFO] ");
+	printf(args);
+	va_end(args);
+	break;
+    case ERROR_NORMAL:
+	va_start(args, msg);
+	printf("[NORM] ");
+	printf(args);
+	va_end(args);
+	break;
+    case ERROR_FATAL:
+	va_start(args, msg);
+	printf("[EXIT] ");
+	printf(args);
+	va_end(args);
+	exit(2);
+  }
+}
--- src/CommonLib/Error/Error.h
+++ src/CommonLib/Error/Error.h
@@ -1,1 +1,13 @@
+/* Error.h */
 
+#include "Types.h"
+
+typedef enum
+{
+  ERROR_DBG,
+  ERROR_INFO,
+  ERROR_NORMAL,
+  ERROR_FATAL
+} ErrorSeverity;
+
+PUBLIC void Error_new(ErrorSeverity severity, char * msg, ...);
