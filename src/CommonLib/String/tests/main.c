05179589c2ec1a11b2b482471294206067215e10
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-07-10 17:49:31 +0100

    Initial revision

--- src/CommonLib/String/tests/main.c
+++ src/CommonLib/String/tests/main.c
@@ -1,1 +1,41 @@
+#include "ObjectMgr.h"
+#include "Object.h"
+#include "String2.h"
 
+int step1()
+{
+  String * testString = 0;
+
+  testString = String_new("Hello World!");
+
+  String_delete(testString);
+
+  return 0;
+}
+
+int step2()
+{
+  String * testString = 0;
+  String * testPattern = 0;
+
+  
+  testString = String_new("Hello World!");
+  testPattern = String_new("World");
+
+  if (String_isContained(testString, testPattern))
+  {
+    printf("Passed\n");
+  }
+
+  String_delete(testString);
+
+  return 0;
+}
+
+int main()
+{
+  step1();
+  step2();
+
+  return 0;
+}
