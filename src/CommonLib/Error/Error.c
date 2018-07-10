995c03a5607f1428f5f798a5d95fbfafa559b8b6
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-07-10 19:43:03 +0100

    Call to vprintf

--- src/CommonLib/Error/Error.c
+++ src/CommonLib/Error/Error.c
@@ -12,25 +12,25 @@ PUBLIC void Error_new(ErrorSeverity severity, char * msg, ...)
     case ERROR_DBG:
 	va_start(args, msg);
 	printf("[DBUG] ");
-	printf(args);
+	vprintf(msg, args);
 	va_end(args);
 	break;
     case ERROR_INFO:
 	va_start(args, msg);
 	printf("[INFO] ");
-	printf(args);
+	vprintf(msg, args);
 	va_end(args);
 	break;
     case ERROR_NORMAL:
 	va_start(args, msg);
 	printf("[NORM] ");
-	printf(args);
+	vprintf(msg, args);
 	va_end(args);
 	break;
     case ERROR_FATAL:
 	va_start(args, msg);
 	printf("[EXIT] ");
-	printf(args);
+	vprintf(msg, args);
 	va_end(args);
 	exit(2);
   }
