9e638223ea25602e8f2ad83d28a32b96d99a0cb0
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-07-15 12:25:35 +0100

    Test matchWilcard

--- src/CommonLib/String/tests/main.c
+++ src/CommonLib/String/tests/main.c
@@ -32,10 +32,28 @@ int step2()
   return 0;
 }
 
+int step3()
+{
+  String * testString = 0;
+  const char testPattern = "*.c";
+
+  testString = String_new("test.c");
+  
+  if (String_matchWildcard(testString, testPattern))
+  {
+    printf("Passed\n");
+  }
+  
+  String_delete(testString);
+  
+  return 0;
+}
+
 int main()
 {
   step1();
   step2();
+  step3();
 
   return 0;
 }
