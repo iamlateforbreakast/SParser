83dc1ac7beb1df716bc7757bcee01110fe60a961
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2019-03-25 18:03:38 +0000

    Add files via upload

--- src/CommonLib/Object/Class.h
+++ src/CommonLib/Object/Class.h
@@ -1,1 +1,17 @@
+/* Class. h */
 
+#ifndef _CLASS_H_
+#define _CLASS_H_
+
+#include "Object.h"
+
+struct Class
+{
+  Constructor f_new;
+  Destructor f_delete;
+  Copy_Operator f_copy;
+  Equal_Operator f_equal;
+  Printer f_print;
+};
+
+#endif /* _CLASS_H_ */
--- src/CommonLib/Object/Object.h
+++ src/CommonLib/Object/Object.h
@@ -5,20 +5,26 @@
 
 #include "Types.h"
 
+typedef struct Class Class;
 typedef struct Object Object;
-
-typedef struct Object * (*Constructor)();
-typedef void (*Destructor)(struct Object *);
-typedef struct Object * (*Copy_operator)(struct Object *);
 
 struct Object
 {
   unsigned int id;
+  Class * class;
   void (*delete)(Object * this);
   Object * (*copy)(Object * this);
   unsigned int refCount;
   unsigned int size;
 };
+
+typedef struct Object * (*Constructor)();
+typedef void (*Destructor)(struct Object *);
+typedef struct Object * (*Copy_Operator)(struct Object *);
+typedef unsigned int (*Equal_Operator)(struct Object *, struct Object *);
+typedef char * (*Printer)(struct Object *);
+
+
 
 PUBLIC Object * Object_new(unsigned int size, void (*f_delete)(Object*), Object * (*f_copy)(Object*));
 PUBLIC void Object_delete(Object * this);
