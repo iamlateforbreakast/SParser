73bbf1c49cddf39c1c32109b1ebfe08f99b137b3
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2019-05-10 18:47:49 +0100

    Update new layout for page

--- tools/webserver/webserver.py
+++ tools/webserver/webserver.py
@@ -2,14 +2,15 @@
 
 import string,cgi,time,sqlite3
 from os import curdir, sep
+import os.path
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 #import pri
 
-class MyHandler(BaseHTTPRequestHandler):
-
-    dbinfo = ""; #Data extracted from the DB file
+db_handler = None
     
+class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        print(self.path)
         try:
             if self.path.endswith(".html"):
                 f = open(curdir + sep + self.path) #self.path has /test.html
@@ -41,9 +42,16 @@ class MyHandler(BaseHTTPRequestHandler):
                 self.send_response(200)
                 self.send_header('Content-type',	'text/json')
                 self.end_headers()
-                self.wfile.write(self.dbinfo)
+                db_handler=MyDbHandler()
+                self.wfile.write(db_handler.getTransUnitList())
                 return
-                
+            #if self.path.endswith("units.json"):   #provide our data
+            #    self.send_response(200)
+            #    self.send_header('Content-type',	'text/json')
+            #    self.end_headers()
+            #    db_handler=MyDbHandler()
+            #    self.wfile.write(db_handler.getTransUnitList())
+            #    return   
             return
                 
         except IOError:
@@ -70,21 +78,42 @@ class MyHandler(BaseHTTPRequestHandler):
 class MyDbHandler:
 
     #data = "{\"nodes\":[{\"id\":\"1\", \"label\":\"Node 1\"},{\"id\":\"2\", \"label\":\"Node 2\"}],\"edges\":[{\"from\":\"1\", \"to\":\"2\"}]}";
-    data = ""
-    nodes = ""
-    edges = ""
+
     db = 0;
     cursor = 0;
     
     def __init__(self):
+       print("MyDbHandler.init()");
        self.db = sqlite3.connect('test.db');
        self.cursor = self.db.cursor()
        self.nodes = "\"nodes\":["
        self.edges = "\"edges\":[{\"from\":\"1\", \"to\":\"2\"}]"
+       
     def __del__(self):
        self.db.close();
-    
+       
+    def getTransUnitList(self):
+       print("Get Translation Unit List")
+       data = ""
+       self.nodes = "["
+       self.edges = "[{\"from\":\"1\", \"to\":\"2\"}]"
+       self.cursor.execute('''SELECT Unit_Id, File_Name FROM Translation_Units''')
+       all_rows = self.cursor.fetchall()
+       for num, row in enumerate(all_rows):
+         print('{0} : {1}'.format(row[0], row[1]))
+         name = os.path.basename(row[1])
+         new_node = "{{\"id\":\"{0}\",\"label\":\"{1}\"}}".format(row[0], name) 
+         if num == 0:
+           self.nodes = "{0}{1}".format(self.nodes, new_node)
+         else:
+           self.nodes = "{0},{1}".format(self.nodes, new_node)
+       self.nodes = "{0}]".format(self.nodes)
+       self.data = "{{\"nodes\": {0},\"edges\": {1}}}".format(self.nodes, self.edges)
+       print(self.data)
+       return self.data
+       
     def get_data(self):
+       print("Get Data")       
        self.cursor.execute('''SELECT NodeId, NodeType, NodePtr FROM Nodes''')
        all_rows = self.cursor.fetchall()
        for num, row in enumerate(all_rows):
@@ -105,9 +134,10 @@ class MyDbHandler:
 def main():
     try:
         keep_running = 1
+        db_handler=MyDbHandler()
         handler = MyHandler
-        db_handler = MyDbHandler();
-        handler.dbinfo = db_handler.get_data();
+        #handler.setDbHandler(MyDbHandler())
+        #handler.dbinfo = db_handler.get_data();
         server = HTTPServer(('', 80), handler)
         print 'started httpserver...'
         while keep_running:
@@ -119,4 +149,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
