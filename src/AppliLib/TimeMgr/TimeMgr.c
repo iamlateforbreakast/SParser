908a08b0daee30f86f9790f3a27a90280c0c24e2
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2022-04-04 21:04:49 +0100

    Created TimeMgr.c

---
+++ src/AppliLib/TimeMgr/TimeMgr.c
@@ -0,0 +1,23 @@
+/* TimeMgr.c */
+
+#include "TimeMgr.h"
+
+PUBLIC void TimeMgr_delete(TimeMgr * this)
+{
+}
+
+PUBLIC TimeMgr * TimeMgr_copy(TimeMgr * this)
+{
+}
+
+PUBLIC TimeMgr * TimeMgr_getRef()
+{
+}
+
+PUBLIC void TimeMgr_latchTime(TimeMgr * this, String * s)
+{
+}
+
+PUBLIC void TimeMgr_report(TimeMgr * this)
+{
+}
