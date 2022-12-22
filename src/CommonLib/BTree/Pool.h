37e70a95395b1ce3286bddb0e6cbc3b402517308
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2022-12-22 12:13:04 +0000

    Create Pool.h

---
+++ src/CommonLib/BTree/Pool.h
@@ -0,0 +1,19 @@
+#ifndef _POOL_
+#define _POOL_
+/*********************************************************************************
+* Pool.h
+*
+*********************************************************************************/
+#include "CommonTypes.h"
+
+typedef struct Pool
+{
+	unsigned int isFile;
+} Pool;
+
+PUBLIC Pool * Pool_new();
+PUBLIC Pool * Pool_newFromFile(char * fileName);
+PUBLIC void Pool_free(Pool * pool);
+PUBLIC void Pool_alloc(Pool * pool, unsigned int size);
+PUBLIC Pool_dealloc();
+#endif
