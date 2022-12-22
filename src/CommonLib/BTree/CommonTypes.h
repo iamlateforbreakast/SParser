96cb26eca091b0dabd6eac10c83b20a13538a4ae
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2022-12-22 12:14:38 +0000

    Create CommonTypes.h

---
+++ src/CommonLib/BTree/CommonTypes.h
@@ -0,0 +1,16 @@
+#ifndef _COMMONTYPES_
+#define _COMMONTYPES_
+
+#include <stdio.h>
+#include <malloc.h>
+
+#define PUBLIC
+#define PRIVATE static
+#define ORDER (3) // ORDER must be 2 or more
+#define TRUE (1)
+#define FALSE (0)
+
+typedef void * Object;
+typedef void * Key;
+
+#endif /* _COMMONTYPES_ */
