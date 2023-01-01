d02074b91a6e92c680ce5a19d72232db57734c08
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-01-01 17:52:30 +0100

    Added all functions

--- src/CommonLib/BTree/Pool.c
+++ src/CommonLib/BTree/Pool.c
@@ -3,3 +3,31 @@
 *
 *********************************************************************************/
 #include "Pool.h"
+
+PUBLIC Pool * Pool_new(unsigned int nbMemChunks, unsigned int memChunkSize)
+{
+}
+
+PUBLIC Pool * Pool_newFromFile(Pool * pool, char * fileName)
+{
+   // If file exists
+   // else
+}
+
+PUBLIC void Pool_free(Pool * pool)
+{
+   // If is file
+   // Close file
+   // Else free mem
+}
+
+PUBLIC void * Pool_alloc(Pool * pool, unsigned int size)
+{
+   // Check if free slots left
+   // return nextFree
+   // NextFree = nextFree next
+}
+
+PUBLIC void Pool_dealloc(Pool * pool, void * p)
+{
+}
