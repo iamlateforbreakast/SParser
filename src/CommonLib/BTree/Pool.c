da848df28cd73c63ed36db9c6dd020d419826918
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-01-02 07:24:25 +0000

    Add allocation

--- src/CommonLib/BTree/Pool.c
+++ src/CommonLib/BTree/Pool.c
@@ -4,24 +4,55 @@
 *********************************************************************************/
 #include "Pool.h"
 
-PUBLIC Pool * Pool_new(unsigned int nbMemChunks, unsigned int memChunkSize)
+typedef struct MemChunk
 {
+   unsigned int next;
+   unsigned int prev;
+   unsigned int isFree;
 }
 
-PUBLIC Pool * Pool_newFromFile(Pool * pool, char * fileName)
+PUBLIC Pool * Pool_new(unsigned int nbMemChunks, unsigned int memChunkSize)
 {
+   Pool * newPool = 0;
+   
+   newPool = (Pool *)malloc(sizeof(Pool));
+   newPool->nbMemChunks = nbMemChunks;
+   newPool->memChunkSize = memChunkSize;
+   
+   return newPool;
+}
+
+PUBLIC Pool * Pool_newFromFile(char * fileName,unsigned int nbMemChunks, unsigned int memChunkSize)
+{
+   Pool * newPool = 0;
+   FILE * file;
+   
+   newPool = (Pool *)malloc(sizeof(Pool));
+   newPool->nbMemChunks = nbMemChunks;
+   newPool->memChunkSize = memChunkSize;
    // If file exists
    // else
+   file = fopen(fileName,"b");
+   for (i=0; i<nbMemChunks; i++)
+   {
+      
+   }
+   
+   return newPool;
 }
 
 PUBLIC void Pool_free(Pool * pool)
 {
-   // If is file
-   // Close file
-   // Else free mem
+   if (pool)
+   {
+      // If is file
+      // Close file
+      // Else free mem
+      free(pool);
+   }
 }
 
-PUBLIC void * Pool_alloc(Pool * pool, unsigned int size)
+PUBLIC void * Pool_alloc(Pool * pool)
 {
    // Check if free slots left
    // return nextFree
