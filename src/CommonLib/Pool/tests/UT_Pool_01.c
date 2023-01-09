9c03fb5211e4d823831e8e08ebb0bd5e2059bd4e
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-01-09 18:12:04 +0000

    Update UT_Pool_01.c

--- src/CommonLib/Pool/tests/UT_Pool_01.c
+++ src/CommonLib/Pool/tests/UT_Pool_01.c
@@ -4,20 +4,51 @@
 #define NB_MEM_CHUNKS (4)
 #define SIZE_MEM_CHUNK (4)
 
-int main()
+Pool * testPool = 0;
+
+int step1()
 {
-   Pool * testPool = 0;
-   unsigned int idx = 0;
-   AllocStatus allocStatus = ALLOC_OK;
-   
-   remove("test.pool");
    testPool = Pool_newFromFile("test.pool", NB_MEM_CHUNKS, SIZE_MEM_CHUNK);
    Pool_report(testPool);
+}
+
+int step2()
+{
+   unsigned int idx = 0;
+   AllocStatus allocStatus = ALLOC_OK;
+
    allocStatus = Pool_alloc(testPool, &idx);
    Pool_report(testPool);
-   
+
    Pool_dealloc(testPool, idx);
    Pool_report(testPool);
+}
+
+int step3()
+{
+   unsigned int idx[NB_MEM_CHUNKS];
+   AllocStatus allocStatus = ALLOC_OK;
+
+   for (int i=0; i<NB_MEM_CHUNKS; i++)
+   {
+      allocStatus = Pool_alloc(testPool, &idx[i]);
+   }
+   Pool_report(testPool);
+}
+
+int step4()
+{
+   Pool_free(testPool);
+}
+
+int main()
+{
+   remove("test.pool");
+  
+   step1();
+   step2(); 
+   step3(); 
+   step4(); 
    //idx = Pool_alloc(testPool);
    //Pool_report(testPool);
    //idx = Pool_alloc(testPool);
