1d402f7bbb360d4bd3637a96dc4646ea0d55f655
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-01-12 17:48:26 +0000

    Test functions inMem

--- src/CommonLib/Pool/tests/UT_Pool_01.c
+++ src/CommonLib/Pool/tests/UT_Pool_01.c
@@ -4,74 +4,98 @@
 #define NB_MEM_CHUNKS (4)
 #define SIZE_MEM_CHUNK (4)
 
-Pool * testPool = 0;
+Pool* testPool = 0;
 
 int step1()
 {
-   testPool = Pool_newFromFile("test.pool", NB_MEM_CHUNKS, SIZE_MEM_CHUNK);
-   Pool_report(testPool);
+    testPool = Pool_newFromFile("test.pool", NB_MEM_CHUNKS, SIZE_MEM_CHUNK);
+    Pool_report(testPool);
 }
 
 int step2()
 {
-   unsigned int idx = 0;
-   AllocStatus allocStatus = ALLOC_OK;
+    unsigned int idx = 0;
+    AllocStatus allocStatus = ALLOC_OK;
 
-   allocStatus = Pool_alloc(testPool, &idx);
-   Pool_report(testPool);
+    allocStatus = Pool_alloc(testPool, &idx);
+    Pool_report(testPool);
 
-   Pool_dealloc(testPool, idx);
-   Pool_report(testPool);
+    Pool_dealloc(testPool, idx);
+    Pool_report(testPool);
 }
 
 int step3()
 {
-   unsigned int idx[NB_MEM_CHUNKS];
-   AllocStatus allocStatus = ALLOC_OK;
+    unsigned int idx[NB_MEM_CHUNKS];
+    AllocStatus allocStatus = ALLOC_OK;
 
-   for (int i=0; i<NB_MEM_CHUNKS; i++)
-   {
-      allocStatus = Pool_alloc(testPool, &idx[i]);
-   }
-   Pool_report(testPool);
+    for (int i = 0; i < NB_MEM_CHUNKS; i++)
+    {
+        allocStatus = Pool_alloc(testPool, &idx[i]);
+    }
+    Pool_report(testPool);
 }
 
 int step4()
 {
-   unsigned int idx[] = {2, 0, 3, 1};
+    unsigned int idx[] = { 2, 0, 3, 1 };
 
-   for (int i=0; i<NB_MEM_CHUNKS; i++)
-   {
-      Pool_dealloc(testPool, idx[i]);
-   }
-   Pool_report(testPool);
+    for (int i = 0; i < NB_MEM_CHUNKS; i++)
+    {
+        Pool_dealloc(testPool, idx[i]);
+    }
+    Pool_report(testPool);
 }
 
 int step5()
 {
-   unsigned int idx[NB_MEM_CHUNKS];
-   AllocStatus allocStatus = ALLOC_OK;
+    unsigned int idx[NB_MEM_CHUNKS];
+    AllocStatus allocStatus = ALLOC_OK;
 
-   for (int i=0; i<1; i++)
-   {
-      allocStatus = Pool_alloc(testPool, &idx[i]);
-   }
-   Pool_report(testPool);   
+    for (int i = 0; i < 1; i++)
+    {
+        allocStatus = Pool_alloc(testPool, &idx[i]);
+    }
+    Pool_report(testPool);
 }
 
 int step6()
 {
-   Pool_free(testPool);
+    Pool_free(testPool);
+}
+
+int step11()
+{
+    testPool = Pool_new(NB_MEM_CHUNKS, SIZE_MEM_CHUNK);
+}
+
+int step12()
+{
+    unsigned int idx = 0;
+    AllocStatus allocStatus = ALLOC_OK;
+
+    allocStatus = Pool_alloc(testPool, &idx);
+    Pool_report(testPool);
+
+    Pool_dealloc(testPool, idx);
+    Pool_report(testPool);
+
+    Pool_free(testPool);
+}
+
+int step16()
+{
+
 }
 
 int main()
 {
-   remove("test.pool");
-  
-   step1();
-   step2(); 
-   step3(); 
-   step4(); 
-   step5();
-   step6();
+    remove("test.pool");
+
+    step1();
+    step2();
+    step3();
+    step4();
+    step5();
+    step6();
 }
