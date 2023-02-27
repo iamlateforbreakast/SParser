87ccded181c5e4de5103e96058b4a6b78a17895b
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-02-27 18:36:28 +0000

    Update inline with Node API changes

--- src/CommonLib/BTree/BTree.c
+++ src/CommonLib/BTree/BTree.c
@@ -6,7 +6,7 @@
 #include "Node.h"
 #include "Pool.h"
 
-#define MAX_NODES (10)
+#define MAX_NODES (100)
 
 /*********************************************************************************
 * BTree_new
@@ -18,13 +18,20 @@ BTree * BTree_new(unsigned int order)
 	BTree* tree;
 
 	tree = (BTree*)malloc(sizeof(BTree));
-	tree->pool = Pool_new(MAX_NODES, sizeof(Node));
+	tree->order = order;
+	tree->nodeSize = sizeof(unsigned short int) * 2
+		+ sizeof(unsigned int) * (tree->order * 2 - 1)
+		+ sizeof(Object) * (tree->order * 2)
+		+ sizeof(unsigned int) * (tree->order * 2);
+	//tree->pool = Pool_newFromFile("test.pool", MAX_NODES, tree->nodeSize);
+	tree->pool = Pool_new(MAX_NODES, tree->nodeSize);
 	tree->root = 0;
 	tree->nbNodes = Pool_reportNbNodes(tree->pool);
 	tree->depth = 0;
-	tree->nbObjects = tree->nbNodes;
-	tree->order = order;
+	tree->nbObjects = Pool_reportNbNodes(tree->pool);
 
+
+	printf("Node size in bytes: %d\n", sizeof(Node));
 	return tree;
 }
 
@@ -36,38 +43,71 @@ BTree * BTree_new(unsigned int order)
 *********************************************************************************/
 void BTree_add(BTree* tree, Key key, Object object)
 {
-	if (tree->nbObjects == 0)
+	if (tree->nbObjects == MAX_NODES)
 	{
-		Node newLeaf;
-		unsigned int newLeafIdx = Node_new(TRUE, tree->pool); //Node newLeaf
-		Pool_read(tree->pool, newLeafIdx, &newLeaf);
-		newLeaf.nbKeyUsed = 1;
-		newLeaf.leaves[0] = object;
-		newLeaf.keys[0] = key;
-		Pool_write(tree->pool, newLeafIdx, &newLeaf);
-		tree->depth = 1;
-		tree->root = newLeafIdx;
-		tree->nbObjects = 1;
-		return;
+		 printf("Tree is full\n");
+		 return;
 	}
-	Node root;
-	Pool_read(tree->pool, tree->root, &root);
-	if (root.nbKeyUsed == ORDER * 2 - 1)
-	{ 
-		printf("Splitting root\n");
-		Node newRoot;
-		unsigned int newRootIdx = Node_new(FALSE, tree->pool);  //Node newLeaf
-		Pool_read(tree->pool, newRootIdx, &newRoot);
-		newRoot.children[0] = tree->root;
-		Pool_write(tree->pool, newRootIdx, &newRoot);
-		unsigned int childForInsertionIdx = Node_splitNode(newRootIdx, tree->root, key, tree->pool);
-		Node_insert(childForInsertionIdx, key, object, tree->pool);
-		tree->root = newRootIdx;
-		tree->depth++;
-		tree->nbObjects++;
-		return;
+
+ 	if (tree->nbObjects == 0)
+	{
+		 unsigned int newLeafIdx;
+		 char* node = Pool_alloc(tree->pool, &newLeafIdx);
+		 short unsigned int * nbKeyUsed = node;
+		 short unsigned int * isLeaf = nbKeyUsed + sizeof(short unsigned int);
+		 unsigned int * keys = isLeaf + sizeof(short unsigned int);
+		 Object * leaves = keys + sizeof(unsigned int) * (2 * tree->order - 1);
+		 unsigned int * children = leaves + sizeof(unsigned int) * (2 * tree->order);
+
+		 *nbKeyUsed = 1;
+		 *isLeaf = 1;
+		 keys[0] = key;
+		 //Pool_addToChunkCache(tree->pool, &key, sizeof(key)); //unsigned int keys[ORDER * 2 - 1];
+		 for (int i = 1; i < tree->order * 2 - 1; i++)
+		 {
+			 keys[i] = 0;
+			 //Pool_addToChunkCache(tree->pool, &null, sizeof(null));
+		 }
+		 leaves[0] = object;
+		 //Pool_addToChunkCache(tree->pool, (Object)object, sizeof(object)); //Object leaves[ORDER * 2];
+		 for (int i = 1; i < tree->order * 2; i++)
+		 {
+			 leaves[i] = 0;
+			 //Pool_addToChunkCache(tree->pool, &null, sizeof(null));
+		 }
+		 for (int i = 0; i < tree->order * 2; i++)
+		 {
+			 children[i] = 0;
+			 //Pool_addToChunkCache(tree->pool, &null, sizeof(null)); //unsigned int children[ORDER * 2];
+		 }
+		 Pool_writeCache(tree->pool, newLeafIdx);
+		 tree->depth = 1;
+		 tree->root = newLeafIdx;
+		 tree->nbObjects = 1;
+		 return;
 	}
-  	Node_insert(tree->root, key, object, tree->pool);
+	else
+	{
+		Node root;
+		Pool_read(tree->pool, tree->root, &root);
+		if (root.nbKeyUsed == ORDER * 2 - 1)
+		{
+			//printf("Splitting root\n");
+			Node newRoot;
+			unsigned int newRootIdx = Node_new(tree->pool);  //Node newLeaf
+			Pool_read(tree->pool, newRootIdx, &newRoot);
+			newRoot.children[0] = tree->root;
+			Pool_addToChunkCache(tree->pool, &newRoot.children[0], sizeof(newRoot.children[0]));
+			Pool_writeCache(tree->pool, newRootIdx);
+			unsigned int childForInsertionIdx = Node_splitNode(newRootIdx, tree->root, key, tree->pool);
+			Node_insert(childForInsertionIdx, key, object, tree->order, tree->pool);
+			tree->root = newRootIdx;
+			tree->depth++;
+			tree->nbObjects++;
+			return;
+		}
+	}
+  	Node_insert(tree->root, key, object, tree->order, tree->pool);
 	tree->nbObjects++;
 }
 
@@ -161,7 +201,7 @@ void BTree_print(BTree* tree)
 	if (tree->depth > 0)
 	{
 		printf("Root 0-0:\n");
-		Node_print(tree->root, tree->depth - 1, tree->pool);
+		Node_print(tree->root, tree->order, tree->depth - 1, tree->pool);
 	}
 	    
 }
@@ -190,4 +230,13 @@ PUBLIC void BTree_free(BTree* tree)
 
 	free(tree);
 }
-	
+
+/*********************************************************************************
+* BTree_reportSize
+* input: None
+* output: None
+*********************************************************************************/
+PUBLIC unsigned int BTree_reportSizeInBytes(BTree * tree)
+{
+	return Pool_reportSizeInBytes(tree->pool);
+}
