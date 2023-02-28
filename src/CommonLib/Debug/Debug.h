84cc6b9b7f929c6623ddefafff770e34e1afc8e3
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2023-02-28 20:15:49 +0000

    Merge main

--- src/CommonLib/Debug/Debug.h
+++ src/CommonLib/Debug/Debug.h
@@ -4,4 +4,6 @@
 
 #define TRACE(x) do { if (DEBUG) dbg_printf x; } while (0)
 
+#define PRINT(x) do { dbg_printf x; } while (0)
+
 void dbg_printf(const char *fmt, ...);
--- src/CommonLib/Error/Error.c
+++ src/CommonLib/Error/Error.c
@@ -1,8 +1,27 @@
+/**********************************************//** 
+  \file Error.c
+     
+  \brief Reports errors
+        
+  This file contains error reporting functions.
+**************************************************/
 #include "Error.h"
+#include "Debug.h"
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DEBUG (1)
+
+/**********************************************//** 
+  \brief Reports errors
+  \param severity  Enum
+  \param msg Variable list of parameters
+  \public
+
+  This function reports errors using different
+  formatting according to severity.
+**************************************************/
 PUBLIC void Error_new(ErrorSeverity severity, char * msg, ...)
 {
   va_list args;
@@ -11,25 +30,25 @@ PUBLIC void Error_new(ErrorSeverity severity, char * msg, ...)
   {
     case ERROR_DBG:
 	va_start(args, msg);
-	printf("[DBUG] ");
+	TRACE(("[DBUG] "));
 	vprintf(msg, args);
 	va_end(args);
 	break;
     case ERROR_INFO:
 	va_start(args, msg);
-	printf("[INFO] ");
+	PRINT(("[INFO] "));
 	vprintf(msg, args);
 	va_end(args);
 	break;
     case ERROR_NORMAL:
 	va_start(args, msg);
-	printf("[NORM] ");
+	PRINT(("[NORM] "));
 	vprintf(msg, args);
 	va_end(args);
 	break;
     case ERROR_FATAL:
 	va_start(args, msg);
-	printf("[EXIT] ");
+	PRINT(("[EXIT] "));
 	vprintf(msg, args);
 	va_end(args);
 	exit(2);
