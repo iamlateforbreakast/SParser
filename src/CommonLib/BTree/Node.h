e533c6c30d1031d6f1bf6cb629f45bec897805d9
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-03-01 21:18:27 +0000

    Add size API

--- src/CommonLib/BTree/Node.h
+++ src/CommonLib/BTree/Node.h
@@ -7,8 +7,6 @@
 #include "Types.h"
 #include "Pool.h"
 
-#define ORDER (3) // ORDER must be 2 or more
-
 typedef unsigned int Key;
 
 typedef struct Node Node;
@@ -17,9 +15,9 @@ typedef struct Node
 {
 	unsigned short int nbKeyUsed;
 	unsigned short int isLeaf;
-	unsigned int keys[ORDER * 2 - 1];
-	void * leaves[ORDER * 2];
-	unsigned int children[ORDER * 2];	
+	unsigned int * keys;
+	void ** leaves;
+	unsigned int * children;	
 } Node;
 
 PUBLIC unsigned int Node_new(Pool* pool);
@@ -29,5 +27,7 @@ PUBLIC void ** Node_remove(unsigned int nodeIdx, Key key, unsigned int* keyToUpd
 PUBLIC void Node_search(unsigned int nodeIdx, Key key, void ** object, unsigned int isFoundAlready, Pool* pool);
 PUBLIC void Node_free(unsigned int nodeIdx, Pool* pool);
 PUBLIC void Node_print(unsigned int nodeIdx, unsigned int order, unsigned int depth, Pool* pool);
+PUBLIC unsigned int Node_getSize(unsigned int order);
+
 
 #endif /* _NODE_ */
