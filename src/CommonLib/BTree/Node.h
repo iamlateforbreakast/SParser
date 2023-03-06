fffaa23101c7b703c63b878b044664f3402cacdf
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-03-06 19:12:59 +0000

    Update Node.h

--- src/CommonLib/BTree/Node.h
+++ src/CommonLib/BTree/Node.h
@@ -23,9 +23,9 @@ typedef struct Node
 
 PUBLIC unsigned int Node_new(Pool* pool);
 PUBLIC unsigned int Node_splitNode(unsigned int nodeIdx, unsigned int nodeToSplitIdx, Key key, unsigned int order, Pool* pool);
-PUBLIC void Node_insert(unsigned int nodeIdx, Key key, Object object, unsigned int order, Pool* pool);
-PUBLIC Object Node_remove(unsigned int nodeIdx, Key key, unsigned int* keyToUpdate, Pool* pool);
-PUBLIC void Node_search(unsigned int nodeIdx, Key key, Object object, unsigned int isFoundAlready, Pool* pool);
+PUBLIC void Node_insert(unsigned int nodeIdx, Key key, void * object, unsigned int order, Pool* pool);
+PUBLIC void * Node_remove(unsigned int nodeIdx, Key key, unsigned int* keyToUpdate, Pool* pool);
+PUBLIC void Node_search(unsigned int nodeIdx, Key key, void * object, unsigned int isFoundAlready, Pool* pool);
 PUBLIC void Node_free(unsigned int nodeIdx, Pool* pool);
 PUBLIC void Node_print(unsigned int nodeIdx, unsigned int order, unsigned int depth, Pool* pool);
 PUBLIC unsigned int Node_getSize(unsigned int order);
