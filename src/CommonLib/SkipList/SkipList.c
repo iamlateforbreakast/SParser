19c4fa52dfe015cc48912f0143b1bc7f65b54949
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-03-15 17:50:50 +0000

    Initial revision

---
+++ src/CommonLib/SkipList/SkipList.c
@@ -0,0 +1,12 @@
+/**********************************************//**
+  @file SkipList.c
+
+  @brief This file contains the implementation of the class SkipList.
+
+  The class List implement the SkipList operations
+  - Add
+  - Remove
+  - Get
+**************************************************/
+
+#include "SkipList.h"
