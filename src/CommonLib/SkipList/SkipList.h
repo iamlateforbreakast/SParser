5495f4f7f24d36fb8048520d3c326e02b64ea8ab
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-03-15 18:14:58 +0000

    Add API

--- src/CommonLib/SkipList/SkipList.h
+++ src/CommonLib/SkipList/SkipList.h
@@ -1,1 +1,24 @@
+#ifndef _SKIPLIST_
+#define _SKIPLIST_
 /* SkipList.h */
+#include "Pool.h"
+
+typedef struct SkipNode
+{  
+   unsigned int level;
+   unsigned int nbObjects;
+   unsigned int key;
+   void * object;
+   SkipNode * header;
+
+typedef struct SkipList
+{
+   Pool * pool;  
+} SkipList;
+
+SkipList * SkipList_new();
+void SkipList_free(SkipList * skipList):
+void SkipList_add(SkipList * this, void * object);
+void * SkipList_delete(SkipList * this, unsigned int key);
+void * SkipList_get(SkipList * this, unsigned int key);
+#end /* _SKIPLIST_ */
