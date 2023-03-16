5718ac1d5f9d21b726d22154a8eb3e940880fdfe
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-03-16 17:41:35 +0000

    Add implementation

--- src/CommonLib/SkipList/tests/UT_SkipList_01.c
+++ src/CommonLib/SkipList/tests/UT_SkipList_01.c
@@ -1,1 +1,49 @@
+// SkipList.c : This file contains the 'main' function. Program execution begins and ends there.
+//
 
+#include <stdio.h>
+#include "SkipList.h"
+
+#define NB_OBJECTS (3)
+
+SkipList* testList;
+
+int step1()
+{
+    testList = SkipList_new();
+
+    return 1;
+}
+
+int step2()
+{
+    unsigned int keys[NB_OBJECTS] = { 55, 30, 80 };
+    unsigned int value[NB_OBJECTS] = { 1972, 2005, 1945 };
+
+    for (int i = 0; i < NB_OBJECTS; i++)
+        SkipList_add(testList, keys[i], &value);
+
+    return 1;
+}
+
+int step3()
+{
+    
+    SkipList_print(testList);
+
+    return 1;
+}
+
+int step4()
+{
+    SkipList_free(testList);
+    return 1;
+}
+
+int main()
+{
+    step1();
+    step2();
+    step3();
+    step4();
+}
