34f6d444b0f38ebd3a0fc798023d2c0ed56150c5
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-03-16 17:43:26 +0000

    Implement more functions

--- src/CommonLib/SkipList/SkipList.c
+++ src/CommonLib/SkipList/SkipList.c
@@ -1,8 +1,6 @@
 /**********************************************//**
   @file SkipList.c
-
   @brief This file contains the implementation of the class SkipList.
-
   The class List implement the SkipList operations
   - Add
   - Remove
@@ -10,50 +8,58 @@
 **************************************************/
 
 #include "SkipList.h"
+#include "Pool.h"
 #include <limits.h>
+#include <stdlib.h>
 
 
 #define MAX_OBJECTS (10)
-#define MAX_LEVEL (6)
+#define SKIPLIST_MAX_LEVEL (6)
 
 typedef struct SkipNode SkipNode;
 
 typedef struct SkipNode
-{  
+{
     unsigned int key;
-    void * object;
-    unsigned int forward[MAX_LEVEL];
+    void* object;
+    unsigned int level;
+    unsigned int forward[SKIPLIST_MAX_LEVEL];
 } SkipNode;
 
 typedef struct SkipList
 {
-   unsigned int level;
-   unsigned int nbObjects;
-   Pool * pool;
-   unsigned int headerIdx;
+    unsigned int level;
+    unsigned int nbObjects;
+    Pool* pool;
+    unsigned int headerIdx;
 } SkipList;
+
+PRIVATE unsigned int SkipList_randLevel(SkipList* this);
 
 /**********************************************//**
   @brief SkipList_new
   @param[in] none
   @return New instance of class SkipList.
 **************************************************/
-SkipList * SkipList_new()
+PUBLIC SkipList* SkipList_new()
 {
-   SkipList * newSkipList = 0;
-   
-   newSkipList = (SkipList*)malloc(sizeof(SkipList));
-   newSkipList->pool = Pool_new(MAX_OBJECTS, sizeof(SkipNode));
-   newSkipList->level = 1;
-   newSkipList->nbObjects = 0;
-   SkipNode * skipNode = Pool_alloc(newSkipList->pool, &newSkipList->headerIdx);
-   for (int i=0; i<MAX_LEVEL; i++)
-   {
-      skipNode->forward[i] = newSkipList->headerIdx;
-   }
-   skipNode->key = INT_MAX;
+    SkipList* newSkipList = 0;
 
-   return newSkipList;
+    newSkipList = (SkipList*)malloc(sizeof(SkipList));
+    newSkipList->pool = Pool_new(MAX_OBJECTS, sizeof(SkipNode));
+    newSkipList->level = 1;
+    newSkipList->nbObjects = 0;
+    SkipNode* skipNode = Pool_alloc(newSkipList->pool, &newSkipList->headerIdx);
+    for (int i = 0; i < SKIPLIST_MAX_LEVEL; i++)
+    {
+        skipNode->forward[i] = newSkipList->headerIdx;
+    }
+    skipNode->key = INT_MAX;
+    skipNode->object = 0;
+    skipNode->level = 1;
+    Pool_write(newSkipList->pool, newSkipList->headerIdx, skipNode);
+
+    return newSkipList;
 }
 
 /**********************************************//**
@@ -61,10 +67,10 @@ SkipList * SkipList_new()
   @param[in] Instance to destroy
   @return None
 **************************************************/
-void SkipList_free(SkipList * this)
+PUBLIC void SkipList_free(SkipList* this)
 {
-  Pool_free(this->pool);
-  free(this);
+    Pool_free(this->pool);
+    free(this);
 }
 
 /**********************************************//**
@@ -73,21 +79,162 @@ void SkipList_free(SkipList * this)
   @param[in] Object to add to SkipList object.
   @return None
 **************************************************/
-void SkipList_add(SkipList * this, void * object)
-{
-   SkipNode * snode = Pool_read(this->pool, this->headerIdx);
-   for (int i=this->level;i=1;i--)
-   {
-      snode->forward[i];
-   }
+PUBLIC void SkipList_add(SkipList* this, unsigned int key, void* object)
+ {
+    unsigned int update[SKIPLIST_MAX_LEVEL + 1];
+    unsigned int iteratorIdx = this->headerIdx;
+    unsigned int prevNodeIdx = this->headerIdx;
+    SkipNode* prevNode = Pool_read(this->pool, this->headerIdx);
+    SkipNode* currentNode = Pool_read(this->pool, this->headerIdx);
+
+    for (int i = this->level; i == 1; i--)
+    {
+        while (currentNode->key < key)
+        {
+            prevNode = currentNode;
+            currentNode = Pool_read(this->pool, currentNode->forward[i]);   
+        }
+        update[i] = prevNode->forward[i];
+    }
+    prevNode = Pool_read(this->pool, prevNode->forward[1]);
+    prevNodeIdx = prevNode->forward[1];
+
+    if (key == currentNode->key)
+    {
+        currentNode->object = object;
+        return 0;
+    }
+    else
+    {
+        unsigned int level;
+        level = SkipList_randLevel(this);
+        if (level > this->level)
+        {
+            for (int i = this->level + 1; i <= level; i++)
+            {
+                update[i] = this->headerIdx;
+            }
+            this->level = level;
+        }
+        unsigned int insertIdx;
+        SkipNode* insert = Pool_alloc(this->pool, &insertIdx);
+        insert->key = key;
+        insert->object = object;
+        insert->level = level;
+        for (int i = 1; i <= level; i++)
+        {
+            insert->forward[i] = update[i];
+            if (prevNode == currentNode) this->headerIdx = insertIdx;
+            else prevNode->forward[i] = insertIdx;
+        }
+        Pool_write(this->pool, prevNodeIdx, prevNode);
+        Pool_write(this->pool, insertIdx, insert);
+        this->nbObjects++;
+
+        return 0;
+    }
 }
 
+//int skiplist_insert(skiplist* list, int key, int value) {
+//    snode* update[SKIPLIST_MAX_LEVEL + 1];
+//    snode* x = list->header;
+//    int i, level;
+//    for (i = list->level; i >= 1; i--) {
+//        while (x->forward[i]->key < key)
+//            x = x->forward[i];
+//        update[i] = x;
+//    }
+//    x = x->forward[1];
+//
+//    if (key == x->key) {
+//        x->value = value;
+//        return 0;
+//    }
+//    else {
+//        level = rand_level();
+//        if (level > list->level) {
+//            for (i = list->level + 1; i <= level; i++) {
+//                update[i] = list->header;
+//            }
+//            list->level = level;
+//        }
+//
+//        x = (snode*)malloc(sizeof(snode));
+//        x->key = key;
+//        x->value = value;
+//        x->forward = (snode**)malloc(sizeof(snode*) * (level + 1));
+//        for (i = 1; i <= level; i++) {
+//            x->forward[i] = update[i]->forward[i];
+//            update[i]->forward[i] = x;
+//        }
+//    }
+//    return 0;
+//}
 /**********************************************//**
   @brief SkipList_delete
   @param[in] Key of object to remove
   @return Object removed from SkipList object.
 **************************************************/
-void * SkipList_delete(SkipList * this, unsigned int key)
+PUBLIC void* SkipList_delete(SkipList* this, unsigned int key)
 {
-   return 0;
+    return 0;
+}
+
+//int skiplist_delete(skiplist* list, int key) {
+//    int i;
+//    snode* update[SKIPLIST_MAX_LEVEL + 1];
+//    snode* x = list->header;
+//    for (i = list->level; i >= 1; i--) {
+//        while (x->forward[i]->key < key)
+//            x = x->forward[i];
+//        update[i] = x;
+//    }
+//
+//    x = x->forward[1];
+//    if (x->key == key) {
+//        for (i = 1; i <= list->level; i++) {
+//            if (update[i]->forward[i] != x)
+//                break;
+//            update[i]->forward[1] = x->forward[i];
+//        }
+//        skiplist_node_free(x);
+//
+//        while (list->level > 1 && list->header->forward[list->level]
+//            == list->header)
+//            list->level--;
+//        return 0;
+//    }
+//    return 1;
+//}
+
+/**********************************************//**
+  @brief SkipList_print
+  @param[in] None
+  @return None
+**************************************************/
+PUBLIC void SkipList_print(SkipList* this)
+{
+    printf("NbObject: %d\n", this->nbObjects);
+    printf("Level: %d\n", this->level);
+
+    SkipNode* skipNode = Pool_read(this->pool, this->headerIdx);
+    for (int i = 0; i < this->nbObjects; i++)
+    {
+        printf("SkipNode: %d ", i);
+        printf(" Level: %d ", skipNode->level);
+        printf(" Key: %d ", skipNode->key);
+        for (int j = 1; j < skipNode->level + 1; j++)
+        {
+            printf(" Forward[%d]: %d", j, skipNode->forward[i]);
+        }
+        printf("\n");
+    }
+}
+
+PRIVATE unsigned int SkipList_randLevel(SkipList* this)
+{
+    int level = 1;
+    while (rand() < RAND_MAX / 2 && level < SKIPLIST_MAX_LEVEL)
+        level++;
+    return level;
 }
