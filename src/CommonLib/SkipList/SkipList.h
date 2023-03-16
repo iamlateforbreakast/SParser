30ff4bab12523835f8ce0c763aa0d10b577bf0da
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-03-16 17:44:09 +0000

    Add print()

--- src/CommonLib/SkipList/SkipList.h
+++ src/CommonLib/SkipList/SkipList.h
@@ -1,13 +1,15 @@
 #ifndef _SKIPLIST_
 #define _SKIPLIST_
 /* SkipList.h */
+#include "Types.h"
 #include "Pool.h"
 
 typedef struct SkipList SkipList;
 
-SkipList * SkipList_new();
-void SkipList_free(SkipList * skipList);
-void SkipList_add(SkipList * this, void * object);
-void * SkipList_delete(SkipList * this, unsigned int key);
-void * SkipList_get(SkipList * this, unsigned int key);
+PUBLIC SkipList* SkipList_new();
+PUBLIC void SkipList_free(SkipList* skipList);
+PUBLIC void SkipList_add(SkipList* this, unsigned int key, void* object);
+PUBLIC void * SkipList_delete(SkipList* this, unsigned int key);
+PUBLIC void * SkipList_get(SkipList* this, unsigned int key);
+PUBLIC void SkipList_print(SkipList* this);
 #endif  /* _SKIPLIST_ */
