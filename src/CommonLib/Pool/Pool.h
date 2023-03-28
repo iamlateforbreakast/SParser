f79b76503d07ab5b8d086e01afe3854f3b4e9a84
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-03-28 20:40:40 +0100

    Merge SP6

--- src/CommonLib/Pool/Pool.h
+++ src/CommonLib/Pool/Pool.h
@@ -5,14 +5,20 @@
 *
 *********************************************************************************/
 #include "Types.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include "Pool.h"
 
 typedef enum AllocStatus
 {
     ALLOC_OK = 0,
     ALLOC_FAIL = 1
 } AllocStatus;
+
+typedef struct PoolCache
+{
+    unsigned int idx;
+    unsigned int isUsed;
+    void* cache;
+} PoolCache;
 
 typedef struct Pool Pool;
 
@@ -22,11 +28,12 @@ PUBLIC void Pool_free(Pool* pool);
 PUBLIC void * Pool_alloc(Pool* pool, unsigned int * ptrIdx);
 PUBLIC void Pool_dealloc(Pool* pool, unsigned int p);
 PUBLIC void Pool_write(Pool* pool, unsigned int idx, void* ptrContent);
-PUBLIC void* Pool_read(Pool* pool, unsigned int idx, void* ptrContent);
+PUBLIC void* Pool_read(Pool* pool, unsigned int idx);
 PUBLIC unsigned int Pool_addToChunkCache(Pool* pool, void* p, unsigned int length);
 PUBLIC void Pool_report(Pool* pool);
 PUBLIC unsigned int Pool_reportSizeInBytes(Pool* pool);
 PUBLIC unsigned int Pool_reportNbNodes(Pool* pool);
-PUBLIC void* Pool_getCache1(Pool* pool);
-PUBLIC void* Pool_getCache2(Pool* pool);
-#endif
+PUBLIC void Pool_discardCache(Pool* pool, unsigned int idx);
+PUBLIC void Pool_discardAllCache(Pool* pool);
+PUBLIC unsigned int Pool_reportCacheUsed(Pool * pool);
+#endif /* _POOL_ */
