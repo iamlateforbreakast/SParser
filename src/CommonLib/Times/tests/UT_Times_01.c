7d15209646b109836bb2cc1cb382d6c0d654dd54
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-04-18 14:51:28 +0100

    Merge from main

---
+++ src/CommonLib/Times/tests/UT_Times_01.c
@@ -0,0 +1,35 @@
+#include <time.h>
+#include <errno.h>
+#include "Times.h"
+
+#define MAX_LOOP (10)
+
+int msleep(long msec)
+{
+    struct timespec ts;
+    int res;
+
+    if (msec < 0)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+
+    ts.tv_sec = msec / 1000;
+    ts.tv_nsec = (msec % 1000) * 1000000;
+
+    do {
+        res = nanosleep(&ts, &ts);
+    } while (res && errno == EINTR);
+
+    return res;
+}
+
+int step1()
+{
+  for (int i=0; i<MAX_LOOP; i++)
+  {
+    printf("Get Wall Time = %Lf\n", get_wall_time());
+    msleep(500);
+  }
+}
