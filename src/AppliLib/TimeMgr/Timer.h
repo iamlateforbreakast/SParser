8eab360606c17490126450e0256e2fca713ab73f
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-04-19 13:24:37 +0100

    Print/latchTime API changes

--- src/AppliLib/TimeMgr/Timer.h
+++ src/AppliLib/TimeMgr/Timer.h
@@ -8,5 +8,5 @@ PUBLIC Timer * Timer_new();
 PUBLIC void Timer_delete(Timer * this);
 PUBLIC Timer * Timer_copy(Timer * this);
 PUBLIC unsigned int Timer_isEqual(Timer * this, Timer * compared);
-PUBLIC char * Timer_print(Timer * this);
-PUBLIC void Timer_latchTime(Timer * this, double t);
+PUBLIC void Timer_print(Timer * this);
+PUBLIC void Timer_latchTime(Timer * this);
