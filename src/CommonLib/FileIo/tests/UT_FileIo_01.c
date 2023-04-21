d983763bec69e576bfcb8e60febb2590676acc52
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-04-21 19:03:06 +0100

    Correct ListFile step6()

--- src/CommonLib/FileIo/tests/UT_FileIo_01.c
+++ src/CommonLib/FileIo/tests/UT_FileIo_01.c
@@ -1,63 +1,77 @@
 #include "FileIo.h"
+#include "String2.h"
+#include "List.h"
 
-#define BUFFER_SIZE (1000)
+#define BUFFER_SIZE_BYTES (1000)
 
-FileIo * f = 0;
-FileIo * d = 0;
+FileIo* f = 0;
+FileIo* d = 0;
 const char* testFileName[5] = { "test1.file","test2.file","test3.file","test4.file","test5.file" };
-char testBuffer[BUFFER_SIZE];
-char readBuffer[BUFFER_SIZE];
+char testBuffer[BUFFER_SIZE_BYTES];
+char readBuffer[BUFFER_SIZE_BYTES];
 
 int step1()
 {
-  remove(testFileName);
-  f = FileIo_new(testFileName[0]);
-} 
+	f = FileIo_new();
+	FileIo_remove(f, testFileName);
+
+	return 1;
+}
 
 int step2()
 {
-  String * testFileName = String_new("test.file");
-  FileIo_createFile(f, testFileName);
-  FileIo_write(f, &testBuffer, BUFFER_SIZE);
-  FileIo_delete(f);
+	String* testFileName = String_new("test.file");
+	FileIo_createFile(f, testFileName);
+	FileIo_write(f, &testBuffer, BUFFER_SIZE_BYTES);
+	FileIo_delete(f);
+
+	return 1;
 }
 
 int step3()
 {
-  int isOK = 1;
-  String * testFileName = String_new("test.file");
-  f = FileIo_new();
-  FileIo_openFile(f, testFileName);
-  FileIo_read(f, &readBuffer, BUFFER_SIZE);
-  for (int i = 0; i < 10; i++)
-  {
-	  isOK = isOK && (testBuffer[i] == readBuffer[i]);
-	  printf("%d %d\n", testBuffer[i], readBuffer[i]);
-  }
-  printf("%d\n", isOK);
-  FileIo_delete(f);
+	int isOK = 1;
+	String* testFileName = String_new("test.file");
+	f = FileIo_new();
+	FileIo_openFile(f, testFileName);
+	FileIo_read(f, &readBuffer, BUFFER_SIZE_BYTES);
+	for (int i = 0; i < 10; i++)
+	{
+		isOK = isOK && (testBuffer[i] == readBuffer[i]);
+		printf("%d %d\n", testBuffer[i], readBuffer[i]);
+	}
+	printf("%d\n", isOK);
+	FileIo_delete(f);
+
+	return 1;
 }
 
 int step4()
 {
-  f	= FileIo_new(testFileName);
-  FileIo_remove(testFileName);
+	f = FileIo_new(testFileName);
+	FileIo_remove(testFileName);
+
+	return 1;
 }
 
 int step5()
 {
 	//d = FileIo_newDir("testDir");
+
+	return 1;
 }
 
 int step6()
 {
 	FileIo* f = 0;
-	FileIo_listDir(f);
+	List* listOfFiles = FileIo_listDir(f);
+	
+	return 1;
 }
 
 void main()
 {
-	for (int i = 0; i < BUFFER_SIZE; i++)
+	for (int i = 0; i < BUFFER_SIZE_BYTES; i++)
 	{
 		testBuffer[i] = (char)i;
 	}
