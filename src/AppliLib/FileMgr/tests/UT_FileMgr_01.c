37aa1a5445e6e2e554f4263d0586abf0cee5254f
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2023-05-01 09:15:44 +0100

    Add tests

--- src/AppliLib/FileMgr/tests/UT_FileMgr_01.c
+++ src/AppliLib/FileMgr/tests/UT_FileMgr_01.c
@@ -1,6 +1,7 @@
 #include "FileMgr.h"
 #include "Object.h"
 #include "List.h"
+#include "FileIo.h"
 #include "Memory.h"
 
 #include <stdio.h>
@@ -12,25 +13,54 @@ typedef struct TestFileMgr
   Object object;
   List * files;
   List * directories;
-  char rootLocation[FILEMGR_MAX_PATH];
+  char * separator;
+  String * rootLocation;
 } TestFileMgr;
 
 int step1()
 {
-  FileMgr * testFileMgr = 0;
+  FileMgr * testFileMgr1 = 0;
+  FileMgr * testFileMgr2 = 0;
+  testFileMgr1 = FileMgr_getRef();
+  testFileMgr2 = FileMgr_getRef();
+  FileIo * f = FileIo_new();
+  String * currentLocation = FileIo_getCwd(f);
+  
+  printf("Step 1: Test 1 - Check there is only one FileMgr: ");
+  if (testFileMgr1!=testFileMgr2) 
+  {
+    printf("Failed\n");
+    return 0;
+  }
+  else
+  {
+    printf("OK\n");
+  }
+  printf("Step 1: Test 2 - Check the root location is correct: ");
+  if (String_compare(((TestFileMgr*)testFileMgr1)->rootLocation, currentLocation)!=0)
+  {
+    printf("Failed\n");
+    return 0;
+  }
+  else
+  {
+    printf("OK\n");
+  }
 
-  testFileMgr = FileMgr_getRef();
-  printf("Root location: %s\n", (((TestFileMgr*)testFileMgr)->rootLocation));
-
+  printf("Root location: %s\n", (((TestFileMgr*)testFileMgr1)->rootLocation));
+  // PUBLIC unsigned int FileMgr_setRootLocation(FileMgr* this, const char * location);
   //FileMgr_addDirectory(testFileMgr, "../../../../../../Solo/AUK40803_tr_solo_dev/vobs/solo/fsw");
-  FileMgr_addDirectory(testFileMgr, "..");
+  //FileMgr_addDirectory(testFileMgr, "..");
   //FileMgr_addDirectory(testFileMgr, "../../..");
 
-  FileMgr_delete(testFileMgr);
+  FileIo_delete(f);
+  String_delete(currentLocation);
+  FileMgr_delete(testFileMgr1);
+  FileMgr_delete(testFileMgr1);
 
   Memory_report();
 
-  return 0;
+  return 1;
 }
 
 int step2()
@@ -74,7 +104,7 @@ int step3()
 
 int step4()
 {
-// PUBLIC unsigned int FileMgr_setRootLocation(FileMgr* this, const char * location);
+
 // PUBLIC char * FileMgr_getRootLocation(FileMgr* this);
 }
 
@@ -87,7 +117,7 @@ int step5()
 int main()
 {
   step1();
-  step2();
+  //step2();
   //step3();
   //step4();
   //step5();
