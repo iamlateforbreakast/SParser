a17142464b58e49f6c13c487bc74420523f68ced
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-05-02 17:51:46 +0100

    Add reportUnallocated

--- src/CommonLib/ObjectMgr/ObjectMgr.h
+++ src/CommonLib/ObjectMgr/ObjectMgr.h
@@ -13,6 +13,7 @@ PUBLIC ObjectMgr * ObjectMgr_copy(ObjectMgr * this);
 PUBLIC ObjectMgr * ObjectMgr_getRef();
 PUBLIC Object * ObjectMgr_allocate(ObjectMgr * this, unsigned int size);
 PUBLIC void ObjectMgr_deallocate(ObjectMgr * this, Object * object);
+PUBLIC void ObjectMgr_reportUnallocated(ObjectMgr* this);
 PUBLIC void ObjectMgr_report(ObjectMgr * this);
 
 #endif /* _OBJECTMGR_H_ */
