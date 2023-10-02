e671689739f47f5e778a85b3c0cbfabf7e3bbdbe
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-10-02 14:52:49 +0100

    Initial design

--- src/CommonLib/Array/Array.h
+++ src/CommonLib/Array/Array.h
@@ -1,14 +1,26 @@
 #ifndef _ARRAY_H_
 #define _ARRAY_H_
 /*********************************************************************************
-* BTree.h
+* Array.h
 *
 *********************************************************************************/
 #include "Types.h"
+#include "Object.h"
 
-typedef struct Array
+typedef struct Array Array;
+
+typedef struct ArrayParam
 {
-} Array;
+  unsigned int defaultSize;
+  unsigned int storageMode;
+  unsinged int autoresize;
+} ArrayParam;
 
-
+PUBLIC Array * Array_new(ArrayParam * param);
+PUBLIC void Array_delete(Array* this);
+PUBLIC List * Array_copy(Array* this);
+PUBLIC int Array_compare(Array * this, Array * compared);
+PUBLIC void Array_print(Array * this);
+PUBLIC void Array_put(Array * this, unsigned int index);
+PUBLIC Object * Array_get(Array * this, unsigned int index);
 #endif /* _ARRAY_H_ */
