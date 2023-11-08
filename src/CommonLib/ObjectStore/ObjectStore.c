4d21b70603ba6e9f3ef4170920cd08d39b4e2861
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2023-11-08 20:43:44 +0000

    Add constructor

--- src/CommonLib/ObjectStore/ObjectStore.c
+++ src/CommonLib/ObjectStore/ObjectStore.c
@@ -15,12 +15,25 @@ struct ObjectStore
   PoolInfo * poolList;
 };
 
+PRIVATE ObjectStore * objectStore = 0;
+PUBLIC Pool * mallocPool = 0;
+
 PUBLIC void ObjectStore_delete(ObjectStore * this)
 {
 }
 
 PUBLIC ObjectStore * ObjectStore_getRef()
 {
+    if (objectStore==0)
+  {
+    objectStore = ObjectStore_new();
+  }
+  else
+  {
+    objectStore->object.refCount++;
+  }
+  
+  return objectMgr;
 }
 
 PUBLIC Pool * ObjectStore_createPool(ObjectStore * this)
@@ -31,4 +44,12 @@ PUBLIC void ObjectStore_deletePool(ObjectStore * this, Pool * pool)
 {
 }
 
+PRIVATE ObjectStore * ObjectStore_new()
+{
+  ObjectStore * objectStore;
+  // Create Malloc Pool
+  //mallocPool = Pool_new(MALLOC_TYPE);
+  // 
+  return objectStore;
+}
 #endif /* _OBJECTSTORE_H_ */
