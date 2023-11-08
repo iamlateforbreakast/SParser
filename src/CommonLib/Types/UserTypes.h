ee04f0064b76ff0c057dc3bcd1ca2e5d8ae626cf
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2023-11-08 20:20:32 +0000

    Update definition

--- src/CommonLib/Types/UserTypes.h
+++ src/CommonLib/Types/UserTypes.h
@@ -2,28 +2,19 @@
 #ifndef _USERTYPES_
 #define _USERTYPES_
 #include "Class.h"
+extern Class listClass;
+extern Class stringClass;
 
-enum {
-  List,
-  BTree,
-  Nb_types
+enum ClassId
+{
+  ListClass,
+  StringClass,
+  NB_CLASSES
 };
 
-Class userTypes[Nb_types-1];
-
-Class userTypes[List] = {
-  .f_new = 0,
-  .f_delete = (Destructor)&List_delete,
-  .f_copy = (Copy_Operator)&List_copy,
-  .f_comp = (Comp_Operator)&List_compare,
-  .f_print = (Printer)&List_print
-};
-
-Class usertypes[BTree] = {
-  .f_new = 0,
-  .f_delete = (Destructor)&BTree_delete,
-  .f_copy = (Copy_Operator)&BTree_copy,
-  .f_comp = (Comp_Operator)&BTree_compare,
-  .f_print = (Printer)&BTree_print
-};
-
+/*Class * userTypes[] =
+{
+  &listClass,
+  &stringClass,
+};*/
+#endif /* UserTypes.h */
