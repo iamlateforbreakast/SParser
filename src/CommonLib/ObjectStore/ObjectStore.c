b3d996b253098c0032edd4324439fa27e580291e
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2023-11-12 22:09:25 +0000

    Update

--- src/CommonLib/ObjectStore/ObjectStore.c
+++ src/CommonLib/ObjectStore/ObjectStore.c
@@ -16,6 +16,7 @@ struct ObjectStore
 };
 
 PRIVATE ObjectStore * objectStore = 0;
+
 PUBLIC Pool * mallocPool = 0;
 
 PUBLIC void ObjectStore_delete(ObjectStore * this)
@@ -44,12 +45,30 @@ PUBLIC void ObjectStore_deletePool(ObjectStore * this, Pool * pool)
 {
 }
 
+PUBLIC Object ObjectStore_createObject(ObjectStore * this, Class * class)
+{
+  Object object;
+  
+  // object = Allocator_allocate(malloc, Class_getSize(class));
+
+  return object;
+}
+
+PUBLIC void ObjectStore_deleteObject(ObjectStore * this, Object object)
+{
+  // Allocator_deallocate(object->allocator, object);
+}
+
 PRIVATE ObjectStore * ObjectStore_new()
 {
   ObjectStore * objectStore;
   // Create Malloc Pool
   //mallocPool = Pool_new(MALLOC_TYPE);
-  // 
+  //objecStore = Pool_allocate(mallocPool,sizeof(ObjectStore));
+  //objectStore->poolList = Pool_allocate(mallocPool,sizeof(PoolInfo));
+  //objectStore->poolList->ptr = mallocPool;
+  //objectStore->poolList->nextPool = 0;
+  //objectStore->poolList->prevPoool = 0; 
   return objectStore;
 }
 #endif /* _OBJECTSTORE_H_ */
--- src/CommonLib/ObjectStore/ObjectStore.h
+++ src/CommonLib/ObjectStore/ObjectStore.h
@@ -4,6 +4,7 @@
 #define _OBJECTSTORE_H_
 
 #include "Types.h"
+#include "Object.h"
 #include "Pool.h"
 
 typedef struct ObjectStore ObjectStore;
@@ -12,6 +13,8 @@ PUBLIC void ObjectStore_delete(ObjectStore * this);
 PUBLIC ObjectStore * ObjectStore_getRef();
 PUBLIC Pool * ObjectStore_createPool(ObjectStore * this);
 PUBLIC void ObjectStore_deletePool(ObjectStore * this, Pool * pool);
+PUBLIC Object ObjectStore_createObject(ObjectStore * this, Class * class);
+PUBLIC void ObjectStore_deleteObject(ObjectStore * this, Object object);
 PUBLIC void ObjectStore_reportUnallocated(ObjectStore * this);
 PUBLIC void ObjectStore_report(ObjectStore * this);
 #endif /* _OBJECTSTORE_H_ */
