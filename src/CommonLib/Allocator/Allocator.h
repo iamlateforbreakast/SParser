f465f3a783da8c6f02a6eefebac647668bd8bf83
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2023-11-13 21:49:43 +0000

    Compile ObjectStore

--- src/CommonLib/Allocator/Allocator.h
+++ src/CommonLib/Allocator/Allocator.h
@@ -2,26 +2,20 @@
 #ifndef _ALLOCATOR_H_
 #define _ALLOCATOR_H_
 
-enum AllocatorType
+typedef struct Allocator Allocator;
+
+typedef char* (*AllocateFunction)(Allocator * allocator, unsigned int size);
+typedef void (*DeAllocateFunction)(Allocator * allocator, void * ptr);
+
+struct Allocator
 {
-  MALLOC,
-  POOLED
+  AllocateFunction allocate;
+  DeAllocateFunction deallocate;
 };
 
-typedef struct AllocatorParam
-{
-  int type;
-  char * fileName;
-} AllocParam;
-
-typedef struct Allocator
-{
-  AllocParam * param;
-} Allocator;
-
-Allocator * Allocator_new(AllocParam * allocParam);
-//void * Allocator_alloc(Allocator * this, unsigned int size);
+Allocator * Allocator_new();
+char * Allocator_allocate(Allocator * this, unsigned int size);
 //void * Allocator_allocFromClass(Allocator * this, /* Class class*/);
-//void Allocator_dealloc(Allocator * this);
+void Allocator_deallocate(Allocator * this, char * ptr);
 void Allocator_delete(Allocator * this);
 #endif /* _ALLOCATOR_H_ */
