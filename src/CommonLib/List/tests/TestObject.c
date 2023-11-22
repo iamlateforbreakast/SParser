09e96ea2d4fd23b8ff21f3658fd944d598434bc0
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-11-22 19:52:26 +0000

    Update TestObject.c

--- src/CommonLib/List/tests/TestObject.c
+++ src/CommonLib/List/tests/TestObject.c
@@ -10,6 +10,19 @@ struct TestObject
   int testValue;
 };
 
+/**********************************************//**
+  @private Class Description
+**************************************************/
+PRIVATE Class testObjectClass =
+{
+  .f_new = (Constructor)0,
+  .f_delete = (Destructor)&TestObject_delete,
+  .f_copy = (Copy_Operator)&TestObject_copy,
+  .f_comp = (Comp_Operator)&TestObject_compare,
+  .f_print = (Printer)&TestObject_print,
+  .f_size = (Sizer)&TestObject_getSize
+};
+
 PUBLIC TestObject * TestObject_new()
 {
   return 0;
@@ -17,10 +30,39 @@ PUBLIC TestObject * TestObject_new()
 
 PUBLIC TestObject * TestObject_newFromAllocator(Allocator* allocator)
 {
-  return 0;
+  TestObject * this = 0;
+
+  this = (TestObject*)Object_newFromAllocator(&testObjectClass, allocator);
+  // TODO: Check if allocation failed
+  this->testValue = 12345678;
+
+  return this;
 }
 
 PUBLIC void TestObject_delete(TestObject * this)
 {
 
+}
+
+PUBLIC int TestObject_compare(TestObject* this, TestObject* compare)
+{
+  return 0;
+}
+
+PUBLIC TestObject* TestObject_copy()
+{
+  return 0;
+}
+
+PUBLIC void TestObject_print(TestObject* this)
+{
+
+}
+
+PUBLIC unsigned int TestObject_getSize(TestObject* this)
+{
+  if (this != 0)
+    return sizeof(TestObject);
+  else
+    return sizeof(TestObject);
 }
