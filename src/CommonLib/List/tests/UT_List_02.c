f588063c84f85674c0a223d87afa566d45c28ce2
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-11-22 19:51:23 +0000

    Develop test

--- src/CommonLib/List/tests/UT_List_02.c
+++ src/CommonLib/List/tests/UT_List_02.c
@@ -1,6 +1,8 @@
 #include "List.h"
 #include "Object.h"
 #include "Allocator.h"
+#include "MyAllocator.h"
+#include "ObjectStore.h"
 #include "TestObject.h"
 #include "Debug.h"
 #include <stdlib.h>
@@ -13,35 +15,65 @@
                           { printf("Passed\n");} \
                           else { printf("Failed\n"); return 0;}
 
+ObjectStore* objectStore = 0;
+MyAllocator* testAlloc = 0;
+AllocInfo* allocInfo = 0;
+
 int step1()
 {
-  List* testList1 = 0;
+  List* testList = 0;
 
   TestObject * items[MAX_OBJECT_NB];
   int i = 0;
 
-  testList1 = List_new();
+  PRINT(("Step1: Test 1 - Create a list from a custom allocator: "));
+  testList = List_newFromAllocator((Allocator*)testAlloc);
 
+  UT_ASSERT((1));
+
+  PRINT(("Step1: Test 2 - Insert %d objects in list: ", MAX_OBJECT_NB));
   for (i = 0; i < MAX_OBJECT_NB; i++)
   {
-    items[i] = TestObject_new();
+    items[i] = TestObject_newFromAllocator((Allocator*)testAlloc);
     //items[i]->x = i;
     //items[i]->y = MAX_OBJECT_NB - i;
-    List_insertHead(testList1, items[i]);
-    printf("Nb items %d\n", List_getSize(testList1));
+    List_insertHead(testList, items[i]);
+    TRACE(("Nb items %d\n", List_getNbNodes(testList)));
   }
+  UT_ASSERT((List_getNbNodes(testList) == MAX_OBJECT_NB));
+
+  PRINT(("Step1: Test 3 - Insert %d objects in list: ", MAX_OBJECT_NB));
+
+  PRINT(("Step1: Test 4 - Check all memory is freed: "));
+  
+  UT_ASSERT((MyAllocator_report((Allocator*)testAlloc) != 0));
+  printf("Nb object allocated: %d\n", MyAllocator_report((Allocator*)testAlloc));
 
   return 0;
 }
 
 int step2()
 {
+  return 0;
+}
+
+int step3()
+{
+  return 0;
 }
 
 int main()
 {
+  objectStore = ObjectStore_getRef();
+  testAlloc = (MyAllocator*)MyAllocator_new();
+  allocInfo = ObjectStore_createAllocator(objectStore, (Allocator*)testAlloc);
+
   step1();
   step2();
+  step3();
+
+  ObjectStore_deleteAllocator(objectStore, allocInfo);
+  ObjectStore_delete(objectStore);
 
   return 0;
 }
