aa9fc0c1d03cfac004f74ac297caf689e4269609
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-11-29 10:40:57 +0000

    Update constructor

--- src/CommonLib/Map/tests/MyAllocator.h
+++ src/CommonLib/Map/tests/MyAllocator.h
@@ -7,8 +7,9 @@
 
 typedef struct MyAllocator MyAllocator;
 
-PUBLIC MyAllocator * MyAllocator_new();
+PUBLIC MyAllocator * MyAllocator_new(unsigned int size);
 PUBLIC void MyAllocator_delete(MyAllocator * this);
+void MyAllocator_reset(Allocator * this);
 PUBLIC void * MyAllocator_allocate(Allocator * allocator, unsigned int size);
 PUBLIC void MyAllocator_deallocate(Allocator * allocator, void * ptr);
 PUBLIC unsigned int MyAllocator_report(Allocator * this);
