63b9d028bbee0f08a78006e59249ff7c3616e377
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-12-06 17:06:52 +0000

    Use Object as key

--- src/CommonLib/SkipList/SkipList.h
+++ src/CommonLib/SkipList/SkipList.h
@@ -4,15 +4,16 @@
 #define _SKIPLIST_
 
 #include "Types.h"
+#include "Object.h"
 
 typedef struct SkipList SkipList;
 
 PUBLIC SkipList* SkipList_new();
 PUBLIC void SkipList_delete(SkipList* skipList);
 PUBLIC SkipList * SkipList_copy(SkipList * this);
-PUBLIC void SkipList_add(SkipList* this, unsigned int key, void* object);
-PUBLIC void * SkipList_remove(SkipList* this, unsigned int key);
-PUBLIC void * SkipList_get(SkipList* this, unsigned int key);
+PUBLIC void SkipList_add(SkipList* this, Object * key, Object * item);
+PUBLIC Object * SkipList_remove(SkipList* this, Object * key);
+PUBLIC Object * SkipList_get(SkipList* this, Object * key);
 PUBLIC int SkipList_compare(SkipList * this, SkipList * compared);
 PUBLIC void SkipList_print(SkipList* this);
 PUBLIC unsigned int SkipList_getSize(SkipList* this);
