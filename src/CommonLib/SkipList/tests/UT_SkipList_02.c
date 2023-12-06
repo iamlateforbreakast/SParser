b3ab36e335b72400bebb37a637f46ed26856c675
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2023-12-06 22:46:30 +0000

    Re-work test to use allocator

--- src/CommonLib/SkipList/tests/UT_SkipList_02.c
+++ src/CommonLib/SkipList/tests/UT_SkipList_02.c
@@ -4,6 +4,8 @@
 
 #include "SkipList.h"
 #include "Object.h"
+#include "MyAllocator.h"
+#include "ObjectStore.h"
 #include "Debug.h"
 #include "TestObject.h"
 #include "Memory.h"
@@ -12,16 +14,103 @@
 #include "String2.h"
 #include "Words1000.h"
 
-#define NB_OBJECTS (5000)
+#define DEBUG (0)
+
+#define UT_ASSERT(cond) if ((cond)) \
+                          { printf("Passed\n");} \
+                          else { printf("Failed\n"); return 0;}
+
+MyAllocator * testAlloc = 0;
+AllocInfo * allocInfo = 0;
 
 SkipList* testList;
 
-unsigned int randomKeys[NB_OBJECTS];
-unsigned int randomValues[NB_OBJECTS];
+int nbWords;
+String** wordKeys;
+TestObject** testObjects;
+
+int init_keys()
+{
+  String* allWords = String_new(words1000);
+  List* tokens = 0;
+
+  tokens = String_splitToken(allWords, " ");
+  nbWords = List_getNbNodes(tokens);
+
+  wordKeys = (String**)Memory_alloc(nbWords * sizeof(String*));
+  testObjects = (TestObject**)Memory_alloc(nbWords * sizeof(TestObject*));
+
+  for (int i = 0; i < nbWords; i++)
+  {
+    wordKeys[i] = (String*)List_getNext(tokens);
+    testObjects[i] = TestObject_new();
+  }
+
+  List_delete(tokens);
+  String_delete(allWords);
+
+  return 1;
+}
+
+int delete_keys()
+{
+  for (int i = 0; i < nbWords; i++)
+  {
+    TestObject_delete(testObjects[i]);
+    String_delete(wordKeys[i]);
+  }
+  Memory_free(wordKeys, sizeof(wordKeys));
+  Memory_free(testObjects, sizeof(testObjects));
+
+  return 1;
+}
 
 int step1()
 {
-    testList = SkipList_new(NB_OBJECTS + 1);
+  int isPassed = 1;
+
+  PRINT(("Step 1: Test 1 - Check SkipList struct is aligned: "));
+
+  isPassed = isPassed && ((SkipList_getSize(0) % MEM_ALIGN) == 0);
+
+  TRACE(("\n  Size: %d\n", SkipList_getSize(0)));
+  UT_ASSERT((isPassed));
+
+  PRINT(("Step 1: Test 2 - Build a SkipList: "));
+
+  testList = SkipList_newFromAllocator(testAlloc);
+
+  char* checkObjectPtr = (char*)testList;
+  char* checkSkipListPtr = (char*)testList + (sizeof(Object) / MEM_ALIGN) * MEM_ALIGN;
+
+  TRACE(("\n  Object id: %d\n", *(unsigned int*)checkObjectPtr));
+  isPassed = isPassed && ((*(unsigned int*)checkObjectPtr) == 1);
+  checkObjectPtr += sizeof(unsigned int) + 3 * sizeof(void*);
+  TRACE(("  Ref count: %d\n", *(unsigned int*)checkObjectPtr));
+  isPassed = isPassed && ((*(unsigned int*)checkObjectPtr) == 1);
+  TRACE(("  Level: %d\n", *(unsigned int*)checkSkipListPtr));
+  isPassed = isPassed && ((*(unsigned int*)checkSkipListPtr) == 0);
+
+  UT_ASSERT((isPassed));
+
+  return isPassed;
+}
+
+int step2()
+{
+  int isPassed = 1;
+
+  PRINT(("Step 5: Test 1 - Delete the SkipList instance: "));
+  SkipList_delete(testList);
+
+  UT_ASSERT((isPassed));
+
+  return isPassed;
+}
+
+int step3()
+{
+    /*testList = SkipList_newFromAllocator();
 
     for (int i=0; i<NB_OBJECTS; i++)
     {
@@ -55,12 +144,26 @@ int step1()
     SkipList_delete(testList);
 
     printf("Insert CPU time %Lf\n", (cpu_time1- cpu_time0)/NB_OBJECTS);
-    printf("Insert Wall Time %Lf\n", (wall_time1 - wall_time0)/NB_OBJECTS);
+    printf("Insert Wall Time %Lf\n", (wall_time1 - wall_time0)/NB_OBJECTS);*/
 
     return 1;
 }
 
 int main()
 {
-    step1();
+  ObjectStore * objectStore = 0;
+  
+  objectStore = ObjectStore_getRef();
+  testAlloc = MyAllocator_new(5000);
+  allocInfo = ObjectStore_createAllocator(objectStore, (Allocator*)testAlloc);
+
+  init_keys();
+
+  step1();
+  step2();
+  
+  delete_keys();
+
+  ObjectStore_deleteAllocator(objectStore, allocInfo);
+  ObjectStore_delete(objectStore);
 }
