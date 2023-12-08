617e6ca0339acb2cef9881ea3fb51bd7ba294659
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2023-12-08 16:08:32 +0000

    Add getSize

--- src/AppliLib/TimeMgr/Timer.h
+++ src/AppliLib/TimeMgr/Timer.h
@@ -7,6 +7,7 @@ typedef struct Timer Timer;
 PUBLIC Timer * Timer_new();
 PUBLIC void Timer_delete(Timer * this);
 PUBLIC Timer * Timer_copy(Timer * this);
+PUBLIC unsigned int Timer_getSize(Timer * this);
 PUBLIC unsigned int Timer_isEqual(Timer * this, Timer * compared);
 PUBLIC void Timer_print(Timer * this);
 PUBLIC void Timer_latchTime(Timer * this);
