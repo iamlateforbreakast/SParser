3982c7813ff49e03b10ffb7ba0f1af3534a1e880
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2023-12-21 17:49:10 +0000

    Add usage comments

--- src/ParseLib/Grammar2/Grammar2.h
+++ src/ParseLib/Grammar2/Grammar2.h
@@ -12,11 +12,11 @@ PUBLIC Grammar2 * Grammar2_copy(Grammar2 * this);
 PUBLIC void Grammar2_print(Grammar2 * this);
 PUBLIC unsigned int Grammar2_getSize(Grammar2 * this);
 PUBLIC void Grammar2_process(Grammar2 * this);
-PUBLIC FileReader * Grammar2_getFileReader(Grammar2 * grammar);
-PUBLIC SdbMgr * Grammar2_getSdbMgr(Grammar2 * grammar);
-PUBLIC void Grammar2_addToBuffer(Grammar2 * grammar, char * text);
-PUBLIC void Grammar2_addComment(Grammar2 * this);
+PUBLIC FileReader * Grammar2_getFileReader(Grammar2 * grammar); // Not used
+PUBLIC SdbMgr * Grammar2_getSdbMgr(Grammar2 * grammar); // Not used
+PUBLIC void Grammar2_addToBuffer(Grammar2 * grammar, char * text); // Used by lex.c
+PUBLIC void Grammar2_addComment(Grammar2 * this); // Used by parse.c
 PUBLIC void Grammar2_addCodeNode(Grammar2 * this);
 PUBLIC void Grammar2_addIncludeNode(Grammar2 * this, char * name);
-PUBLIC char * Grammar2_processNewFile(Grammar2 * this, String * fileName);
-PUBLIC void Grammar2_returnToFile(Grammar2 * this);
+PUBLIC char * Grammar2_processNewFile(Grammar2 * this, String * fileName); // Used by lex.c
+PUBLIC void Grammar2_returnToFile(Grammar2 * this); // Used by lex.c
