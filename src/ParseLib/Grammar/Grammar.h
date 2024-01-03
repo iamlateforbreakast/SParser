5adc71ed8ebc6177e57de1bcf43ffcce756714d2
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-01-03 22:55:58 +0100

    Update Grammar.h

--- src/ParseLib/Grammar/Grammar.h
+++ src/ParseLib/Grammar/Grammar.h
@@ -2,4 +2,19 @@
 #ifndef _GRAMMAR_H_
 #define _GRAMMAR_H_
 
+typedef struct Grammar Grammar;
+
+struct Grammar
+{
+  Grammar * (*new)(void);
+  void (*delete)(Grammar * this);
+  Grammar * (*copy)(Grammar * this);
+  void (*print)(Grammar * this);
+};
+
+PUBLIC Grammar * Grammar_new();
+PUBLIC void Grammar_delete(Grammar * this);
+PUBLIC void Grammar_process(Grammar * this);
+PUBLIC void Grammar_print(Grammar * this);
+
 #endif /* _GRAMMAR_H_ */
