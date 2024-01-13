5e1c82aca168d5cb5a9ccf392ddb955181ebac6f
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-01-13 17:16:18 +0000

    Re organise test

--- src/CommonLib/BTree/tests/UT_BTree.c
+++ src/CommonLib/BTree/tests/UT_BTree.c
@@ -1,67 +1,28 @@
 /*********************************************************************************
 * Main.c
-* 
-*********************************************************************************/
-#include <stdlib.h>
-#include <time.h>
-
-#include "Types.h"
+/* UT_BTree */
 #include "BTree.h"
+#include "TestObject.h"
 
 #define NB_ITEMS (6)
 
-//  Windows
-#ifdef _WIN32
-#include <Windows.h>
-double get_wall_time() {
-	LARGE_INTEGER time, freq;
-	if (!QueryPerformanceFrequency(&freq)) {
-		//  Handle error
-		return 0;
-	}
-	if (!QueryPerformanceCounter(&time)) {
-		//  Handle error
-		return 0;
-	}
-	return (double)time.QuadPart / freq.QuadPart;
+void init_keys()
+{
 }
-double get_cpu_time() {
-	FILETIME a, b, c, d;
-	if (GetProcessTimes(GetCurrentProcess(), &a, &b, &c, &d) != 0) {
-		//  Returns total user time.
-		//  Can be tweaked to include kernel times as well.
-		return
-			(double)(d.dwLowDateTime |
-				((unsigned long long)d.dwHighDateTime << 32)) * 0.0000001;
-	}
-	else {
-		//  Handle error
-		return 0;
-	}
-}
-//  Posix/Linux
-#else
-#include <time.h>
-#include <sys/time.h>
-double get_wall_time() {
-	struct timeval time;
-	if (gettimeofday(&time, NULL)) {
-		//  Handle error
-		return 0;
-	}
-	return (double)time.tv_sec + (double)time.tv_usec * .000001;
-}
-double get_cpu_time() {
-	return (double)clock() / CLOCKS_PER_SEC;
-}
-#endif
 
-/*********************************************************************************
-* main
-* input: None
-* output: None
-*********************************************************************************/
-unsigned int main(void)
+void delete_keys()
+{
+}
+
+int step1()
+{
+  int isPassed = 1;
+  PRINT(("Step 1: Test 1:"));
+	
+  return isPassed;
+}
+
+int main(void)
 {
 	BTree* testTree;
 	unsigned int keys[NB_ITEMS];
