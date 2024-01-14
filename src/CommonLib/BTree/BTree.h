dd3b229e893592abcb1bf1d7cbf01052049129f7
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-01-14 16:52:10 +0000

    Use Object *

--- src/CommonLib/BTree/BTree.h
+++ src/CommonLib/BTree/BTree.h
@@ -6,9 +6,8 @@
 *********************************************************************************/
 #include "Types.h"
 #include "Node.h"
-
-#define TRUE (1)
-#define FALSE (0)
+#include "Object.h"
+#include "Allocator.h"
 
 typedef struct BTree
 {
@@ -21,11 +20,12 @@ typedef struct BTree
 } BTree;
 
 PUBLIC BTree * BTree_new(unsigned int order);
+PUBLIC BTree * BTree_newFromAllocator(Allocator * allocator);
 PUBLIC BTree * BTree_newFromFile(char* fileName);
 PUBLIC void BTree_delete(BTree * tree);
-PUBLIC void BTree_add(BTree * tree, Key key, Object object, int isOwner);
-PUBLIC Object BTree_get(BTree * tree, Key key);
-PUBLIC Object BTree_remove(BTree * tree, Key key);
+PUBLIC void BTree_add(BTree * tree, Object * key, Object * object, int isOwner);
+PUBLIC Object BTree_get(BTree * tree, Object * key);
+PUBLIC Object BTree_remove(BTree * tree, Object * key);
 PUBLIC void BTree_print(BTree * tree);
 PUBLIC unsigned int BTree_sizeof(BTree* tree);
 PUBLIC unsigned int BTree_reportSizeInBytes(BTree * tree);
