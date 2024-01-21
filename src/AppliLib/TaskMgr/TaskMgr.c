125be82b1db0f3071a691ced668e33bd278c6045
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-01-21 17:37:13 +0000

    Create TaskMgr.c

---
+++ src/AppliLib/TaskMgr/TaskMgr.c
@@ -0,0 +1,22 @@
+/* TaskMgr.c */
+#include "TaskMgr.h"
+#include "Object.h"
+
+struct TaskMgr
+{
+  Object object;
+};
+
+Class taskMgrClass{
+};
+
+PUBLIC TaskMgr * TaskMgr_new()
+{
+
+}
+
+PUBLIC void TaskMgr_delete(TaskMgr * this)
+{
+}
+
+
