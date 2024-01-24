ce5ea7a6ce4abb4e081766d0a9e31e5d53aa34f3
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-01-24 19:06:30 +0000

    Update TaskMgr.h

--- src/AppliLib/TaskMgr/TaskMgr.h
+++ src/AppliLib/TaskMgr/TaskMgr.h
@@ -1,6 +1,14 @@
 /* TaskMgr.h */
+#ifndef _TASKMGR_H_
+#define _TASKMGR_H_
+
+#include "Types.h"
 
 typedef struct TaskMgr TaskMgr;
 
 PUBLIC TaskMgr * TaskMgr_new();
 PUBLIC void TaskMgr_delete(TaskMgr * this);
+
+// create Async task(function)
+// create Sync task(function)
+#endif /* _TASKMGR_H_ */
