9ea739b35402be169b099f962ef85c600ed0f1c6
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-01-25 18:23:04 +0000

    Update HTTPServer.h

--- src/ParseLib/HTTPServer/HTTPServer.h
+++ src/ParseLib/HTTPServer/HTTPServer.h
@@ -8,5 +8,9 @@ typedef struct HTTPServer HTTPServer;
 
 PUBLIC HTTPServer * HTTPServer_new();
 PUBLIC void HTTPServer_delete(HTTPServer * this);
-
+PUBLIC HTTPServer* HTTPServer_copy(HTTPServer* this);
+PUBLIC int HTTPServer_compare(HTTPServer* this, HTTPServer* compared);
+PUBLIC void HTTPServer_print(HTTPServer* this);
+PUBLIC unsigned int HTTPServer_getSize(HTTPServer* this);
+//PUBLIC void HTTPServer_start(HTTPServer* this);
 #endif /* _HTTPSERVER_H_ */
