0cc80db4cc0710f51d24fa547bbf6a3b055f7189
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-01-27 23:35:40 +0000

    Update UT_TaskMgr_01.c

--- src/AppliLib/TaskMgr/tests/UT_TaskMgr_01.c
+++ src/AppliLib/TaskMgr/tests/UT_TaskMgr_01.c
@@ -1,16 +1,25 @@
 /* UT_TaskMgr_01.c */
-
+#include "Task.h"
 #include "TaskMgr.h"
+#include "Debug.h"
+
+void * taskBody(void * this)
+{
+  PRINT(("Here\n"));
+  
+  return 0;
+}
 
 int step1()
 {
   int isPassed = 1;
+  Task * testTask = Task_create(&taskBody);
   
   TaskMgr * testTaskMgr = 0;
 
   testTaskMgr = TaskMgr_new();
 
-  TaskMgr_start(testTaskMgr, 0);
+  TaskMgr_start(testTaskMgr, testTask);
 
   //TaskMgr_stop(testTaskMgr);
 
