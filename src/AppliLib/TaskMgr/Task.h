ebd1c41b9fc6e118d6e33087c244bfb1ca3454a5
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-01-29 18:18:41 +0000

    Add API and separate Task.c

--- src/AppliLib/TaskMgr/Task.h
+++ src/AppliLib/TaskMgr/Task.h
@@ -3,55 +3,14 @@
 #define _TASK_H_
 
 #include "Types.h"
-#include "Memory.h"
-#include "TaskMgr.h"
 
-#ifndef WIN32
-#include <pthread.h>
-#else
-#include <windows.h>
-#include <conio.h>
-#include <process.h>
-#endif
+typedef struct Task Task;
 
-TaskMgr * taskMgr = 0;
-
-typedef struct Task
-{
-  void (*body)(Object * this, int nbParams, void **params);
-  int nbParams;
-  void **params;
-  int isFinished;
-  int execTime;
-} Task;
-
-PUBLIC Task * Task_create(void (*body)(Object * this, int nbParams, void **params))
-{
-  Task * this = 0;
-  
-  this = (Task*)Memory_alloc(sizeof(Task));
-
-  //TaskMgr_startTask(this);
-
-  return 0;
-}
-
-    // Wait for each thread to complete
-    //WaitForSingleObject(hThreads[--ThreadNr], INFINITE);
-
-PRIVATE void Task_executeBody()
-{
-  //wait run mutex
-
-  // wait clock mutex
-
-  //wait run mutex with timeout
-
-  //terminate
-
-  //this->body(this->object, this->nbParams, this->params);
-
-  //Task_delete();
-}
+PUBLIC Task* Task_create(void * (*body)(void* p));
+PUBLIC void Task_start(Task* this);
+PUBLIC int Task_isReady(Task* this);
+PUBLIC int Task_isRunning(Task* this);
+PUBLIC void Task_destroy(Task* this);
+PUBLIC void Task_executeBody(Task* this);
 
 #endif /* _TASK_H_ */
