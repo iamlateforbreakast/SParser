52ed0dc19c49dabc4bb23ab2d178bd12ca8ab656
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-02-07 16:56:08 +0000

    Create Configuration.c

---
+++ src/ParseLib/Configuration/Configuration.c
@@ -0,0 +1,18 @@
+/* Configuration.c */
+#include "Configuration.h"
+#include "Object.h"
+
+struct Configuration
+{
+  Object object;
+};
+
+PUBLIC Configuration * Configuration_new(String * input)
+{
+  return 0;
+};
+
+PUBLIC void Configuration_delete(Configuration * this)
+{
+  
+}
