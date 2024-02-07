a851c9f3ee21c8e9658455fbd2d92d870b55a529
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-02-07 17:04:23 +0000

    Add products member

--- src/ParseLib/Configuration/Configuration.c
+++ src/ParseLib/Configuration/Configuration.c
@@ -1,10 +1,12 @@
 /* Configuration.c */
 #include "Configuration.h"
+#include "Product.h"
 #include "Object.h"
 
 struct Configuration
 {
   Object object;
+  List * products;
 };
 
 PUBLIC Configuration * Configuration_new(String * input)
