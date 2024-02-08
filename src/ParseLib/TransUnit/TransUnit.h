36d65f533f91015be9694439c8f5b6dd6371a67c
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-02-08 18:34:34 +0000

    Create TransUnit.h

---
+++ src/ParseLib/TransUnit/TransUnit.h
@@ -0,0 +1,16 @@
+/* TransUnit.h */
+#ifndef _TRANSUNIT_H_
+#define _TRANSUNIT_H_
+
+#include "Types.h"
+#include "FileMgr.h"
+
+typedef struct TransUnit TransUnit;
+
+PUBLIC TransUnit * TransUnit_new(FileDesc * file);
+PUBLIC void TransUnit_delete(TransUnit * this);
+PUBLIC void TransUnit_print(TransUnit* this);
+PUBLIC unsigned int TransUnit_getSize(TransUnit* this);
+PUBLIC String * TransUnit_getNextBuffer(TransUnit* this);
+
+#endif /* _CONFIGURATION_H_ */
