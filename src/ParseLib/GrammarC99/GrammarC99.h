4702b17f5f1239c5ec0153dc4dd0c8495bff652b
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2024-02-12 10:11:00 +0000

    Create grammar

---
+++ src/ParseLib/GrammarC99/GrammarC99.h
@@ -0,0 +1,8 @@
+/* GrammarC99.h */
+
+#include "Types.h"
+
+typedef struct GrammarC99 GrammarC99;
+
+PUBLIC GrammarC99 * GrammarC99_new();
+PUBLIC void GrammarC99_delete(GrammarC99 * this);
---
+++ src/ParseLib/GrammarC99/GrammarC99.parse.h
@@ -0,0 +1,214 @@
+/* A Bison parser, made by GNU Bison 3.8.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
+   Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
+
+#ifndef YY_GRAMMARC99_GRAMMARC99_PARSE_H_INCLUDED
+# define YY_GRAMMARC99_GRAMMARC99_PARSE_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int GrammarC99_debug;
+#endif
+
+/* Token kinds.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    IDENTIFIER = 258,              /* IDENTIFIER  */
+    CONSTANT = 259,                /* CONSTANT  */
+    STRING_LITERAL = 260,          /* STRING_LITERAL  */
+    SIZEOF = 261,                  /* SIZEOF  */
+    PTR_OP = 262,                  /* PTR_OP  */
+    INC_OP = 263,                  /* INC_OP  */
+    DEC_OP = 264,                  /* DEC_OP  */
+    LEFT_OP = 265,                 /* LEFT_OP  */
+    RIGHT_OP = 266,                /* RIGHT_OP  */
+    LE_OP = 267,                   /* LE_OP  */
+    GE_OP = 268,                   /* GE_OP  */
+    EQ_OP = 269,                   /* EQ_OP  */
+    NE_OP = 270,                   /* NE_OP  */
+    AND_OP = 271,                  /* AND_OP  */
+    OR_OP = 272,                   /* OR_OP  */
+    MUL_ASSIGN = 273,              /* MUL_ASSIGN  */
+    DIV_ASSIGN = 274,              /* DIV_ASSIGN  */
+    MOD_ASSIGN = 275,              /* MOD_ASSIGN  */
+    ADD_ASSIGN = 276,              /* ADD_ASSIGN  */
+    SUB_ASSIGN = 277,              /* SUB_ASSIGN  */
+    LEFT_ASSIGN = 278,             /* LEFT_ASSIGN  */
+    RIGHT_ASSIGN = 279,            /* RIGHT_ASSIGN  */
+    AND_ASSIGN = 280,              /* AND_ASSIGN  */
+    XOR_ASSIGN = 281,              /* XOR_ASSIGN  */
+    OR_ASSIGN = 282,               /* OR_ASSIGN  */
+    TYPE_NAME = 283,               /* TYPE_NAME  */
+    TYPEDEF = 284,                 /* TYPEDEF  */
+    EXTERN = 285,                  /* EXTERN  */
+    STATIC = 286,                  /* STATIC  */
+    AUTO = 287,                    /* AUTO  */
+    REGISTER = 288,                /* REGISTER  */
+    INLINE = 289,                  /* INLINE  */
+    RESTRICT = 290,                /* RESTRICT  */
+    CHAR = 291,                    /* CHAR  */
+    SHORT = 292,                   /* SHORT  */
+    INT = 293,                     /* INT  */
+    LONG = 294,                    /* LONG  */
+    SIGNED = 295,                  /* SIGNED  */
+    UNSIGNED = 296,                /* UNSIGNED  */
+    FLOAT = 297,                   /* FLOAT  */
+    DOUBLE = 298,                  /* DOUBLE  */
+    CONST = 299,                   /* CONST  */
+    VOLATILE = 300,                /* VOLATILE  */
+    VOID = 301,                    /* VOID  */
+    BOOL = 302,                    /* BOOL  */
+    COMPLEX = 303,                 /* COMPLEX  */
+    IMAGINARY = 304,               /* IMAGINARY  */
+    STRUCT = 305,                  /* STRUCT  */
+    UNION = 306,                   /* UNION  */
+    ENUM = 307,                    /* ENUM  */
+    ELLIPSIS = 308,                /* ELLIPSIS  */
+    CASE = 309,                    /* CASE  */
+    DEFAULT = 310,                 /* DEFAULT  */
+    IF = 311,                      /* IF  */
+    ELSE = 312,                    /* ELSE  */
+    SWITCH = 313,                  /* SWITCH  */
+    WHILE = 314,                   /* WHILE  */
+    DO = 315,                      /* DO  */
+    FOR = 316,                     /* FOR  */
+    GOTO = 317,                    /* GOTO  */
+    CONTINUE = 318,                /* CONTINUE  */
+    BREAK = 319,                   /* BREAK  */
+    RETURN = 320                   /* RETURN  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
+#endif
+/* Token kinds.  */
+#define YYEMPTY -2
+#define YYEOF 0
+#define YYerror 256
+#define YYUNDEF 257
+#define IDENTIFIER 258
+#define CONSTANT 259
+#define STRING_LITERAL 260
+#define SIZEOF 261
+#define PTR_OP 262
+#define INC_OP 263
+#define DEC_OP 264
+#define LEFT_OP 265
+#define RIGHT_OP 266
+#define LE_OP 267
+#define GE_OP 268
+#define EQ_OP 269
+#define NE_OP 270
+#define AND_OP 271
+#define OR_OP 272
+#define MUL_ASSIGN 273
+#define DIV_ASSIGN 274
+#define MOD_ASSIGN 275
+#define ADD_ASSIGN 276
+#define SUB_ASSIGN 277
+#define LEFT_ASSIGN 278
+#define RIGHT_ASSIGN 279
+#define AND_ASSIGN 280
+#define XOR_ASSIGN 281
+#define OR_ASSIGN 282
+#define TYPE_NAME 283
+#define TYPEDEF 284
+#define EXTERN 285
+#define STATIC 286
+#define AUTO 287
+#define REGISTER 288
+#define INLINE 289
+#define RESTRICT 290
+#define CHAR 291
+#define SHORT 292
+#define INT 293
+#define LONG 294
+#define SIGNED 295
+#define UNSIGNED 296
+#define FLOAT 297
+#define DOUBLE 298
+#define CONST 299
+#define VOLATILE 300
+#define VOID 301
+#define BOOL 302
+#define COMPLEX 303
+#define IMAGINARY 304
+#define STRUCT 305
+#define UNION 306
+#define ENUM 307
+#define ELLIPSIS 308
+#define CASE 309
+#define DEFAULT 310
+#define IF 311
+#define ELSE 312
+#define SWITCH 313
+#define WHILE 314
+#define DO 315
+#define FOR 316
+#define GOTO 317
+#define CONTINUE 318
+#define BREAK 319
+#define RETURN 320
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+union YYSTYPE
+{
+#line 19 "GrammarC99.y"
+
+  char* sval;
+
+#line 201 "GrammarC99.parse.h"
+
+};
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+
+
+int GrammarC99_parse (void * scanner, GrammarC99 * grammar);
+
+
+#endif /* !YY_GRAMMARC99_GRAMMARC99_PARSE_H_INCLUDED  */
