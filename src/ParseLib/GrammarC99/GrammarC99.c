4a593a171695c3639b93fd029d9adef1c02af657
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2024-02-12 10:06:53 +0000

    Create grammar

---
+++ src/ParseLib/GrammarC99/GrammarC99.c
@@ -0,0 +1,16 @@
+/* GrammarC99 */
+
+#include "GrammarC99.h"
+
+struct GrammarC99
+{
+};
+
+PUBLIC GrammarC99 * GrammarC99_new()
+{
+  return 0;
+}
+
+PUBLIC void GrammarC99_delete(GrammarC99 * this)
+{
+}
