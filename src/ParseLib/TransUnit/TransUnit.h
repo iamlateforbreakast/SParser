57dd6cab8e60dd9a51a9beea1189d1218451f617
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-02-13 21:33:45 +0000

    Add get name

--- src/ParseLib/TransUnit/TransUnit.h
+++ src/ParseLib/TransUnit/TransUnit.h
@@ -11,6 +11,7 @@ PUBLIC TransUnit * TransUnit_new(FileDesc * file);
 PUBLIC void TransUnit_delete(TransUnit * this);
 PUBLIC void TransUnit_print(TransUnit* this);
 PUBLIC unsigned int TransUnit_getSize(TransUnit* this);
+PUBLIC char* TransUnit_getName(TransUnit* this);
 PUBLIC String * TransUnit_getNextBuffer(TransUnit* this);
 
 #endif /* _CONFIGURATION_H_ */
