021fb43c41930ca5731e43d8fe4216aada69d5d1
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-02-13 14:32:08 +0000

    Update GrammarC99.h

--- src/ParseLib/GrammarC99/GrammarC99.h
+++ src/ParseLib/GrammarC99/GrammarC99.h
@@ -1,11 +1,17 @@
 /* GrammarC99.h */
+#ifndef _GRAMMARC99_H_
+#define _GRAMMARC99_H_
 
 #include "Types.h"
+#include "FileMgr.h"
 #include "Grammar.h"
 
 typedef struct GrammarC99 GrammarC99;
 
-PUBLIC Grammar * GrammarC99_new();
-PUBLIC void GrammarC99_delete(Grammar * this);
-PUBLIC void GrammarC99_print(Grammar * this);
-PUBLIC unsigned int GrammarC99_getSize(Grammar * this);
+PUBLIC Grammar* GrammarC99_new(FileDesc * fileDesc);
+PUBLIC void GrammarC99_delete(Grammar* this);
+PUBLIC void GrammarC99_print(Grammar* this);
+PUBLIC unsigned int GrammarC99_getSize(Grammar* this);
+PUBLIC void GrammarC99_process(GrammarC99* this)
+
+#endif /* _GRAMMARC99_H_ */
