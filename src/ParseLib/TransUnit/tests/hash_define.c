36c6d03c7492f0c18471f36b634d2778b9d8baeb
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-02-15 09:58:08 +0000

    Update hash_define.c

--- src/ParseLib/TransUnit/tests/hash_define.c
+++ src/ParseLib/TransUnit/tests/hash_define.c
@@ -6,13 +6,22 @@
 
 #define ADD(X,Y) X=X+Y
 
+#define SKIP_SPACES(p, limit)     \
+do { char *lim = (limit);         \
+     while (p < lim) {            \
+       if (*p++ != ' ') {         \
+         p--; break; }}}          \
+while (0)
+  
 void f()
 {
   int a;
   int b;
-
+  char line[] = "      Hello";
+  
   INC(a);
   ADD(a,b);
-
+  SKIP_SPACES(line, &line[10]);
+  
   a = TRUE;
 }
