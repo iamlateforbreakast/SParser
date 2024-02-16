0e76f6478b9bb28141206cc95ed35952f247edd2
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-02-16 14:30:52 +0000

    Develop UT_TransUnit_01.c

--- src/ParseLib/TransUnit/tests/UT_TransUnit_01.c
+++ src/ParseLib/TransUnit/tests/UT_TransUnit_01.c
@@ -20,8 +20,6 @@ int step1()
   ObjectMgr* objectMgr = ObjectMgr_getRef();
   FileDesc * c_file = FileMgr_addFile(fileMgr, "test.c");
 
-  FileDesc * h_file = FileMgr_addFile(fileMgr, "test.h");
-
   PRINT(("Step 1: Test 1 - Create an instance of class TransUnit: "));
   testTransUnit = TransUnit_new(c_file);
   UT_ASSERT((testTransUnit != 0));
@@ -30,7 +28,39 @@ int step1()
   TransUnit_delete(testTransUnit);
 
   FileMgr_delete(fileMgr);
+
   PRINT(("Step 1: Test 3 - Check all memory is freed: "));
+  UT_ASSERT((ObjectMgr_report(objectMgr) == 1));
+  TRACE(("Nb objects left allocated: %d\n", ObjectMgr_report(objectMgr)));
+
+  ObjectMgr_reportUnallocated(objectMgr);
+  ObjectMgr_delete(objectMgr);
+
+  return isPassed; 
+}
+
+int step2()
+{
+  int isPassed = 1;
+
+  FileMgr* fileMgr = FileMgr_getRef();
+  ObjectMgr* objectMgr = ObjectMgr_getRef();
+  FileDesc* c_file = FileMgr_addFile(fileMgr, "test.c");
+  FileDesc* h_file = FileMgr_addFile(fileMgr, "test.h");
+  TransUnit* testTransUnit = TransUnit_new(c_file);
+
+  PRINT(("Step 2: Test 1 - Read a buffer from TrasnUnit instance: "));
+  String* buffer = TransUnit_getNextBuffer(testTransUnit);
+  String_print(buffer);
+
+  
+  //PRINT(("Step 1: Test 2 - Delete an instance of class TransUnit: "));
+  String_delete(buffer);
+  TransUnit_delete(testTransUnit);
+
+  FileMgr_delete(fileMgr);
+
+  PRINT(("Step 2: Test 1 - Check all memory is freed: "));
   UT_ASSERT((ObjectMgr_report(objectMgr) == 1));
   TRACE(("Nb objects left allocated: %d\n", ObjectMgr_report(objectMgr)));
 
@@ -45,7 +75,9 @@ int main()
   int isPassed = 1;
   
   step1();
-  
+  step2();
+
   Memory_report();
+
   return isPassed;
 }
