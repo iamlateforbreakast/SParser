73113905206badb24c6d59027234edb797f679ff
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-03-05 18:56:48 +0000

    Update Configuration.c

--- src/ParseLib/Configuration/Configuration.c
+++ src/ParseLib/Configuration/Configuration.c
@@ -9,12 +9,44 @@ struct Configuration
   List * products;
 };
 
+PRIVATE Configuration_readLabel(Configuration * this);
+PRIVATE Configuration_readValue(Configuration * this);
 PUBLIC Configuration * Configuration_new(String * input)
 {
+  Configuration* c = (Configuration*)Object_new(sizeof(Configuration), &configurationClass)
+    char* p = String_getBuffer(input);
+  while (1)
+  {
+    s = Configuration_readLabel(this, p);
+    if s
+    v = Configurationg_readValue(this, p);
+    if (String_compare(s, "Location"))
+    {
+      p
+    }
+    else if (String_compare(s, "Includes"))
+    {
+    }
+    else if (String_compare(s, "Uses"))
+    {
+    }
+    else if (String_compare(s, "Sources"))
+    {
+    }
+    else
+    {
+      p = Product_new();
+    }
+  }
   return 0;
 };
 
 PUBLIC void Configuration_delete(Configuration * this)
 {
+  Object_deallocate(&this->object);
+}
   
+PRIVATE String* Configuration_readLabel(Configuration* this)
+{
+  return 0;
 }
