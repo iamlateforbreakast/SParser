bf1f4acf139a6c14c962e08182cc8282f0176781
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2024-03-05 21:30:55 +0000

    Update

--- src/ParseLib/Configuration/Configuration.c
+++ src/ParseLib/Configuration/Configuration.c
@@ -9,44 +9,85 @@ struct Configuration
   List * products;
 };
 
-PRIVATE Configuration_readLabel(Configuration * this);
-PRIVATE Configuration_readValue(Configuration * this);
+/**********************************************//**
+  @private Class Description
+**************************************************/
+PRIVATE Class configurationClass =
+{
+  .f_new = (Constructor)0,
+  .f_delete = (Destructor)&Configuration_delete,
+  .f_copy = (Copy_Operator)0,
+  .f_comp = (Comp_Operator)0,
+  .f_print = (Printer)&Configuration_print,
+  .f_size = (Sizer)&Configuration_getSize
+};
+
+PRIVATE String * Configuration_readLabel(Configuration * this, char * p);
+PRIVATE String * Configuration_readValue(Configuration * this, char * p);
+
 PUBLIC Configuration * Configuration_new(String * input)
 {
-  Configuration* c = (Configuration*)Object_new(sizeof(Configuration), &configurationClass)
-    char* p = String_getBuffer(input);
-  while (1)
+  Configuration* c = (Configuration*)Object_new(sizeof(Configuration), &configurationClass);
+  char* p = String_getBuffer(input);
+  int nbCharRead = 0;
+
+  while (nbCharRead<String_getLength(input))
   {
-    s = Configuration_readLabel(this, p);
-    if s
-    v = Configurationg_readValue(this, p);
+    String * s = Configuration_readLabel(c, p);
+    
+    if (s==0) exit(0);
+
+    String * v = Configuration_readValue(c, p);
+
+    if (v==0) exit(0);
+
     if (String_compare(s, "Location"))
     {
-      p
+      Product_setLocation(p, v);
     }
     else if (String_compare(s, "Includes"))
     {
+      Product_setIncludes(p, v);
     }
     else if (String_compare(s, "Uses"))
     {
+      Product_setUses(p, v);
     }
     else if (String_compare(s, "Sources"))
     {
+      Product_setSources(p, v);
     }
     else
     {
-      p = Product_new();
+      p = Product_new(s);
+      List_insertHead(c->products, p, 1);
     }
   }
-  return 0;
+
+  return c;
 };
 
 PUBLIC void Configuration_delete(Configuration * this)
 {
   Object_deallocate(&this->object);
 }
-  
-PRIVATE String* Configuration_readLabel(Configuration* this)
+
+PUBLIC void Configuration_print(Configuration * this)
+{
+
+}
+
+PUBLIC unsigned int Configuration_getSize(Configuration * this)
+{
+  return sizeof(Configuration);
+}
+
+PRIVATE String* Configuration_readLabel(Configuration* this, char * p)
+{
+  return 0;
+}
+
+PRIVATE String * Configuration_readValue(Configuration * this, char * p)
 {
   return 0;
 }
--- src/ParseLib/Configuration/Configuration.h
+++ src/ParseLib/Configuration/Configuration.h
@@ -9,5 +9,6 @@ typedef struct Configuration Configuration;
 
 PUBLIC Configuration * Configuration_new(String * input);
 PUBLIC void Configuration_delete(Configuration * this);
-
+PUBLIC void Configuration_print(Configuration * this);
+PUBLIC unsigned int Configuration_getSize(Configuration * this);
 #endif /* _CONFIGURATION_H_ */
