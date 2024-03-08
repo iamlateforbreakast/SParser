89c2024490f67d1868f450001ddfd50d18a65ae1
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-03-08 16:33:01 +0000

    Add isManaged

--- src/AppliLib/FileMgr/FileMgr.h
+++ src/AppliLib/FileMgr/FileMgr.h
@@ -22,4 +22,5 @@ PUBLIC unsigned int FileMgr_addDirectory(FileMgr * this, const char * directoryN
 PUBLIC FileDesc * FileMgr_addFile(FileMgr * this, const char * fileName);
 PUBLIC List * FileMgr_filterFiles(FileMgr * this, const char * pattern);
 PUBLIC FileDesc * FileMgr_searchFile(FileMgr * this, String * name, List * preferredDir);
+PUBLIC FileDesc* FileMgr_isManaged(FileMgr* this, String* fullName);
 #endif /* _FILEMGR_H_ */
