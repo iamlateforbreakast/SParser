47e5b4fc6b25e2877b23c5a2826a20ee50d55baf
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-04-11 17:11:34 +0100

    Add design for getFileforProduct()

--- src/ParseLib/Configuration/Configuration.c
+++ src/ParseLib/Configuration/Configuration.c
@@ -45,9 +45,9 @@ PRIVATE Class configurationClass =
   .f_size = (Sizer)&Configuration_getSize
 };
 
-PRIVATE List * Configuration_readProducts(Configuration* this, String* s);
-PRIVATE String * Configuration_readLocation(Configuration* this, String* s, unsigned int * idx);
-PRIVATE List * Configuration_readIncludes(Configuration* this, String* s, unsigned int * idx);
+PRIVATE List* Configuration_readProducts(Configuration* this, String* s);
+PRIVATE String* Configuration_readLocation(Configuration* this, String* s, unsigned int* idx);
+PRIVATE List* Configuration_readIncludes(Configuration* this, String* s, unsigned int* idx);
 PRIVATE List* Configuration_readUses(Configuration* this, String* s, unsigned int* idx);
 PRIVATE List* Configuration_readSources(Configuration* this, String* s, unsigned int* idx);
 PRIVATE String* Configuration_readString(Configuration* this, String* s, unsigned int* idx);
@@ -55,10 +55,10 @@ PRIVATE List* Configuration_readList(Configuration* this, String* s, unsigned in
 PRIVATE void Configuration_readEndOfLine(Configuration* this, String* s, unsigned int* idx);
 PRIVATE unsigned int Configuration_readIndent(Configuration* this, String* s, unsigned int* idx);
 
-PUBLIC Configuration * Configuration_new(String* input)
+PUBLIC Configuration* Configuration_new(String* input)
 {
   Configuration* c = (Configuration*)Object_new(sizeof(Configuration), &configurationClass);
-  
+
   if (c == 0) return 0;
 
   c->products = Configuration_readProducts(c, input);
@@ -94,15 +94,30 @@ PUBLIC unsigned int Configuration_getSize(Configuration* this)
   return sizeof(Configuration);
 }
 
-PRIVATE List* Configuration_readProducts(Configuration* this, String * s)
+PUBLIC FileMgr* Configuration_getFilesForProduct(Configuration* this, Product * product)
+{
+  FileMgr* fm = 0;
+
+  // location = Product_getLocation(product);
+  // fm = FileMgr_new();
+  // FileMgr_setRootLocation(fm, location);
+  // for each directory in include
+  // add directoy to fm
+  // for each file in source
+  // add file to fm
+
+  return fm;
+}
+
+PRIVATE List* Configuration_readProducts(Configuration* this, String* s)
 {
   char* p = String_getBuffer(s);
-  List * products = 0;
+  List* products = 0;
   Product* product = 0;
   unsigned int idx1 = 0;
   unsigned int idx2 = 0;
 
-  while (IS_LIST(p+idx2) && (idx2<String_getLength(s)))
+  while (IS_LIST(p + idx2) && (idx2 < String_getLength(s)))
   {
     idx2 += 2;
     idx1 = idx2;
@@ -110,7 +125,7 @@ PRIVATE List* Configuration_readProducts(Configuration* this, String * s)
 
     //String* productName = String_subString(s, idx1, idx2);
 
-    String * productName = Configuration_readString(this, s, &idx2);
+    String* productName = Configuration_readString(this, s, &idx2);
 
     if (productName)
     {
@@ -119,33 +134,33 @@ PRIVATE List* Configuration_readProducts(Configuration* this, String * s)
       List_insertHead(products, product, 1);
     }
 
-    if (!IS_COLON(p+idx2))
+    if (!IS_COLON(p + idx2))
     {
       return 0;
     }
     idx2++;
 
-    while (IS_IGNORED(*(p+idx2))) idx2++;
+    while (IS_IGNORED(*(p + idx2))) idx2++;
 
-    String * location = Configuration_readLocation(this, s, &idx2);
+    String* location = Configuration_readLocation(this, s, &idx2);
 
     if (location) product->location = location;
 
     while (IS_IGNORED(*(p + idx2))) idx2++;
 
-    List * includes = Configuration_readIncludes(this, s, &idx2);
+    List* includes = Configuration_readIncludes(this, s, &idx2);
 
     if (includes) product->includes = includes;
 
     while (IS_IGNORED(*(p + idx2))) idx2++;
 
-    List * uses = Configuration_readUses(this, s, &idx2);
+    List* uses = Configuration_readUses(this, s, &idx2);
 
     if (uses) product->uses = uses;
 
     while (IS_IGNORED(*(p + idx2))) idx2++;
 
-    List * sources = Configuration_readSources(this, s, &idx2);
+    List* sources = Configuration_readSources(this, s, &idx2);
 
     if (sources) product->sources = sources;
 
@@ -157,7 +172,7 @@ PRIVATE List* Configuration_readProducts(Configuration* this, String * s)
   return products;
 }
 
-PRIVATE String * Configuration_readLocation(Configuration* this, String* s, unsigned int * idx)
+PRIVATE String* Configuration_readLocation(Configuration* this, String* s, unsigned int* idx)
 {
   char* p = String_getBuffer(s);
 
@@ -168,7 +183,7 @@ PRIVATE String * Configuration_readLocation(Configuration* this, String* s, unsi
   return Configuration_readString(this, s, idx);
 }
 
-PRIVATE List* Configuration_readIncludes(Configuration * this, String* s, unsigned int * idx)
+PRIVATE List* Configuration_readIncludes(Configuration* this, String* s, unsigned int* idx)
 {
   char* p = String_getBuffer(s);
 
@@ -179,7 +194,7 @@ PRIVATE List* Configuration_readIncludes(Configuration * this, String* s, unsign
   return Configuration_readList(this, s, idx);
 }
 
-PRIVATE List* Configuration_readUses(Configuration* this, String* s, unsigned int * idx)
+PRIVATE List* Configuration_readUses(Configuration* this, String* s, unsigned int* idx)
 {
   char* p = String_getBuffer(s);
 
@@ -190,7 +205,7 @@ PRIVATE List* Configuration_readUses(Configuration* this, String* s, unsigned in
   return Configuration_readList(this, s, idx);
 }
 
-PRIVATE List* Configuration_readSources(Configuration* this, String* s, unsigned int * idx)
+PRIVATE List* Configuration_readSources(Configuration* this, String* s, unsigned int* idx)
 {
   char* p = String_getBuffer(s);
 
@@ -201,7 +216,7 @@ PRIVATE List* Configuration_readSources(Configuration* this, String* s, unsigned
   return Configuration_readList(this, s, idx);
 }
 
-PRIVATE String* Configuration_readString(Configuration* this, String* s, unsigned int * idx)
+PRIVATE String* Configuration_readString(Configuration* this, String* s, unsigned int* idx)
 {
   char* p = String_getBuffer(s);
   unsigned int idx1 = *idx;
@@ -220,7 +235,7 @@ PRIVATE List* Configuration_readList(Configuration* this, String* s, unsigned in
   unsigned int idx1 = *idx;
   List* l = 0;
 
-  if (*(p+*idx)=='[')
+  if (*(p + *idx) == '[')
   {
     l = List_new();
     (*idx)++;
@@ -258,7 +273,7 @@ PRIVATE void Configuration_readEndOfLine(Configuration* this, String* s, unsigne
 {
   char* p = String_getBuffer(s);
 
-  while (!IS_EOL(p + *idx) && (*idx<String_getLength(s))) (*idx)++;
+  while (!IS_EOL(p + *idx) && (*idx < String_getLength(s))) (*idx)++;
   *idx += 2;
 }
 
