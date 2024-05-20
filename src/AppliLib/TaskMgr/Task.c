dfeb57a733e7396ba2f212322d5cf693e4892a75
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-05-20 14:36:53 +0100

    Add comments

--- src/AppliLib/TaskMgr/Task.c
+++ src/AppliLib/TaskMgr/Task.c
@@ -5,14 +5,14 @@
 #include "Memory.h"
 #include "Debug.h"
 
-#ifndef WIN32
-#include <pthread.h>
-#else
-#include <windows.h>
-#include <conio.h>
-#include <process.h>
-#endif
+/* Max number of params for body of callback */
+#define MAX_PARAMS (5)
 
+PRIVATE TaskMgr* taskMgr = 0;
+
+/**********************************************//**
+  @class TaskMgr
+**************************************************/
 struct Task
 {
   void* (*body)(void* p);
@@ -22,11 +22,14 @@ struct Task
   int isRunning;
   int isCompleted;
   int execTime;
-
 };
 
-PRIVATE TaskMgr* taskMgr = 0;
-
+/********************************************************//**
+  @brief Create a task object.
+  @public
+  @memberof Task
+  @return The new instance of a task.
+************************************************************/
 PUBLIC Task* Task_create(void * (*body)(void* p), int nbParams, void ** params)
 {
   Task* this = 0;
@@ -42,15 +45,26 @@ PUBLIC Task* Task_create(void * (*body)(void* p), int nbParams, void ** params)
   this->isRunning = 0;
   this->isCompleted = 0;
   this->execTime = 0;
-
+  
   return this;
 }
 
+/********************************************************//**
+  @brief Destroy a task object.
+  @public
+  @memberof Task
+************************************************************/
 PUBLIC void Task_destroy(Task* this)
 {
   Memory_free(this, sizeof(Task));
 }
 
+/********************************************************//**
+  @brief Obtain the status of the ready flag.
+  @public
+  @memberof Task
+  @return Ready flag.
+************************************************************/
 PUBLIC int Task_isReady(Task* this)
 {
   if (this == 0) return 0;
@@ -58,16 +72,69 @@ PUBLIC int Task_isReady(Task* this)
   return this->isReady;
 }
 
+/********************************************************//**
+  @brief Mark a task as ready.
+  @public
+  @memberof Task
+************************************************************/
+PUBLIC void Task_setReady(Task* this)
+{
+  this->isReady = 1;
+  this->isRunning = 0;
+  this->isCompleted = 0;
+}
+
+/********************************************************//**
+  @brief Obtain the status of the running flag.
+  @public
+  @memberof Task
+  @return Running flag.
+************************************************************/
 PUBLIC int Task_isRunning(Task* this)
 {
   return this->isRunning;
 }
 
+/********************************************************//**
+  @brief Mark a task as running.
+  @public
+  @memberof Task
+************************************************************/
+PUBLIC void Task_setRunning(Task* this)
+{
+  this->isReady = 0;
+  this->isRunning = 1;
+  this->isCompleted = 0;
+}
+
+/********************************************************//**
+  @brief Obtain the status of the completion flag.
+  @public
+  @memberof Task
+  @return Completion flag.
+************************************************************/
 PUBLIC int Task_isCompleted(Task* this)
 {
   return this->isCompleted;
 }
 
+/********************************************************//**
+  @brief Mark a task as completed
+  @public
+  @memberof Task
+************************************************************/
+PUBLIC void Task_setCompleted(Task* this)
+{
+  this->isReady = 0;
+  this->isRunning = 0;
+  this->isCompleted = 1;
+}
+
+/********************************************************//**
+  @brief Start a Task.
+  @public
+  @memberof Task
+************************************************************/
 PUBLIC void Task_start(Task* this)
 {
   this->isReady = 1;
@@ -76,16 +143,17 @@ PUBLIC void Task_start(Task* this)
   TaskMgr_start(taskMgr, this);
 }
 
+/********************************************************//**
+  @brief Execute the body of the task.
+  @public
+  @memberof Task
+  @return TBD
+************************************************************/
 PUBLIC void Task_executeBody(Task* this)
 {
-  this->isRunning = 1;
-  this->isCompleted = 0;
-  this->isReady = 0;
 
   //PRINT(("Arg: %d\n", (int)this->params[0]));
   this->body(&this->params[0]);
 
-  this->isCompleted = 1;
-  this->isRunning = 0;
-  this->isReady = 0;
+  Task_setCompleted(this);
 }
