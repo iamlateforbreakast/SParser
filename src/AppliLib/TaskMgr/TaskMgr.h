bf8c4e6a99cea477e10204edcc086f34cd0669af
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-05-24 11:27:36 +0100

    Add stop

--- src/AppliLib/TaskMgr/TaskMgr.h
+++ src/AppliLib/TaskMgr/TaskMgr.h
@@ -12,4 +12,5 @@ PUBLIC void TaskMgr_delete(TaskMgr * this);
 PUBLIC void TaskMgr_print(TaskMgr * this);
 PUBLIC unsigned int TaskMgr_getSize(TaskMgr * this);
 PUBLIC int TaskMgr_start(TaskMgr * this, Task * task);
+PUBLIC void TaskMgr_stop(TaskMgr* this);
 #endif /* _TASKMGR_H_ */
