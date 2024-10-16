a6da98c04cd87a7f8821a62cf719d51c5e8cb874
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-10-16 17:22:26 +0100

    Update UT_String_01.c

--- src/CommonLib/String/tests/UT_String_01.c
+++ src/CommonLib/String/tests/UT_String_01.c
@@ -1,10 +1,8 @@
-/* UT_String_01.c */
 #include "ObjectMgr.h"
 #include "Object.h"
 #include "String2.h"
 #include "Memory.h"
 #include "Debug.h"
-
 #include <stdio.h>
 
 #define DEBUG (0)
@@ -17,39 +15,32 @@
                           { printf("\x1b[32mPassed\x1b[0m\n");} \
                           else { printf("\x1b[31mFailed\x1b[0m\n"); return 0;}
 #endif
-
 int step1()
 {
   int isPassed = 1;
-
   String * testString = 0;
 
   PRINT(("Step 1: Test 1 - Create an instance of class String: "));
   testString = String_new("Hello World!");
 
   isPassed = isPassed && (testString!=0);
-
   UT_ASSERT((isPassed));
-
   PRINT(("Step 1: Test 2 - Delete instance of String: "));
   String_delete(testString);
 
   UT_ASSERT((1));
-
   PRINT(("Step 1: Test 3 - Check all memory is freed: "));
   ObjectMgr * objectMgr = ObjectMgr_getRef();
   isPassed = isPassed && (ObjectMgr_report(objectMgr) == 1);
   UT_ASSERT((isPassed));
   TRACE(("Nb objects left allocated: %d\n", ObjectMgr_report(objectMgr)));
   ObjectMgr_delete(objectMgr);
-
   return isPassed;
 }
 
 int step2()
 {
   int isPassed = 1;
-
   String * testString = 0;
   String * testPattern = 0;
 
@@ -71,21 +62,21 @@ int step2()
   TRACE(("  Nb objects left allocated: %d\n", ObjectMgr_report(objectMgr)));
   UT_ASSERT((isPassed));
   ObjectMgr_delete(objectMgr);
-
   return isPassed;
 }
 
 int step3()
 {
   int isPassed = 1;
-
   String * testString = String_new("test.c");
   const char * testPattern = "*.c";
+
   
   PRINT(("Step 3: Test 1 - Check a string matches a pattern: "));
   isPassed = isPassed && String_matchWildcard(testString, testPattern); 
   UT_ASSERT((isPassed));
 
+  
   String_delete(testString);
   
   PRINT(("Step 3: Test 2 - Check all memory is freed: "));
@@ -94,7 +85,6 @@ int step3()
   TRACE(("  Nb objects left allocated: %d\n", ObjectMgr_report(objectMgr)));
   UT_ASSERT((isPassed));
   ObjectMgr_delete(objectMgr);
-  
   return isPassed;
 }
 
@@ -102,11 +92,10 @@ int step3()
 int step4()
 {
   int isPassed = 1;
-
   String * testString = 0;
-  const char * testPatterns[] = { "*.txt", "*.[ch]","*.c" };
-  const char * testStrings[] = {  "test.c", "test.h", "/cygdrive/c/Solo/CSW_3.0.3_19wk35/payload/pl/plParam.c" };
-  const unsigned int results[] = { 0, 1, 1 };
+  const char * testPatterns[] = { "*.txt", "*.[ch]","*.c", "/index.html" , "/test.js", "/"};
+  const char * testStrings[] = {  "test.c", "test.h", "/cygdrive/c/Solo/CSW_3.0.3_19wk35/payload/pl/plParam.c", "/", "/test.js", "/"};
+  const unsigned int results[] = { 0, 1, 1 , 0, 1, 1};
   unsigned int nbChecksOk = 0;
   int i = 0;
   
@@ -114,9 +103,17 @@ int step4()
   {
     testString = String_new(testStrings[i]);
 
-    if (String_matchWildcard(testString, testPatterns[i]) == results[i]) nbChecksOk++;
-    
-    String_delete(testString);
+    PRINT((" Check %s matches %s: ", testStrings[i], testPatterns[i]));
+    if (String_matchWildcard(testString, testPatterns[i]) == results[i])
+    {
+      nbChecksOk++;
+      PRINT(("OK\n"));
+    }
+    else
+    {
+      PRINT(("Failed\n"));
+    }
+      String_delete(testString);
   }
   
   isPassed = isPassed && (nbChecksOk == sizeof(results)/sizeof(results[0]));
@@ -134,21 +131,20 @@ int step4()
 int step5()
 {
   int isPassed = 1;
+    String* testString = String_new("TestString");
+    String* compareString = 0;
 
-  String* testString = String_new("TestString");
-  String* compareString = 0;
+    char* compareText[] = {"AestString", "U", "test", "TestString5", "TestString"};
 
-  char* compareText[] = {"AestString", "U", "test", "TestString5", "TestString"};
+    int comp = 0;
 
-  int comp = 0;
-
-  for (int i = 0; i < 5; i++)
-  {
+    for(int i = 0; i < 5; i++)
+    {
     compareString = String_newByRef(compareText[i]);
-    comp = String_compare(testString, compareString);
-    printf("Compare %s with %s result = %d\n", String_getBuffer(testString), String_getBuffer(compareString), comp);
-    String_delete(compareString);
-  }
+        comp = String_compare(testString, compareString);
+        printf("Compare %s with %s result = %d\n", String_getBuffer(testString), String_getBuffer(compareString), comp);
+        String_delete(compareString);
+    }
 
   PRINT(("Step 5: Test 2 - Check all memory is freed: "));
   ObjectMgr * objectMgr = ObjectMgr_getRef();
@@ -156,14 +152,12 @@ int step5()
   TRACE(("  Nb objects left allocated: %d\n", ObjectMgr_report(objectMgr)));
   UT_ASSERT((isPassed));
   ObjectMgr_delete(objectMgr);
-
   return isPassed;
 }
 
 int step6()
 {
   int isPassed = 1;
-
   String * testString = String_new("TestString");
 
   String_append(testString, "123");
@@ -171,22 +165,19 @@ int step6()
   printf("String append %s\n", String_getBuffer(testString));
 
   String_delete(testString);
-
   return isPassed;
 }
-
 int main()
 {
-  step1();
-  step2();
-  step3();
+  //step1();
+  //step2();
+  //step3();
   step4();
-  step5();
-  step6();
+  //step5();
+  //step6();
 
   ObjectMgr* objMgr = ObjectMgr_getRef();;
   ObjectMgr_reportUnallocated(objMgr);
   Memory_report();
-
   return 0;
 }
