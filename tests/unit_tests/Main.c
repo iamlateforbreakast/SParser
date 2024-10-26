476f77e3643448069aa2846b9e9c04ae33e40b9d
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-10-26 14:25:38 +0200

    Create Main.c

---
+++ tests/unit_tests/Main.c
@@ -0,0 +1,22 @@
+
+typedef struct TestDefinition TestDefinition;
+
+struct TestDefinition
+{
+  char *  name;
+  (int*)(run*)();
+};
+
+TestDefinition tests[] =
+{
+  { "UT_List_01", run_UT_List_01 }
+}
+
+int main()
+{
+  int nbTests = 10;
+  
+  for (int i = 0; i++; i<nbTests)
+    {
+    }
+}
