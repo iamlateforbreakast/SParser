d913cb0d15631efb5280a36e3aa39226834c252e
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2024-10-27 22:09:18 +0000

    Allow to output in file

--- src/CommonLib/Debug/Debug.c
+++ src/CommonLib/Debug/Debug.c
@@ -10,10 +10,34 @@
 
 #include <stdio.h>
 
+FILE * open_channel(const char * name)
+{
+  FILE * channel;
+
+  channel = fopen(name,"wb+");
+
+  if (!channel) channel = stdout;
+
+  return channel;
+}
+
+void close_channel(FILE * channel)
+{
+  fclose(channel);
+}
+
 void dbg_printf(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
     vfprintf(stdout, fmt, args);
+    va_end(args);
+}
+
+void dbg_fprintf(FILE * channel, const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(channel, fmt, args);
     va_end(args);
 }
--- src/CommonLib/Debug/Debug.h
+++ src/CommonLib/Debug/Debug.h
@@ -4,7 +4,12 @@
 #include <stdio.h>
 
 #define TRACE(x) do { if (DEBUG) dbg_printf x; } while (0)
+#define TRACE2(x) do { if (DEBUG) dbg_fprintf x; } while (0)
 
 #define PRINT(x) do { dbg_printf x; } while (0)
+#define PRINT2(x) do { dbg_fprintf x; } while (0)
 
+FILE * open_channel(const char * name);
+void close_channel(FILE * channel);
 void dbg_printf(const char *fmt, ...);
+void dbg_fprintf(FILE * channel, const char *fmt, ...);
