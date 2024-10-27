674f6c55391b945794eb9e85465e70edfdd89190
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2024-10-27 22:08:50 +0000

    Implement test

--- tests/unit_tests/main.c
+++ tests/unit_tests/main.c
@@ -1,3 +1,6 @@
+#include "Debug.h"
+#include <stdio.h>
+
 extern int run_UT_List_01();
 
 typedef struct TestDefinition TestDefinition;
@@ -20,5 +23,8 @@ int main()
   for (int i = 0; i<nbTests; i++)
   {
     int result = tests[i].run();
+    if (result) PRINT2((stdout, "%s: OK\n", tests[i].name));
+    else
+      PRINT2((stdout, "%s: FAILED\n", tests[i].name));
   }
 }
