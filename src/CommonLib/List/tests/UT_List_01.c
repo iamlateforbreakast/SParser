917b5a7c1bfa9b386fec136ac7054f763b65fb23
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2024-10-27 22:10:24 +0000

    Rework structure

--- src/CommonLib/List/tests/UT_List_01.c
+++ src/CommonLib/List/tests/UT_List_01.c
@@ -23,6 +23,8 @@
 
 TestObject* items[MAX_OBJECT_NB * 2];
 
+FILE * logChannel;
+
 int init_testobjects()
 {
   for (int i = 0; i < MAX_OBJECT_NB * 2; i++)
@@ -51,37 +53,37 @@ int step1()
 
   int i = 0;
 
-  PRINT(("Step 1: Test 1 - Create a list: "));
+  PRINT2((logChannel, "Step 1: Test 1 - Create a list: "));
   testList = List_new();
 
   UT_ASSERT((testList!=0));
 
-  PRINT(("Step 1: Test 2 - Insert %d objects in list: ", MAX_OBJECT_NB * 2));
+  PRINT2((logChannel, "Step 1: Test 2 - Insert %d objects in list: ", MAX_OBJECT_NB * 2));
   for (i = 0; i < MAX_OBJECT_NB * 2; i++)
   {
     List_insertHead(testList, items[i], 1);
-    TRACE(("Nb items %d\n", List_getNbNodes(testList)));
-    TRACE(("  Allocated %d bytes at %x\n", ((Object*)items[i])->class->f_size(0), items[i]));
+    TRACE2((logChannel, "Nb items %d\n", List_getNbNodes(testList)));
+    TRACE2((logChannel, "  Allocated %d bytes at %x\n", ((Object*)items[i])->class->f_size(0), items[i]));
   }
   UT_ASSERT((List_getNbNodes(testList) == MAX_OBJECT_NB * 2));
 
-  PRINT(("Step 1: Test 3 - Remove %d objects from list: ", MAX_OBJECT_NB * 2));
+  PRINT2((logChannel, "Step 1: Test 3 - Remove %d objects from list: ", MAX_OBJECT_NB * 2));
   for (i = 0; i < MAX_OBJECT_NB * 2; i++)
   {
     removed[i] = List_removeHead(testList);
-    TRACE(("  Removed %d bytes at %x\n", ((Object*)removed[i])->class->f_size(0), removed[i]));
+    TRACE2((logChannel, "  Removed %d bytes at %x\n", ((Object*)removed[i])->class->f_size(0), removed[i]));
     TestObject_delete(removed[i]);
   }
   UT_ASSERT((1));
 
-  PRINT(("Step 1: test 4 - Delete List: "));
+  PRINT2((logChannel, "Step 1: test 4 - Delete List: "));
   List_delete(testList);
   UT_ASSERT((1));
 
-  PRINT(("Step 1: Test 5 - Check all memory is freed: "));
+  PRINT2((logChannel, "Step 1: Test 5 - Check all memory is freed: "));
   ObjectMgr * objectMgr = ObjectMgr_getRef();
   UT_ASSERT((ObjectMgr_report(objectMgr) == 1));
-  TRACE(("Nb objects left allocated: %d\n", ObjectMgr_report(objectMgr)));
+  TRACE2((logChannel, "Nb objects left allocated: %d\n", ObjectMgr_report(objectMgr)));
   ObjectMgr_delete(objectMgr);
 
   return 0;
@@ -95,38 +97,38 @@ int step2()
 
   int i = 0;
 
-  PRINT(("Step 2: Test 1 - Build 2 Lists: "));
+  PRINT2((logChannel, "Step 2: Test 1 - Build 2 Lists: "));
   testList1 = List_new();
   testList2 = List_new();
 
   UT_ASSERT((1));
 
-  PRINT(("Step 2: test 2 - Insert head and insert tail: \n"));
+  PRINT2((logChannel, "Step 2: test 2 - Insert head and insert tail: \n"));
 
   for (i=0; i< MAX_OBJECT_NB; i++)
   {
     List_insertHead(testList1, items[i], 1);
-    TRACE((" Nb items %d\n", List_getNbNodes(testList1)));
+    TRACE2((logChannel, " Nb items %d\n", List_getNbNodes(testList1)));
   }
   for (i= MAX_OBJECT_NB; i < MAX_OBJECT_NB * 2; i++)
   {
     List_insertTail(testList2, items[i], 1);
-    TRACE((" Nb items %d\n", List_getNbNodes(testList2)));
+    TRACE2((logChannel, " Nb items %d\n", List_getNbNodes(testList2)));
   }
 
-  PRINT(("Step 2: test 3 - Print both lists: \n"));
-  PRINT((" List1 ------------->\n"));
+  PRINT2((logChannel, "Step 2: test 3 - Print both lists: \n"));
+  PRINT2((logChannel, " List1 ------------->\n"));
   List_forEach(testList1, (void (*)(void* o))&TestObject_print);
-  PRINT((" List2 ------------->\n"));
+  PRINT2((logChannel, " List2 ------------->\n"));
   List_forEach(testList2, (void (*)(void* o))&TestObject_print);
 
-  PRINT(("Step 2: test 4 - Merge both lists: \n"));
+  PRINT2((logChannel, "Step 2: test 4 - Merge both lists: \n"));
   List_merge(testList1, testList2);
-  PRINT((" List1 + List2 ----->\n"));
+  PRINT2((logChannel, " List1 + List2 ----->\n"));
   List_forEach(testList1, (void (*)(void* o))&TestObject_print);
 
   testList3 = List_copy(testList1);
-  PRINT((" Copy of List1 ------>\n"));
+  PRINT2((logChannel, " Copy of List1 ------>\n"));
   List_forEach(testList3, (void (*)(void* o))&TestObject_print);
 
   List_delete(testList1);
@@ -181,8 +183,9 @@ int step2()
   return 0;
 }*/
 
-int main()
+int run_UT_List_01()
 {
+  logChannel = open_channel("UT_List_01.log");
 
   init_testobjects();
 
@@ -197,6 +200,8 @@ int main()
   ObjectMgr* objMgr = ObjectMgr_getRef();;
   ObjectMgr_reportUnallocated(objMgr);
   Memory_report();
+
+  close_channel(logChannel);
 
   return 0;
 }
