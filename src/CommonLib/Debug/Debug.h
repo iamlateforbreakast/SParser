6460926d15ca9762591a9fa17d22e3f962a1bd33
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2024-10-28 15:55:53 +0000

    Update Debug.h

--- src/CommonLib/Debug/Debug.h
+++ src/CommonLib/Debug/Debug.h
@@ -3,13 +3,15 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-#define TRACE(x) do { if (DEBUG) dbg_printf x; } while (0)
-#define TRACE2(x) do { if (DEBUG) dbg_fprintf x; } while (0)
+#define TRACE(x) do { if (DEBUG) Debug_dbgPrintf x; } while (0)
+#define TRACE2(x) do { if (DEBUG) Debug_dbgFprintf x; } while (0)
 
-#define PRINT(x) do { dbg_printf x; } while (0)
-#define PRINT2(x) do { dbg_fprintf x; } while (0)
+#define PRINT(x) do { Debug_dbgPrintf x; } while (0)
+#define PRINT2(x) do { Debug_dbgFprintf x; } while (0)
 
-FILE * open_channel(const char * name);
-void close_channel(FILE * channel);
-void dbg_printf(const char *fmt, ...);
-void dbg_fprintf(FILE * channel, const char *fmt, ...);
+FILE * Debug_openChannel(const char * name);
+void Debug_closeChannel(FILE * channel);
+void Debug_setStdoutChannel(FILE * channel);
+void Debug_setStderrChannel(FILE * channel);
+void Debug_dbgPrintf(const char *fmt, ...);
+void Debug_dbgFprintf(FILE * channel, const char *fmt, ...);
