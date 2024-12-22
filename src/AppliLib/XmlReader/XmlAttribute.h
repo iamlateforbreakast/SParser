542610c227aeeb34cabf1949f5b50b2f4c1b9185
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2024-12-22 00:14:56 +0000

    Create file

---
+++ src/AppliLib/XmlReader/XmlAttribute.h
@@ -0,0 +1,6 @@
+#ifndef _XMLATTRIBUTE_H_
+#define _XMLATTRIBUTE_H_
+
+typedef struct XmlAttribute XmlAttribute;
+
+#endif /* _XMLATTRIBUTE_H_ */
---
+++ src/AppliLib/XmlReader/XmlElement.h
@@ -0,0 +1,6 @@
+#ifndef _XMLELEMENT_H_
+#define _XMLELEMENT_H_
+
+typedef struct XmlElement XmlElement;
+
+#endif /* _XMLELEMENT_H_ */
