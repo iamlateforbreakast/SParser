7500a3df6a8e03d8dd3895ad85ebd09c36d729b1
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2024-12-23 11:08:01 +0000

    Implement test

--- src/AppliLib/XmlReader/tests/UT_XmlReader_01.c
+++ src/AppliLib/XmlReader/tests/UT_XmlReader_01.c
@@ -1,11 +1,25 @@
 #include "XmlReader.h"
+#include "String2.h"
+#include "Object.h"
 #include "Debug.h"
 
 PRIVATE FILE * logChannel;
+PRIVATE char * testXmlBuffer = "<root><person></person></root>";
+PRIVATE String* testXmlString;
 
 PRIVATE int UT_XmlReader_01_step1()
 {
   int isPassed = 1;
+
+  PRINT2((logChannel, "Step 1: Test 1 - Create an XmlReader object: "));
+
+  XmlReader * testXmlReader = XmlReader_new(testXmlString);
+
+  PRINT2((logChannel, "Step 1: Test 2 - Delete an XmlReader object: "));
+
+  XmlReader_delete(testXmlReader);
+
+  isPassed = isPassed && (OBJECT_IS_INVALID(testXmlReader));
 
   return isPassed;
 }
@@ -16,7 +30,11 @@ int main()
 
   logChannel = Debug_openChannel("UT_List_01.log");
   
+  testXmlString = String_newByRef(testXmlBuffer);
+
   isPassed = isPassed && UT_XmlReader_01_step1();
+
+  String_delete(testXmlString);
 
   Debug_closeChannel(logChannel);
 
