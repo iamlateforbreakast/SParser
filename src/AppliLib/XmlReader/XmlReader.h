738bda2a146ad548c1afb899f06bb8ca5c0c7385
iamlateforbreakast <iamlateforbreakast@users.noreply.github.com>
2024-12-23 08:54:29 +0000

    Compile test

--- src/AppliLib/XmlReader/XmlReader.h
+++ src/AppliLib/XmlReader/XmlReader.h
@@ -1,9 +1,12 @@
 #ifndef _XMLREADER_H_
 #define _XMLREADER_H_
+#include "XmlElement.h"
+#include "String2.h"
 
 typedef struct XmlReader XmlReader;
 
-XmlReader* XmlReader_new();
+XmlReader* XmlReader_new(String* s);
 void XmlReader_delete(XmlReader* this);
+XmlElement* XmlReader_read(XmlReader* this);
 
 #endif /* _XMLREADER_H_ */
--- src/AppliLib/XmlReader/tests/UT_XmlReader_01.c
+++ src/AppliLib/XmlReader/tests/UT_XmlReader_01.c
@@ -1,1 +1,24 @@
+#include "XmlReader.h"
+#include "Debug.h"
 
+PRIVATE FILE * logChannel;
+
+PRIVATE int UT_XmlReader_01_step1()
+{
+  int isPassed = 1;
+
+  return isPassed;
+}
+
+int main()
+{
+  int isPassed = 1;
+
+  logChannel = Debug_openChannel("UT_List_01.log");
+  
+  isPassed = isPassed && UT_XmlReader_01_step1();
+
+  Debug_closeChannel(logChannel);
+
+  return isPassed;
+}
