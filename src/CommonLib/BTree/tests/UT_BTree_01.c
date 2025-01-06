7dc9a7bce9d1e0cf18a6ab44f44077c32882e075
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-01-06 16:20:44 +0000

    Update UT_BTree_01.c

--- src/CommonLib/BTree/tests/UT_BTree_01.c
+++ src/CommonLib/BTree/tests/UT_BTree_01.c
@@ -5,6 +5,7 @@
 #include "ObjectMgr.h"
 #include "Memory.h"
 #include "Debug.h"
+
 #include <stdio.h>
 
 #define DEBUG (0)
@@ -21,12 +22,11 @@
 #define ORDER (10)
 
 extern char words1000[];
-
 PRIVATE List * keys;
 PRIVATE TestObject ** testObjects;
 PRIVATE TestObject ** removedObjects;
 PRIVATE int nbTokens = 0;
-PRIVATE FILE* channelLog = 0;
+PRIVATE FILE* UT_BTree_01_channelLog = 0;
 
 PRIVATE int UT_BTree_01_init_keys()
 {
@@ -66,7 +66,6 @@ PRIVATE int UT_BTree_01_delete_keys()
 PRIVATE int UT_BTree_01_step1()
 {
   int isPassed = 1;
-
   TestObject * removedObject = 0;
   String * key = (String*)List_getHead(keys);
 
@@ -76,38 +75,34 @@ PRIVATE int UT_BTree_01_step1()
   PRINT(("Step 1: Test 1 - Create an instance of class BTree: "));
   testTree = BTree_new(ORDER);
   isPassed = isPassed && (OBJECT_IS_VALID(testTree));
-
   UT_ASSERT((isPassed));
 
   PRINT(("Step 1: Test 2 - Insert one object: "));
   BTree_add(testTree, (Object*)key, (Object*)testObjects[0], 0);
-
   UT_ASSERT((isPassed));
 
   PRINT(("Step 1: test 3 - Check the number of nodes: "));
   unsigned int nbNodes = BTree_getNbNodes(testTree);
-  //PRINT(("  Nb nodes = %d\n", nbNodes));
   isPassed = isPassed && (nbNodes == 1);
-
   UT_ASSERT(isPassed);
-
   PRINT(("Step 1: Test 3 - Remove the object: "));
   removedObject = (TestObject*)BTree_remove(testTree, (Object*)key);
-  UT_ASSERT((1));
+  nbNodes = BTree_getNbNodes(testTree);
+  isPassed = isPassed && (nbNodes == 0);
+  UT_ASSERT((isPassed));
 	
   PRINT(("Step 1: Test 4 - Delete BTree: "));
   //String_delete(key);
   BTree_delete(testTree);
-  UT_ASSERT((1));
+  isPassed = isPassed && (OBJECT_IS_INVALID(testTree));
+  UT_ASSERT((isPassed));
 
   PRINT(("Step 1: Test 5 - Check all memory is freed: "));
-
   ObjectMgr* objectMgr = ObjectMgr_getRef();
   isPassed = isPassed && (ObjectMgr_report(objectMgr) == 1);
   UT_ASSERT((isPassed));
   TRACE(("Nb objects left allocated: %d\n", ObjectMgr_report(objectMgr)));
   ObjectMgr_delete(objectMgr);
-
   return isPassed;
 }
 
@@ -124,7 +119,8 @@ PRIVATE int UT_BTree_01_step2()
 
   PRINT(("Step 2: Test 1 - Create an instance of class BTree: "));
   testTree = BTree_new(ORDER);
-  UT_ASSERT((1));
+  isPassed = isPassed && (OBJECT_IS_VALID(testTree));
+  UT_ASSERT((isPassed));
 
   PRINT(("Step 2: Test 2 - Insert %d object: ", n));
   for (int i=0; i< n; i++)
@@ -132,8 +128,10 @@ PRIVATE int UT_BTree_01_step2()
 	key = List_getNext(keys);
 	//Object_print((Object*)key);
     BTree_add(testTree, (Object*)key, (Object*)testObjects[i], 0);
+	int nbNodes = BTree_getNbNodes(testTree);
+	isPassed = isPassed && (nbNodes == i + 1);
   }
-  UT_ASSERT((1));
+  UT_ASSERT((isPassed));
 
   //BTree_print(testTree);
 
@@ -190,6 +188,7 @@ PRIVATE int UT_BTree_01_step3()
 	//Object_print(key);
 	removedObjects[i] = (TestObject*)BTree_remove(testTree, (Object*)key);
 	//BTree_print(testTree);
+
   }
 
   
@@ -204,10 +203,8 @@ PRIVATE int UT_BTree_01_step3()
 PUBLIC int run_UT_BTree_01(void)
 {
   int isPassed = 1;
-
-  channelLog = Debug_openChannel("UT_BTree_01.log");
-  Debug_setStdoutChannel(channelLog);
-
+  UT_BTree_01_channelLog = Debug_openChannel("UT_BTree_01.log");
+  Debug_setStdoutChannel(UT_BTree_01_channelLog);
   ObjectMgr* objMgr = ObjectMgr_getRef();
 
   UT_BTree_01_init_keys();
@@ -215,14 +212,38 @@ PUBLIC int run_UT_BTree_01(void)
   isPassed = isPassed && UT_BTree_01_step1();
   isPassed = isPassed && UT_BTree_01_step2();
   isPassed = isPassed && UT_BTree_01_step3();
-  
+  //step2();
   UT_BTree_01_delete_keys();
 
   ObjectMgr_report(objMgr);
   ObjectMgr_reportUnallocated(objMgr);
   Memory_report();
 
-  Debug_closeChannel(channelLog);
+  //printf("Btree size of pool in   bytes: %d\n", BTree_reportSizeInBytes(testTree));
+  //printf("Btree size of pool in Kibytes: %d\n", BTree_reportSizeInBytes(testTree) / 1024);
+  //printf("Btree size of pool in Mibytes: %d\n", BTree_reportSizeInBytes(testTree) / (1024 * 1024));
+  //BTree_print(testTree);
+	
+  //double cpu_time1 = get_cpu_time();
+  //double wall_time1 = get_wall_time();
+	
+  //printf("Insert CPU time %f\n", cpu_time1 - cpu_time0);
+  //printf("Insert Wall time %f\n", wall_time1 - wall_time0);
+  Debug_closeChannel(UT_BTree_01_channelLog);
+  //cpu_time0 = get_cpu_time();
+  //wall_time0 = get_wall_time();
+
+  //for (int i = 0; i< NB_ITEMS; i++)
+  //{
+    //BTree_get(testTree, keys[i], &pTestItem);
+    //printf("test item = %x %x\n", pTestItem, &items[i]);
+  //}
+
+  //cpu_time1 = get_cpu_time();
+  //wall_time1 = get_wall_time();
+
+  //printf("Search CPU time %f\n", cpu_time1 - cpu_time0);
+  //printf("Search Wall time %f\n", wall_time1 - wall_time0);
 
   return isPassed;
 }
