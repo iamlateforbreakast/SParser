21b6c4906ab56e321d0b37c3c5d7ef653497bfe2
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-05-20 16:58:48 +0100

    Improve file layout

--- src/ParseLib/HTTPServer/HTTPServer.c
+++ src/ParseLib/HTTPServer/HTTPServer.c
@@ -1,9 +1,10 @@
 /**********************************************//**
   @file HTTPServer.c
-
+  
   @brief A HTTP Server class.
-  This class provides server function to create, start
-  HTML pages.
+  
+  This class provides server function to serve
+  files.
 **************************************************/
 
 #include "HTTPServer.h"
@@ -18,7 +19,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 #ifndef WIN32
 #include <sys/socket.h>
 #include <netinet/ip.h>
@@ -29,6 +29,7 @@
 
 #ifdef _WIN32
 #include <windows.h>	/* WinAPI */
+
 /* Windows sleep in 100ns units */
 int msleep(long msec) {
   /* Declarations */
@@ -58,22 +59,27 @@ int msleep(long msec)
 {
   struct timespec ts;
   int res;
+
   if (msec < 0)
   {
     errno = EINVAL;
     return -1;
   }
+
   ts.tv_sec = msec / 1000;
   ts.tv_nsec = (msec % 1000) * 1000000;
+
   do {
     res = nanosleep(&ts, &ts);
   } while (res && errno == EINTR);
+
   return res;
 }
 #endif
+
 #define REQUEST_BUFFER_SIZE (4096)
 #define RESPONSE_BUFFER_SIZE (4096)
-#define MAX_CONNECTIONS (5)
+#define MAX_CONNECTIONS (4)
 #define PORT (8080)
 
 PRIVATE void* HTTPServer_listenTaskBody(void* params);
@@ -95,6 +101,7 @@ struct HTTPServer
 #endif
   Task* connections[MAX_CONNECTIONS];
 };
+
 struct ConnectionParam
 {
 #ifndef WIN32
@@ -127,12 +134,13 @@ Class httpServerClass =
 PUBLIC HTTPServer* HTTPServer_new()
 {
   HTTPServer* this = 0;
+
   int sockOptions = 1;
+
   this = (HTTPServer*)Object_new(sizeof(HTTPServer), &httpServerClass);
 
   if (OBJECT_IS_INVALID(this)) return 0;
 
-  /* Socket_init() */
 #ifdef WIN32   
   if (WSAStartup(MAKEWORD(2, 2), &this->wsa) != 0) {
     PRINT(("\nError: Windows socket subsytsem could not be initialized. Error Code: %d. Exiting..\n", WSAGetLastError()));
@@ -141,8 +149,6 @@ PUBLIC HTTPServer* HTTPServer_new()
 #endif
 
   this->port = PORT;
-
-  /* Socket_create */
   if ((this->fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
     PRINT(("Socket failed.\n"));
@@ -164,7 +170,6 @@ PUBLIC HTTPServer* HTTPServer_new()
   this->server_addr.sin_port = htons(this->port);
 
   // bind socket to port
-  /* Socket_bind */
   if (bind(this->fd,
     (struct sockaddr*)&this->server_addr,
     sizeof(this->server_addr)) < 0)
@@ -173,7 +178,6 @@ PUBLIC HTTPServer* HTTPServer_new()
     HTTPServer_delete(this);
     exit(1);
   }
-
   return this;
 }
 
@@ -186,6 +190,7 @@ PUBLIC HTTPServer* HTTPServer_new()
 PUBLIC void HTTPServer_delete(HTTPServer* this)
 {
   if (OBJECT_IS_INVALID(this)) return;
+
 #ifndef WIN32
   close(this->fd);
 #else
@@ -240,7 +245,6 @@ PUBLIC unsigned int HTTPServer_getSize(HTTPServer* this)
 **************************************************/
 PUBLIC void HTTPServer_start(HTTPServer* this)
 {
-
   // listen for connections
   if (listen(this->fd, 10) < 0) {
     PRINT(("Listen failed.\n"));
@@ -248,30 +252,32 @@ PUBLIC void HTTPServer_start(HTTPServer* this)
     exit(1);
   }
 
-  // client info
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
-  int *client_fd = malloc(sizeof(int) * MAX_CONNECTIONS);
-
+  SOCKET client_fd = 0;
+  char *requestBuffer = (char*)malloc(REQUEST_BUFFER_SIZE);
   TaskMgr* taskMgr = TaskMgr_getRef();
   int nbRequests = 0;
   while (nbRequests<MAX_CONNECTIONS)
   {
-    if ((*client_fd = accept(this->fd,
-                           (struct sockaddr *)&client_addr, 
-                           &client_addr_len)) < 0) 
+    if ((client_fd = accept(this->fd,
+      (struct sockaddr*)&client_addr,
+      &client_addr_len)) < 0) 
     {
       PRINT(("Accept failed.\n"));
       exit(1);
     }
     PRINT(("Received connection request.\n"));
+
     
     void * params[5];
     params[0] = &client_fd;
     Task* connectionListen = Task_create(&HTTPServer_listenTaskBody, 1, params);
+
     Task_start(connectionListen);
     nbRequests++;
   }
+
   int timer = 0;
   while (timer<50)
   {
@@ -282,7 +288,8 @@ PUBLIC void HTTPServer_start(HTTPServer* this)
   TaskMgr_stop(taskMgr);
   //Task_destroy(connectionListen);
   TaskMgr_delete(taskMgr);
-  free(client_fd);
+
+  //free(client_fd);
 #ifndef WIN32
   close(this->fd);
 #else
@@ -296,11 +303,11 @@ PUBLIC void HTTPServer_start(HTTPServer* this)
   @public
   @memberof HTTPServer
 **************************************************/
-PRIVATE void* HTTPServer_listenTaskBody(void* params)
-{ 
+PRIVATE void* HTTPServer_listenTaskBody(void** params)
+{
   //int msg_len = 0;
-#ifndef WIN32   
-  int* client_fd = ((struct ConnectionParam**)params)[0]->client_fd;
+#ifndef WIN32
+  int client_fd = ((struct ConnectionParam**)params)[0]->client_fd;
 #else
   SOCKET client_fd = ((struct ConnectionParam**)params)[0]->client_fd;
 #endif
@@ -308,21 +315,17 @@ PRIVATE void* HTTPServer_listenTaskBody(void* params)
   Memory_set(requestBuffer, 0, REQUEST_BUFFER_SIZE);
 
   PRINT(("Listen body starts\n Client FD:%d\n", client_fd));
-  
   /* request = Connection_waitForHttpRequest*/
   if ((client_fd) && (requestBuffer))
   {
     int nbCharRead = 0;
     do 
     {
-      int msg_len = recv(*client_fd, &requestBuffer[nbCharRead], REQUEST_BUFFER_SIZE - 1, 0);
+      int msg_len = recv(client_fd, &requestBuffer[nbCharRead], REQUEST_BUFFER_SIZE - 1, 0);
       if (msg_len>0)
       {
         nbCharRead += msg_len;
-        //PRINT(("%d out of %d\n", nbCharRead, REQUEST_BUFFER_SIZE));
-        //PRINT(("Request Buffer %s\n", requestBuffer));
         HTTPRequest* request = HTTPRequest_new(requestBuffer);
-        //HTTPRequest_print(request);
         HTTPResponse* response = HTTPServer_serveRequest(request);
         HTTPRequest_delete(request);
         char* responseBuffer = (char*)malloc(RESPONSE_BUFFER_SIZE);
@@ -330,7 +333,7 @@ PRIVATE void* HTTPServer_listenTaskBody(void* params)
         int nbCharToWrite = HTTPResponse_generate(response, responseBuffer, RESPONSE_BUFFER_SIZE - 1);
         if ((client_fd) && (responseBuffer))
         {
-          int msg_len = send(*client_fd, responseBuffer, nbCharToWrite, 0);
+          int msg_len = send(client_fd, responseBuffer, nbCharToWrite, 0);
           if (msg_len == 0) 
           {
             PRINT(("Client closed connection\n"));
@@ -352,90 +355,82 @@ PRIVATE void* HTTPServer_listenTaskBody(void* params)
         return 0;
       }
     } while (1);
-    
-    /* Fails if following line commented out */
-
-    
     #ifndef WIN32
     close(*client_fd);
     #else
     closesocket(client_fd);
     #endif
-    //FileMgr_delete(fm);
-    //String_delete(errorMessage);
   }
   return 0;
 }
-
 PRIVATE HTTPResponse* HTTPServer_serveRequest(HTTPRequest* request)
 { 
   HTTPResponse* response = HTTPResponse_new();
-  String* errorMessage = String_newByRef("<doctype !html><html><head><title>Error</title></head>"
-    "<body><h1>Error!</h1></body></html>\r\n");
-  String* content = errorMessage;
+  HTTPResponse_setVersion(response, 1, 1);
+  HTTPResponse_setStatusCode(response, 200);
+  HTTPResponse_setReason(response, REASON_OK);
+  HTTPResponse_addHeader(response, "Content - Type", "text/html; charset=UTF-8");
+
   FileMgr* fm = FileMgr_new();
 
   PRINT(("Requested path %s\n", String_getBuffer(HTTPRequest_getPath(request))));
-  if (String_matchWildcard(HTTPRequest_getPath(request),"/index.html"))
+
+  if (String_matchWildcard(HTTPRequest_getPath(request), "/index.html"))
   {
     PRINT(("Load index.html\n"));
     FileDesc* fd = FileMgr_addFile(fm, "index.html");
     if (fd)
     {
-      content = FileMgr_load(fm, "index.html");
-      HTTPResponse_setVersion(response, 1, 1);
-      HTTPResponse_setStatusCode(response, 200);
-      HTTPResponse_setReason(response, REASON_OK);
-      HTTPResponse_addHeader(response, "Content - Type", "text/html; charset=UTF-8");
+      String* content = content = FileMgr_load(fm, "index.html");
       HTTPResponse_setBody(response, String_getBuffer(content));
     }
-  } else if (String_matchWildcard(HTTPRequest_getPath(request), "/hello.css"))
+    else
+    {
+      String* errorMessage = String_newByRef("<doctype !html><html><head><title>Error</title></head>"
+        "<body><h1>Error!</h1></body></html>\r\n");
+      HTTPResponse_setBody(response, String_getBuffer(errorMessage));
+    }
+  }
+  else if (String_matchWildcard(HTTPRequest_getPath(request), "/hello.css"))
   {
-    PRINT(("Load hello.cs\n"));
+    PRINT(("Loading hello.cs: "));
     FileDesc* fd = FileMgr_addFile(fm, "hello.css");
     if (fd)
     {
-      content = FileMgr_load(fm, "hello.css");
-      HTTPResponse_setVersion(response, 1, 1);
-      HTTPResponse_setStatusCode(response, 200);
-      HTTPResponse_setReason(response, REASON_OK);
-      HTTPResponse_addHeader(response, "Content - Type", "text/css; charset=UTF-8");
+      String* content = FileMgr_load(fm, "hello.css"); 
       HTTPResponse_setBody(response, String_getBuffer(content));
+      PRINT(("Done\n"));
     }
     else
     {
       PRINT(("Cannot find hello.css.\n"));
     }
-  } else if (String_matchWildcard(HTTPRequest_getPath(request), "/hello.js"))
+  }
+  else if (String_matchWildcard(HTTPRequest_getPath(request), "/hello.js"))
   {
-    PRINT(("Load hello.js\n"));
+    PRINT(("Loading hello.js: "));
     FileDesc* fd = FileMgr_addFile(fm, "hello.js");
     if (fd)
     {
-      content = FileMgr_load(fm, "hello.js");
-      HTTPResponse_setVersion(response, 1, 1);
-      HTTPResponse_setStatusCode(response, 200);
-      HTTPResponse_setReason(response, REASON_OK);
-      HTTPResponse_addHeader(response, "Content - Type", "text/javascript; charset=UTF-8");
+      String* content = FileMgr_load(fm, "hello.js");
       HTTPResponse_setBody(response, String_getBuffer(content));
+      PRINT(("Done\n"));
     }
     else
     {
       PRINT(("Cannot find hello.js.\n"));
       exit(1);
     }
-  } else if (String_matchWildcard(HTTPRequest_getPath(request), "/favicon.ico"))
+  }
+  else if (String_matchWildcard(HTTPRequest_getPath(request), "/favicon.ico"))
   {
-    PRINT(("Load favicon.ico\n"));
+    PRINT(("Loading favicon.ico: "));
     FileDesc* fd = FileMgr_addFile(fm, "favicon.ico");
     if (fd)
     {
-      content = FileMgr_load(fm, "favicon.ico");
-      HTTPResponse_setVersion(response, 1, 1);
-      HTTPResponse_setStatusCode(response, 200);
-      HTTPResponse_setReason(response, REASON_OK);
-      HTTPResponse_addHeader(response, "Content - Type", "image/png; charset=UTF-8");
+      String* content = FileMgr_load(fm, "favicon.ico");
       HTTPResponse_setBody(response, String_getBuffer(content));
+      PRINT(("Done\n"));
     }
     else
     {
@@ -445,11 +440,16 @@ PRIVATE HTTPResponse* HTTPServer_serveRequest(HTTPRequest* request)
   }
   else
   {
-    HTTPResponse_setVersion(response, 1, 1);
-    HTTPResponse_setStatusCode(response, 200);
-    HTTPResponse_setReason(response, REASON_OK);
-    HTTPResponse_addHeader(response, "Content - Type", "text/html; charset=UTF-8");
+    String* errorMessage = String_newByRef("<doctype !html><html><head><title>Error</title></head>"
+      "<body><h1>Error!</h1></body></html>\r\n");
     HTTPResponse_setBody(response, String_getBuffer(errorMessage));
+    //String_delete(errorMessage);
   }
+
+  FileMgr_delete(fm);
+
+  //String_delete(content);
+  
+
   return response;
 }
