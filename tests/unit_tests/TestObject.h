7e241938b57fef8ac1c841815a7d048d2d84e3c9
Thomas Remion <thomas.remion@outlook.com>
2026-03-27 15:01:38 +0000

    Correct constructor parameters

--- tests/unit_tests/TestObject.h
+++ tests/unit_tests/TestObject.h
@@ -11,7 +11,7 @@ PUBLIC TestObject * TestObject_new();
 PUBLIC TestObject * TestObject_newFromAllocator(Allocator* allocator);
 PUBLIC void TestObject_delete(TestObject * this);
 PUBLIC int TestObject_compare(TestObject* this, TestObject* compare);
-PUBLIC TestObject* TestObject_copy();
+PUBLIC TestObject* TestObject_copy(TestObject * this);
 PUBLIC void TestObject_print(TestObject* this);
 PUBLIC unsigned int TestObject_getSize(TestObject* this);
 #endif /* _TESTOBJECT_H_ */
