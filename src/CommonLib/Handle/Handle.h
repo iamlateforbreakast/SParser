6e28fb53c0d36d370f02a226dc09b733745892a3
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2026-04-11 19:58:27 +0100

    Update Handle.h

--- src/CommonLib/Handle/Handle.h
+++ src/CommonLib/Handle/Handle.h
@@ -1,1 +1,18 @@
 /* Handle.h */
+
+#ifndef HANDLE_H
+#define HANDLE_H
+
+#include "Object.h"
+
+typedef struct Handle Handle;
+
+struct Handle
+{
+  Object *object;
+};
+
+Handle * Handle_new();
+void Handle_delete(Handle * this);
+
+#endif /* HANDLE_H */
