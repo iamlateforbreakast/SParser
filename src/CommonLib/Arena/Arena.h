5bf8f3b60a6084d775010eab5e665743a52c4fed
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2026-04-26 20:40:01 +0100

    Add protocol API

--- src/CommonLib/Arena/Arena.h
+++ src/CommonLib/Arena/Arena.h
@@ -1,1 +1,14 @@
 /* Arean.h */
+#ifndef _ARENA_H_
+#define _ARENA_H_
+#include "Allocator.h"
+#include "Types.h"
+
+typedef struct Arena Arena；
+
+PUBLIC Arena * Arena_getRef();
+PUBLIC void Arena_delete(Allocator * this);
+PUBLIC void * Arena_allocate(Allocator * this, unsigned int size);
+PUBLIC void Arena_deallocate(Allocator * this, void * ptr);
+PUBLIC unsigned int Arena_report(Allocator * this);
+#endif /* _ARENA_H_ */
