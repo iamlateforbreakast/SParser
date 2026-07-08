42aaab03116d0d6903556735253a9f6bf3a405fc
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2026-07-08 17:03:22 +0100

    Add re-allocate

--- src/CommonLib/Allocator/Allocator.h
+++ src/CommonLib/Allocator/Allocator.h
@@ -6,8 +6,9 @@ typedef struct Allocator Allocator;
 
 typedef void * (*NewFunction)();
 typedef void (*DeleteFunction)(Allocator * allocator);
-typedef void* (*AllocateFunction)(Allocator * allocator, unsigned int size);
+typedef void * (*AllocateFunction)(Allocator * allocator, unsigned int size);
 typedef void (*DeAllocateFunction)(Allocator * allocator, void * ptr);
+typedef void * (*ReallocateFunction)(Allocator * allocator, void * ptr, unsigned int size);
 typedef unsigned int (*ReportFunction)(Allocator * allocator);
 
 struct Allocator
@@ -16,13 +17,14 @@ struct Allocator
   DeleteFunction delete;
   AllocateFunction allocate;
   DeAllocateFunction deallocate;
+  ReallocateFunction reallocate;
   ReportFunction report;
   unsigned int nbAllocatedObjects;
 };
 
 Allocator * Allocator_new();
 void * Allocator_allocate(Allocator * this, unsigned int size);
-//void * Allocator_allocFromClass(Allocator * this, /* Class class*/);
 void Allocator_deallocate(Allocator * this, void * ptr);
+void * Allocator_reallocate(Allocator * this, void * ptr, unsigned int size);
 void Allocator_delete(Allocator * this);
 #endif /* _ALLOCATOR_H_ */
