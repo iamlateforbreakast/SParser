39106981f7ad76ff41711c776ef5992b0f7c839d
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2026-07-08 17:04:08 +0100

    Update Malloc.c

--- src/CommonLib/Allocator/Malloc.c
+++ src/CommonLib/Allocator/Malloc.c
@@ -9,9 +9,17 @@ struct Malloc
   Allocator allocator;
 };
 
+/**********************************************//**
+  @private
+**************************************************/
 PRIVATE Malloc mem_alloc;
 
-
+/******************************************************//**
+  @brief Get a reference to the Malloc allocator.
+  @public
+  @memberof Allocator
+  @return A pointer to the Malloc allocator instance.
+*******************************************************/
 PUBLIC Malloc * Malloc_getRef()
 {
   mem_alloc.allocator.new = 0;
@@ -23,23 +31,62 @@ PUBLIC Malloc * Malloc_getRef()
   return &mem_alloc;
 }
 
+/********************************************************//**
+  @brief Delete an instance of the class Malloc.
+  @public
+  @memberof Allocator
+  @return None
+************************************************************/
 PUBLIC void Malloc_delete(Allocator * this)
 {
   //Malloc_deallocate((Allocator*)Malloc_getRef(), (char*)this);
 }
 
+/********************************************************//**
+  @brief Allocate memory using the Malloc allocator.
+  @public
+  @memberof Allocator
+  @param size The size of the memory block to allocate.
+  @return A pointer to the allocated memory or NULL if allocation failed.
+************************************************************/
 PUBLIC void * Malloc_allocate(Allocator * this, unsigned int size)
 {
   this->nbAllocatedObjects++;
   return (void*)malloc(size);
 }
 
+/********************************************************//**
+  @brief Deallocate memory using the Malloc allocator.
+  @public
+  @memberof Allocator
+  @param ptr A pointer to the memory block to deallocate.
+  @return None
+************************************************************/
 PUBLIC void Malloc_deallocate(Allocator * this, void * ptr)
 {
   free(ptr);
   this->nbAllocatedObjects--;
 }
 
+/********************************************************//**
+  @brief Reallocate memory using the Malloc allocator.
+  @public
+  @memberof Allocator
+  @param ptr A pointer to the memory block to reallocate.
+  @param size The new size of the memory block.
+  @return A pointer to the reallocated memory or NULL if reallocation failed.
+************************************************************/
+PUBLIC void * Malloc_reallocate(Allocator * this, void * ptr, unsigned int size)
+{
+  return (void*)realloc(ptr, size);
+}
+
+/******************************************************//**
+  @brief Report the number of allocated objects using the Malloc allocator.
+  @public
+  @memberof Allocator
+  @return The number of currently allocated objects.
+*******************************************************/
 PUBLIC unsigned int Malloc_report(Allocator * this)
 {
   return this->nbAllocatedObjects;
